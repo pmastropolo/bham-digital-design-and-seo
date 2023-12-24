a374e66652be866e83989ddfc18cf4fafd1bf55f
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-23 21:16:14 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,30 +64,95 @@ export default function Home() {
           </div>
         </section>
 
-        <div className="mt-20 mb-20 mx-auto py-8 px-4 bg-white rounded-2xl shadow-xl overflow-hidden" style={{ maxWidth: '90%' }}>
+        <div className="mt-20 mb-20 mx-auto py-8 px-4 rounded-2xl shadow-xl overflow-hidden relative z-10" style={{ maxWidth: '90%', backgroundColor: 'white' }}>
   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-10">
-    <div className="space-y-4">
-      <h3 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-800">Explore Our Web Development Services</h3>
-      <p className="text-md md:text-lg text-gray-600">Discover how our tailored web development solutions can elevate your business, leveraging the latest technologies and innovative design.</p>
-        <Link href="/features" legacyBehavior>
-            <a className="inline-block bg-blue-600 hover:bg-blue-800 text-white text-lg py-3 px-8 md:py-4 md:px-10 rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
-              Learn More
-            </a>
-          </Link>
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="relative w-full h-64 md:h-96"> 
-            <Image
-              src="/images/istockphoto-1249598036-612x612.jpg"
-              alt="Web Development"
-              layout="fill"
-              objectFit="contain" 
-              className="rounded-lg"
-            />
-          </div>
-        </div>
+    <div className="space-y-8 text-center"> 
+      <h3 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-800">
+        Explore Our Web Development Services
+      </h3>
+      <p className="text-md md:text-lg text-gray-600">
+        Discover how our tailored web development solutions can elevate your business, leveraging the latest technologies and innovative design.
+      </p>
+      <Link href="/features" legacyBehavior>
+        <a className="inline-block bg-blue-600 hover:bg-blue-800 text-white text-lg py-3 px-8 rounded-md transition duration-300 transform hover:-translate-y-1 shadow-md">
+          Learn More
+        </a>
+      </Link>
+    </div>
+    <div className="flex justify-center items-center">
+      <div className="relative w-full h-64 md:h-96"> 
+        <Image
+          src="/images/istockphoto-1249598036-612x612.jpg"
+          alt="Web Development"
+          layout="fill"
+          objectFit="contain" 
+          className="rounded-lg"
+        />
       </div>
     </div>
+  </div>
+</div>
+
+<div className="mt-20 mb-20 mx-auto py-8 px-4 rounded-2xl shadow-xl overflow-hidden relative z-10" style={{ maxWidth: '90%', backgroundColor: 'white' }}>
+  <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-10">
+    
+    <div className="flex justify-center items-center">
+      <div className="relative w-full h-64 md:h-96"> 
+        <Image
+          src="/images/seocoollll.jpg"
+          alt="SEO image"
+          layout="fill"
+          objectFit="contain" 
+          className="rounded-lg"
+        />
+      </div>
+    </div>
+
+    <div className="space-y-8 text-center"> 
+      <h3 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-800">
+        Discover Our SEO Mastery Packages
+      </h3>
+      <p className="text-md md:text-lg text-gray-600">
+        Enhance your online presence and search engine rankings with our specialized SEO packages. Tailored to fit your unique business needs, our strategies are designed to increase visibility and drive traffic to your website.
+      </p>
+      <Link href="/seopackagepage" legacyBehavior>
+        <a className="inline-block bg-blue-600 hover:bg-blue-800 text-white text-lg py-3 px-8 md:py-4 md:px-10 rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-md">
+          Learn More
+        </a>
+      </Link>
+    </div>
+  </div>
+</div>
+
+
+<div className="mt-20 mb-20 mx-auto py-8 px-4 rounded-2xl shadow-xl overflow-hidden relative z-10" style={{ maxWidth: '90%', backgroundColor: 'white' }}>
+  <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-10">
+    <div className="space-y-8 text-center"> 
+      <h3 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-800">
+        Boost Your Business with Digital Marketing
+      </h3>
+      <p className="text-md md:text-lg text-gray-600">
+        Take the first step towards transforming your online presence. Our tailored Digital Marketing Packages are designed to propel your brands growth and maximize your digital impact. Explore our innovative strategies and solutions that make a real difference. Visit us now and see how we can help you achieve your business goals!
+      </p>
+      <Link href="/digitalmarketingg" legacyBehavior>
+        <a className="inline-block bg-blue-600 hover:bg-blue-800 text-white text-lg py-3 px-8 md:py-4 md:px-10 rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-md">
+          Learn More
+        </a>
+      </Link>
+    </div>
+    <div className="flex justify-center items-center">
+      <div className="relative w-full h-64 md:h-96"> 
+        <Image
+          src="/images/marketttttt.jpg"
+          alt="Web Development"
+          layout="fill"
+          objectFit="contain" 
+          className="rounded-lg"
+        />
+      </div>
+    </div>
+  </div>
+</div>
       </main>
     </div>
   );
