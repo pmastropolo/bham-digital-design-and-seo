29edbff9dff04f0796af08080717ccacbe1e1630
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-27 00:31:03 -0600

    Update MyHead.js

--- components/MyHead.js
+++ components/MyHead.js
@@ -4,11 +4,11 @@ export default function MyHead({ title, description, image, url }) {
     return (
         <Head>
             <title>{`${title} | Bham SEO and Design Co`}</title>
-            <meta name="description" content={description} />
+            <meta name="description" content="Bham SEO and Design Co offers expert SEO and web design services to elevate your online presence. Discover innovative strategies tailored for your success." />
             <meta property="og:title" content={title} key="title" />
             <meta property="og:description" content={description} key="description" />
             <meta property="og:image" content={image} key="image" />
-            <meta property="og:url" content={url} key="url" />
+            <meta property="og:url" content="https://bhamseoanddesignco.com/" key="url" />
             <link rel="icon" href="/images/computerfavicon/favicon.ico" /> 
 
             <meta name="twitter:title" content={title} />
