d100ffe91b1383f96ffef033c23c6205a8d5eda9
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-27 10:16:36 -0600

    Update apply.js

--- pages/apply.js
+++ pages/apply.js
@@ -2,64 +2,142 @@ import React from 'react'
 
 const Apply = () => {
     return (
-      <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px', color: '#333' }}>
-        <h1 style={{ fontSize: '36px', fontWeight: 'bold', textAlign: 'center', marginBottom: '30px', color: '#1a202c' }}>
-          About Us
-        </h1>
-  
-        <section style={{ marginBottom: '30px' }}>
-          <h2 style={{ fontSize: '28px', fontWeight: 'bold', marginBottom: '15px', color: '#2d3748' }}>Our Mission</h2>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            At Bham SEO & Design Co., our mission is to empower businesses with innovative web design and digital solutions. We are driven by a passion for creativity, technology, and client success.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            What sets us apart is our commitment to tailoring each project to our clients&apos; unique needs. We take the time to understand your business, goals, and target audience to create customized solutions that make a lasting impact.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            We stand out in the industry by consistently staying ahead of design trends and emerging technologies. Our mission is to make your online presence shine and help your business thrive in the digital landscape.
-          </p>
-        </section>
-  
-        <h1 style={{ fontSize: '36px', fontWeight: 'bold', textAlign: 'center', marginBottom: '30px', color: '#1a202c' }}>
-          FAQ
-        </h1>
-  
-        <section style={{ marginBottom: '30px' }}>
-          <h2 style={{ fontSize: '28px', fontWeight: 'bold', marginBottom: '15px', color: '#2d3748' }}>Website Creation</h2>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>How much will it cost to build a website for my business?</strong><br />
-            The cost varies based on complexity. Basic websites start at a few hundred dollars, while complex ones may cost more.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>What is the process of designing and developing a website?</strong><br />
-            The process involves planning, design, development, content creation, testing, launch, and ongoing maintenance.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>What features and functionalities does my website need to meet my business needs?</strong><br />
-            Consider responsive design, easy navigation, a content management system, contact info, SEO, e-commerce, security, and analytics.
-          </p>
-        </section>
-  
-        <section style={{ marginBottom: '30px' }}>
-          <h2 style={{ fontSize: '28px', fontWeight: 'bold', marginBottom: '15px', color: '#2d3748' }}>SEO (Search Engine Optimization)</h2>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>How can you improve my website&apos;s search engine ranking?</strong><br />
-            We focus on optimizing your website&apos;s content, meta tags, and acquiring quality backlinks.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>What kinds of keywords will you target to attract my target audience?</strong><br />
-            We identify keywords relevant to your business and audience using keyword research tools.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>Is on-page SEO or off-page SEO more important for my website?</strong><br />
-            Both are important; on-page SEO involves optimizing your website&apos;s content, while off-page SEO involves building backlinks.
-          </p>
-          <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#4a5568', marginBottom: '20px' }}>
-            <strong>What SEO tools and strategies are effective for my industry?</strong><br />
-            We use industry-specific SEO tools and strategies to target your audience effectively.
-          </p>
-        </section>
-      </div>
+
+<div className="apply-container bg-gray-100 text-gray-800 py-16">
+  <div className="max-w-6xl mx-auto">
+
+    {/* ABOUT US TITLE SECTION */}
+    <h1 className="text-5xl lg:text-6xl font-bold text-center mb-10">About Us</h1>
+
+<div className="about-section grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
+<div className="text-center lg:text-left">
+
+{/* OUR MISSION SECTION */}
+<h2 className="text-3xl lg:text-4xl font-semibold mb-4 text-indigo-600">Our Mission</h2>
+<p className="text-xl mb-8 leading-relaxed">
+At Bham SEO & Design Co., our mission is to empower businesses with innovative web design and digital solutions. We are driven by a passion for creativity, technology, and client success.
+</p>
+<p className="text-xl mb-8 leading-relaxed">
+What sets us apart is our commitment to tailoring each project to our clients' unique needs. We take the time to understand your business, goals, and target audience to create customized solutions that make a lasting impact. We stand out in the industry by consistently staying ahead of design trends and emerging technologies. Our mission is to make your online presence shine and help your business thrive in the digital landscape.
+</p>
+</div>
+
+{/* TEAMWORK IMAGE */}
+<div>
+<img src="/images/teammmmmy.png" alt="Team Photo" className="rounded-lg shadow-lg" />
+</div>
+</div>
+
+{/* FAQ SECTION */}
+<div className="container mx-auto px-6 py-8 bg-white">
+<div className="max-w-6xl mx-auto text-gray-800">
+<h1 className="text-5xl lg:text-6xl font-bold text-center mb-10 underline decoration-blue-500 decoration-4 underline-offset-8">FAQ</h1>
+
+{/* WEB DESIGN SECTION */}
+<h2 className="text-4xl font-extrabold text-indigo-600 mt-12 mb-8">Web Design</h2>
+
+ {/* FIRST QUESTION - HOW MUCH WEBSITE COST */} 
+ <div class="custom-category-card">
+<span class="custom-category-title">How much will it cost to build a website for my business?</span>
+<div class="custom-category-description">
+The cost varies based on complexity. Basic websites start at a few hundred dollars, while complex ones may cost more.
+</div>
+</div>
+
+{/* QUESTION TWO - WHAT IS PROCESS OF BUILDING WEBSITE*/}
+<div class="custom-category-card">
+<span class="custom-category-title">What is the process of designing and developing a website?</span>
+<div class="custom-category-description">
+The process involves planning, design, development, content creation, testing, launch, and ongoing maintenance.
+</div>
+</div>
+
+{/* QUESTION THREE - WHAT FEATURES AND FUNCTIONALITIES */}
+<div class="custom-category-card">
+<span class="custom-category-title">What features and functionalities does my website need to meet my business needs?</span>
+<div class="custom-category-description">
+Consider responsive design, easy navigation, a content management system, contact info, SEO, e-commerce, security, and analytics.
+</div>
+</div>
+
+{/* QUESTION FOUR - HOW LONG DOES IT TAKE */}
+<div class="custom-category-card">
+<span class="custom-category-title">How Long Will It Take to Build the Website?</span>
+<div class="custom-category-description">
+A standard website typically takes between 4-6 weeks to complete, depending on the complexity and our current workload. For more complex projects, it could take longer. We'll give you a more accurate timeline once we've discussed your requirements.
+</div>
+</div>
+
+{/* QUESTION FIVE - WEBSITE MOBILE FRIENDLY */}
+<div class="custom-category-card">
+<span class="custom-category-title">Will My Website Be Mobile-Friendly?</span>
+<div class="custom-category-description">
+Absolutely, all our websites are designed to be responsive and mobile-friendly. This ensures a seamless experience for users on various devices.
+</div>
+</div>
+
+{/* QUESTION SIX - IMPROVE EXISTING WEBSITE */}
+<div class="custom-category-card">
+<span class="custom-category-title">Can You Improve My Existing Website?</span>
+<div class="custom-category-description">
+Yes, we can work with your existing website for redesigns, functionality improvements, or content updates. We'll first assess your current site to recommend the best course of action.
+</div>
+</div>
+
+
+{/* QUESTION SEVEN - SOCIAL MEDIA ON WEBSITE */}
+<div class="custom-category-card">
+<span class="custom-category-title">Can You Integrate Social Media into My Website?</span>
+<div class="custom-category-description">
+Yes, we can integrate your social media profiles with your website and make your content easily shareable across social platforms.
+</div>
+</div>
+
+{/* QUESTION EIGHT - WEBSITE MATIENCE */}
+<div class="custom-category-card">
+<span class="custom-category-title">How Do We Handle Website Maintenance?</span>
+<div class="custom-category-description">
+We offer ongoing maintenance packages to keep your website updated, secure, and performing optimally. These packages can be tailored to your specific needs.
+</div>
+</div>
+
+{/* QUESTION NINE -  DOMAIN REGISTRATION */}
+<div class="custom-category-card">
+<span class="custom-category-title">Can You Help with Domain Registration and Hosting?</span>
+<div class="custom-category-description">
+We can certainly assist with domain registration and hosting services. While these are not included in the standard design package, we can offer them as additional services or guide you through the process.
+</div>
+</div>
+
+{/* QUESTION TEN - UPDATE WEBSITE THEMSELVES */}
+<div class="custom-category-card">
+<span class="custom-category-title">Can I Update the Website Myself?</span>
+<div class="custom-category-description">
+Our websites are built on user-friendly platforms, allowing you to easily make updates and changes. We also provide training and resources to help you manage your site effectively.
+</div>
+</div>
+
+{/* QUESTION ELEVEN - SUPPORT AFTER LAUNCH */}
+<div class="custom-category-card">
+<span class="custom-category-title">What Kind of Support Will I Receive After the Website Goes Live?</span>
+<div class="custom-category-description">
+After your website goes live, we provide a period of post-launch support to address any immediate concerns. We're also available for ongoing support and updates as needed.
+</div>
+</div>
+
+{/* QUESTION TWELVE - USER SUPPORT */}
+<div class="custom-category-card">
+<span class="custom-category-title">How Do You Approach Usability and User Experience?</span>
+<div class="custom-category-description">
+User experience is at the forefront of our design process. We focus on creating intuitive, user-friendly websites that engage and retain visitors.
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+    
     );
   };
   
