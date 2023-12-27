e1d45995ec96a8a63db8255553d7a6a49170ea37
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-27 00:40:48 -0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,20 +5,30 @@ import Image from 'next/image';
 
 const NavBar = () => {
     const router = useRouter();
+    // State to manage the visibility of the mobile menu
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+
+    // Function to toggle the mobile menu open/close
     const toggleMobileMenu = () => {
         setMobileMenuOpen(!mobileMenuOpen);
     };
 
+
+    // Effect to close the mobile menu when navigating to a new route
     useEffect(() => {
         setMobileMenuOpen(false);
     }, [router.asPath]);
 
     return (
         <>
+            {/* Navigation bar container */}
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
+
+                {/* Flex container for navigation content */}
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+                    {/* Logo and company name */}
                     <Link href="/" className="flex items-center">
                         <div className="mr-3">
                             <Image 
@@ -32,10 +42,16 @@ const NavBar = () => {
                             Bham SEO & Design Co.
                         </span>
                     </Link>
+
+                    {/* Mobile menu toggle button */}
                     <button onClick={toggleMobileMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
+
+                        {/* Hamburger icon */}
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
+
+                    {/* Navigation links */}
                     <div className={`${mobileMenuOpen ? "" : "hidden"} w-full md:block md:w-auto focus:outline-none`} id="navbar-default">
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
