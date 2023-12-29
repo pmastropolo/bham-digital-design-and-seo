18935a9ac0c5cd8336d48771c13f41d71167c8d6
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-29 04:24:05 -0600

    Add files via upload

---
+++ pages/privacy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PPrivacyPolicy from '../components/PPrivacyPolicy'
+
+const privacy = () => (
+  <div>
+    <h1>Privacy Policy</h1>
+    <PPrivacyPolicy />
+  </div>
+);
+
+export default privacy;
