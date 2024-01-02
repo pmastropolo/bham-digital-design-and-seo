d8ff1d83b73fb540efc8a7c86118aa1747e361d2
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-01 19:24:22 -0600

    Update Lander.js

--- components/Lander.js
+++ components/Lander.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 
 const landerData = [
-
   {
     "name": "Basic Landing Page Design Package",
     "price": "$300 - $800",
@@ -20,8 +19,6 @@ const landerData = [
     ],
     "idealFor": "Small Businesses or Startups Needing a Simple Online Presence Quickly."
   },
-  
-
   {
     "name": "Enhanced Landing Page Package with Analytics",
     "price": "$800 - $1,500",
@@ -39,7 +36,6 @@ const landerData = [
     ],
     "idealFor": "Businesses Aiming to Generate Leads and Track Visitor Engagement."
   },
-
   {
     "name": "Premium Conversion-Optimized Landing Page Package",
     "price": "$1,500 - $3,000",
@@ -57,9 +53,6 @@ const landerData = [
     ],
     "idealFor": "Marketing Campaigns and Product Launches Requiring Focused User Conversion."
   },
-  
-
-
   {
     "name": "Elite Full-Service Landing Page and Campaign Management",
     "price": "$3,000+",
@@ -76,10 +69,8 @@ const landerData = [
       "Continuous A/B Testing and Analytics Reporting"
     ],
     "idealFor": "Large-Scale Digital Marketing Campaigns Needing Extensive Landing Page Support and Strategic Insight."
-  }  
-
+  }
 ];
-
 
 const Lander = () => {
   return (
@@ -97,7 +88,7 @@ const Lander = () => {
           </div>
   
 <h4 className="text-4xl mt-4 font-semibold text-gray-800">Conversion-Focused <span className="text-orange-500">Landing Page</span> Package</h4>
-<p className="text-md text-gray-700 mt-4">Maximize your marketing efforts with our Conversion-Focused Landing Page Package. Ideal for businesses and marketers, this package is designed to create high-impact landing pages that effectively convert visitors into leads, subscribers, or customers.</p>
+<p className="text-md text-gray-700 mt-4 mb-4">Maximize your marketing efforts with our Conversion-Focused Landing Page Package. Ideal for businesses and marketers, this package is designed to create high-impact landing pages that effectively convert visitors into leads, subscribers, or customers.</p>
 
   
   <div className="custom-category-card-container">
@@ -143,12 +134,12 @@ const Lander = () => {
 
 <div class="custom-category-card">
   <div class="custom-category-icon">📈</div>
-  <span class="custom-category-title">Analytics</span>
+  <span class="custom-category-title">Integrated Analytics</span>
   <div class="custom-category-description">
     Track your landing pages performance with integrated analytics to make data-driven decisions.
   </div>
 </div>
-
+{/* Speed Optimizations Card */}
 <div class="custom-category-card">
   <div class="custom-category-icon">⚡</div>
   <span class="custom-category-title">Speed Optimization</span>
@@ -167,7 +158,7 @@ const Lander = () => {
 
 <div class="custom-category-card">
   <div class="custom-category-icon">📧</div>
-  <span class="custom-category-title">Email Marketing</span>
+  <span class="custom-category-title">E-mail Marketing</span>
   <div class="custom-category-description">
     Nurture leads and engage with visitors through integrated email marketing tools.
   </div>
@@ -198,70 +189,93 @@ const Lander = () => {
 </div>
 </div>
 
+{/* BENEFITS SECTION */}
 <h5 className="text-2xl font-semibold text-gray-800 mt-6">Benefits</h5>
-<p className="text-md text-gray-700 mt-4"><b>Boost Conversions:</b> Increase your conversion rates with focused messaging.</p>
-    <p className="text-md text-gray-700 mt-4"><b>Simplify Your Message:</b> Deliver a clear, distraction-free pitch.</p>
-    <p className="text-md text-gray-700 mt-4"><b>Collect Valuable Data:</b> Gather insights to tailor your marketing.</p>
-    <p className="text-md text-gray-700 mt-4"><b>Enhance Credibility:</b> Build trust and professionalism.</p>
-    <p className="text-md text-gray-700 mt-4"><b>Track Performance:</b> Analyze metrics for informed decisions.</p>
-    <p className="text-md text-gray-700 mt-4"><b>Increase ROI:</b> Maximize returns on your marketing investment.</p>
 
+{/* BENEFIT DESCRIPTIONS */}
+<p className="text-md text-gray-700 mt-4"><b>Boost Conversions</b><br></br>Increase your conversion rates with focused messaging.</p>
+<p className="text-md text-gray-700 mt-4"><b>Simplify Your Message</b><br></br>Deliver a clear, distraction-free pitch.</p>
+<p className="text-md text-gray-700 mt-4"><b>Collect Valuable Data</b><br></br>Gather insights to tailor your marketing.</p>
+<p className="text-md text-gray-700 mt-4"><b>Enhance Credibility</b><br></br>Build trust and professionalism.</p>
+<p className="text-md text-gray-700 mt-4"><b>Track Performance</b><br></br>Analyze metrics for informed decisions.</p>
+<p className="text-md text-gray-700 mt-4"><b>Increase ROI</b><br></br>Maximize returns on your marketing investment.</p>
+
+{/* IDEAL FOR */}
 <h5 className="text-2xl font-semibold text-gray-800 mt-6">Ideal For</h5>
+
 <div className="custom-category-card-container">
-<div class="custom-category-card">
-  <div class="custom-category-icon">🚀</div>
-  <span class="custom-category-title">Startups</span>
-  <div class="custom-category-descriphtion">
+  {/* Startups */}
+  <div className="custom-category-card">
+  <div className="custom-category-icon">🚀</div>
+  <span className="custom-category-title">Startups</span>
+  <div className="custom-category-descriphtion">
+    {/* Description for Startups */}
     Get your new business off the ground quickly with our Basic Landing Page Design Package. Its perfect for startups looking to establish an online presence and capture initial customer interest.
   </div>
 </div>
 
-<div class="custom-category-card">
-  <div class="custom-category-icon">💼</div>
-  <span class="custom-category-title">Small Businesses</span>
-  <div class="custom-category-descrihption">
+{/* Small Businesses */}
+<div className="custom-category-card">
+  <div className="custom-category-icon">💼</div>
+  <span className="custom-category-title">Small Businesses</span>
+  <div className="custom-category-descrihption">
+    {/* Description for Small Businesses */}
     Our Enhanced Landing Page Package with Analytics is tailored for small businesses. Generate leads and track visitor engagement to grow your customer base effectively.
   </div>
 </div>
 
-<div class="custom-category-card">
-  <div class="custom-category-icon">📈</div>
-  <span class="custom-category-title">Marketing Campaigns</span>
-  <div class="custom-category-descrihption">
+{/* Marketing Campaigns */}
+<div className="custom-category-card">
+  <div className="custom-category-icon">📈</div>
+  <span className="custom-category-title">Marketing Campaigns</span>
+  <div className="custom-category-descrihption">
+    {/* Description for Marketing Campaigns */}
     The Premium Conversion-Optimized Landing Page Package is designed for marketing campaigns and product launches. Maximize conversion rates, integrate email marketing, and boost your online presence.
   </div>
 </div>
 
-<div class="custom-category-card">
-  <div class="custom-category-icon">🌐</div>
-  <span class="custom-category-title">Digital Marketing Professionals</span>
-  <div class="custom-category-descrhiption">
+{/* Digital Marketing Professionals */}
+<div className="custom-category-card">
+  <div className="custom-category-icon">🌐</div>
+  <span className="custom-category-title">Digital Marketing Professionals</span>
+  <div className="custom-category-descrhiption">
+    {/* Description for Digital Marketing Professionals */}
     If youre a digital marketing professional handling large-scale campaigns, our Elite Full-Service Landing Page and Campaign Management package is ideal. It offers comprehensive strategies, multiple landing pages, and ongoing support to ensure your marketing efforts shine.
   </div>
 </div>
 </div>
-      
-      
-      
-      <p className="mt-6 text-md text-gray-700">Launch your next campaign with a high-converting landing page. Contact us at <a href="mailto:services@bhamseoanddesignco.com">services@bhamseoanddesignco.com</a>, or click the button below for a detailed consultation.</p>
-      <Link href="/contact" legacyBehavior>
-        <a className="inline-block mt-4 mb-6 bg-gradient-to-r from-orange-600 to-orange-600 hover:from-orange-400 hover:to-orange-600 text-white text-lg py-3 px-8 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-lg">
-          Explore Landing Pages
-        </a>
-      </Link>
-    </div>
-  </div>
+         
+{/* LANDING PAGE INFORMATION */}
+<p className="mt-6 text-md text-gray-700">
+  Launch your next campaign with a high-converting landing page. Contact us at{' '}
+  <a href="mailto:services@bhamseoanddesignco.com">services@bhamseoanddesignco.com</a>, or click the button below for a detailed consultation.
+</p>
 
+<Link href="/contact" legacyBehavior>
+  <a className="inline-block mt-4 mb-6 bg-gradient-to-r from-orange-600 to-orange-600 hover:from-orange-400 hover:to-orange-600 text-white text-lg py-3 px-8 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-lg">
+    Explore Landing Pages
+  </a>
+</Link>
+</div>
+</div>
 
-
-<section></section>
-<h1 className="text-3xl lg:text-5xl font-bold text-center mb-8 mt-10 underline decoration-orange-500 decoration-4 underline-offset-8">
-  Landing Page Package Pricing
+{/* LANDING PAGE PACKAGE PRICING TITLE */}
+<div className="relative py-5 px-6 lg:px-12 bg-gradient-to-br from-orange-500 via-orange-500 to-orange-600 rounded-3xl shadow-xl mx-4 my-10">
+<div className="text-center">
+<h1 className="text-xl lg:text-3xl font-extrabold text-white mb-2 mt-2 tracking-wide leading-tight hover:text-red-800 transition-colors duration-300">
+Landing Page Package Pricing
 </h1>
+<p className="text-md lg:text-lg font-medium text-white mb-2">
+Find the Ideal Plan for Your Business
+</p>
+<span className="inline-block w-16 h-1 bg-orange-100 rounded"></span>
+</div>
+</div>
+
 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-x-4 gap-y-4">
   {landerData.map((pkg, index) => (
     <div key={index} className="bg-white rounded-lg overflow-hidden shadow-lg border border-orange-300 hover:shadow-2xl transition-shadow duration-500 ease-in-out">
-      <div className="flex flex-col h-full"> 
+      <div className="flex flex-col h-full">
         <div className="p-4">
           <h3 className="text-2xl font-semibold mb-1 tracking-wide leading-relaxed text-gray-800 hover:text-orange-400 transition-colors duration-300 ease-in-out">{pkg.name}</h3>
           <p className="text-md mb-1 font-medium text-orange-400">{pkg.price}</p>
@@ -281,7 +295,7 @@ const Lander = () => {
             <p className="bold">Total Pages | {pkg.totalPages}</p>
           </div>
         </div>
-        <div className="bg-orange-500 p-4 text-white mt-auto"> 
+        <div className="bg-orange-500 p-4 text-white mt-auto">
           {pkg.idealFor}
         </div>
       </div>
@@ -289,25 +303,22 @@ const Lander = () => {
   ))}
 </div>
 
-  {/* Contact Information */}
-  <div className="text-center mt-8">
-  <p className="text-2xl text-orange-600 font-semibold">
-    Interested in our Landing Page Web Design Package?
-  </p>
-  <p className="text-lg text-gray-600">
-    Contact us today to request a <span className="text-orange-500">free price quote</span>.
-  </p>
-  <Link href="/contact" legacyBehavior>
-    <a className="inline-block bg-orange-500 hover:bg-orange-800 text-white text-lg py-3 px-12 mb-6 mt-4 rounded-md transition duration-300 transform hover:-translate-y-1 shadow-md">
-      Request a Free Price Quote
-    </a>
-  </Link>
-
-          </div>
-        </div>
-
+{/* CONTACT INFORMATION */}
+<div className="text-center mt-8">
+<p className="text-2xl text-orange-600 font-semibold">
+Interested in our Landing Page Web Design Package?
+</p>
+<p className="text-lg text-gray-600">
+Contact us today to request a <span className="text-orange-500">free price quote</span>.
+</p>
+<Link href="/contact" legacyBehavior>
+<a className="inline-block bg-orange-500 hover:bg-orange-800 text-white text-lg py-3 px-12 mb-6 mt-4 rounded-md transition duration-300 transform hover:-translate-y-1 shadow-md">
+Request a Free Price Quote
+</a>
+</Link>
+</div>
+</div>
    );
 };
-
 
 export default Lander;
