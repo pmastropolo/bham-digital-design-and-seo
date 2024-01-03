64e2f09974a88cb0f8be2290a2315c47adc21a6d
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-03 04:49:42 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'standalone',
-}
+  output: 'standalone', // This option doesn't exist in Next.js config.
+  webpack: (config) => {
+    // Configure webpack to handle XML files (sitemap.xml in your case).
+    config.module.rules.push({
+      test: /\.xml$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
