b1dd14a3514e949dca5ab204f74972d1e68e36b3
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-03 05:03:46 -0600

    Update termsandconditions.js

--- components/termsandconditions.js
+++ components/termsandconditions.js
@@ -78,14 +78,11 @@ const TermsandConditions = () => {
     <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
     <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
 </ul>
-
 <p className="text-gray-600 mt-4 mb-4">The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
-
 <p className="text-gray-600 mt-4 mb-5">As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
 </div>
 
 );
-
 };
 
 export default TermsandConditions;
