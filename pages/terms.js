219c5f33c415d3216f0b02b3d13afccf4ed90f5d
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-03 01:00:38 -0600

    Add files via upload

---
+++ pages/terms.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TermsandConditions from '../components/termsandconditions'
+
+const terms = () => (
+  <div>
+    <TermsandConditions />
+  </div>
+);
+
+export default terms;
