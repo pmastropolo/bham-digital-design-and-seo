38fbb0a232c23cae2f8bbefa045f9081a0542513
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-03 03:39:59 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'standalone',
+  
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap.xml',
+      },
+  
+    ];
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
