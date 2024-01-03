9be883195a7a014593575ebee831911e55236aa9
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-03 04:59:17 -0600

    Add files via upload

---
+++ seo.config.js
@@ -0,0 +1,12 @@
+
+export default {
+        title: "Bham SEO and Web Design Co",
+        description: "Bham SEO and Design Co offers expert SEO and web design services to elevate your online presence. Discover innovative strategies tailored for your success.",
+        openGraph: {
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://bhamseoanddesignco.com/',
+          site_name: 'Bham SEO and Web Design Co',
+        },
+
+      };
