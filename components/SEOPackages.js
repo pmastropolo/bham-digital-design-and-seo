97f379f5e27d36935e2118975e7addc861a1e150
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-03 06:02:32 -0600

    Update SEOPackages.js

--- components/SEOPackages.js
+++ components/SEOPackages.js
@@ -4,40 +4,61 @@ import Link from 'next/link';
 const packages = [
         {
           name: 'Basic SEO Package',
-          price: '$499 | monthly',
+          price: '$299 | monthly',
           description: 'Ideal For Small Businesses And Startups Looking To Establish An Online Presence.',
           features: [
-            'Keyword Research | - Identification Of 10 Key Phrases',
-            'On-Page SEO | - Basic Optimization Of Title Tags And Meta Descriptions For Up To 10 Pages',
-            'Content Recommendations | - Basic Guidelines For Content Creation',
-            'Monthly Performance Report | - Overview Of Rankings And Traffic'
+            'Keyword Research | 10 Key Phrases',
+            'On-Page SEO Optimize Title Tags & Meta Descriptions | Up to 10 Pages',
+            'Content Recommendations Guidelines for Content Creation',
+            'Performance Report Rankings & Traffic Overview',
+            'Local SEO Optimization Boost Local Visibility',
+            'Site Speed Optimization Faster Loading Times'
           ]
         },
         {
           name: 'Standard SEO Package',
-          price: '$999 | monthly',
+          price: '$799 | monthly',
           description: 'Perfect for growing businesses aiming to enhance their online visibility and reach.',
           features: [
-            'Advanced Keyword Research | - Up To 25 Key Phrases',
-            'Comprehensive On-Page SEO | - Optimization For Up To 25 Pages',
-            'Technical SEO Audit | - Basic Site Audit And Recommendations',
-            'Content Strategy | - Creation Of A Detailed Content Plan',
-            'Backlink Analysis | - Basic Analysis And Strategy For Link Building',
-            'Bi-Weekly Performance Reports'
-          ]
+            'Everything In Basic SEO Package',
+            'Advanced Keyword Research | Up To 25 Key Phrases',
+            'Advanced Technical SEO | In-Depth Site Optimizations',
+            'Local SEO Citations | Business Listing On Local Directories',
+            'Comprehensive On-Page SEO | Optimization For 25 Pages',
+            'Advanced Content Recommendations | Detailed Content Strategy',
+            'Technical SEO Audit | Site Audit And Recommendations',
+            'Content Strategy | Creation Of A Detailed Content Plan',
+            'Backlink Analysis | Analysis And Strategy For Link Building',
+            'Bi-Weekly Performance Reports',
+            'Advanced Technical SEO | In-Depth Technical Optimizations',
+            'Conversion Rate Optimization (CRO) | Improve Conversions',
+            'Local SEO Citations | Business Listing On Local Directories',
+            'Advanced Content Recommendations | Detailed Content Strategy',
+            'Structured Data Markup | Schema Markup For Rich Search Results',
+            'Monthly Competitor Reports | Monitor Competitors And Adjust Strategies',
+            'Social Media Marketing | Manage And Promote Social Media Profiles',
+          ],
         },
         {
           name: 'Premium SEO Package',
-          price: '$1,999 | monthly',
+          price: '$1,600 | monthly',
           description: 'Designed for Established Businesses Targeting Aggressive Growth and Market Leadership.',
           features: [
-            'Expert Keyword Research | - Unlimited Key Phrases',
-            'Full On-Page SEO Optimization | - For All Website Pages',
-            'In-Depth Technical SEO Audit | - Including Performance Optimization',
-            'Advanced Content Strategy | - Including Content Creation For Blogs And Landing Pages',
-            'Robust Backlink Strategy | - Comprehensive Analysis And Active Link Building',
-            'Weekly Performance Reports | - Detailed Insights With Action Plans',
-            'Dedicated SEO Account Manager | - Personalized Consultancy And Support'
+            'Everything In Basic And Starter Package',
+            'Expert Keyword Research | Unlimited Key Phrases',
+            'Full On-Page SEO Optimization | For All Website Pages',
+            'In-Depth Technical SEO Audit | Including Performance Optimization',
+            'Advanced Content Strategy | Including Content Creation For Blogs And Landing Pages',
+            'Robust Backlink Strategy | Comprehensive Analysis And Active Link Building',
+            'Weekly Performance Reports | Detailed Insights With Action Plans',
+            'Dedicated SEO Account Manager | Personalized Consultancy And Support',
+            'International SEO | Expand Reach To International Markets (if applicable)',
+            'E-commerce SEO | Optimize Product Pages And Shopping Carts',
+            'Advanced Technical Audit | Comprehensive Site Audit And Performance Optimization',
+            'Custom Link Building | Develop High-Quality Backlinks Through Outreach And Partnerships',
+            'Custom Content Creation | Regular Blog Posts, Infographics, And Multimedia Content',
+            'Advanced Analytics | In-Depth Tracking And Analysis Of User Behavior',
+            'A/B Testing | Implement Split Testing To Improve Conversions',
           ]
         },
         {
@@ -45,55 +66,60 @@ const packages = [
           price: 'Custom Pricing',
           description: 'Tailored for Large Organizations with Complex Websites Needing Bespoke SEO Strategies.',
           features: [
-            'Custom Keyword Research | - Tailored To Business Needs And Industry',
-            'End-To-End On-Page And Off-Page SEO | - Including International SEO If Required',
-            'Deep Technical SEO Optimization | - Focused On Scalability And Efficiency',
-            'Comprehensive Content Strategy | - Covering All Types Of Digital Content',
-            'Advanced Backlink Development | - Using Premium Tools And Resources',
-            'Real-Time Performance Dashboards | - Custom-Built Reports And Analytics',
-            'Dedicated SEO Team | - Including Strategists, Analysts, And Content Creators',
-            'Regular Consultation And Strategy Meetings'
+            'Custom Keyword Research | Tailored To Business Needs And Industry',
+            'Everything In Lower Tier Packages',
+            'End-To-End On-Page And Off-Page SEO | Including International SEO If Required',
+            'Deep Technical SEO Optimization | Focused On Scalability And Efficiency',
+            'Comprehensive Content Strategy | Covering All Types Of Digital Content',
+            'Advanced Backlink Development | Using Premium Tools And Resources',
+            'Real-Time Performance Dashboards | Custom-Built Reports And Analytics',
+            'Dedicated SEO Team | Including Strategists, Analysts, And Content Creators',
+            'Global SEO Strategy | Tailored International SEO Strategies For Multi-Location Businesses',
+            'AI-Powered SEO | Utilize AI and Machine Learning For Advanced Insights',
+            'Advanced Technical Scalability | Scalable Solutions For Complex Websites',
+            'Full-Scale E-commerce Optimization | Advanced Product Optimization And Category Management',
+            'Custom Analytics Dashboards | Track Specific KPIs With Custom Reports',
+            'Multichannel Integration | Integrate SEO Efforts Across Various Marketing Channels',
+            'Reputation Management | Monitor And Manage Online Reviews And Reputation',
+            'Regular Consultation And Strategy Meetings',
           ]
         }
       ];
 
-const SEOPackages = () => {
+      const PackageCard = ({ pkg }) => {
         return (
-            <div className="container mx-auto px-4">
-                {packages.map((pkg, index) => (
-                    <div key={index} className="mb-6 p-4 border rounded-lg">
-                        <h3 className="text-2xl font-semibold mb-1 tracking-wide leading-relaxed text-gray-800 hover:text-blue-600 transition-colors duration-300 ease-in-out">{pkg.name}</h3>
-                        <p className="text-md mb-1 font-medium text-gray-800">{pkg.price}</p>
-                        <hr className="mb-2" />
-                        <p>{pkg.description}</p>
-                        <ul className="list-disc list-inside">
-                            {pkg.features.map((feature, featureIndex) => (
-                                <li key={featureIndex} className="my-2">
-                                    <span className="font-semibold">{feature.split(' | ')[0]}</span>
-                                    <span className="ml-2">{feature.split(' | ')[1]}</span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-
-  {/* Contact Information */}
-<div className="text-center mt-8">
-        <p className="text-2xl text-blue-700 font-semibold">
-          Interested in our SEO Packages?
-        </p>
-        <p className="text-lg text-gray-600">
-          Contact us today to request a <span className="text-blue-700">free price quote</span>.
-        </p>
-        <Link href="/contact" legacyBehavior>
-          <a className="inline-block bg-blue-600 hover:bg-blue-800 text-white text-lg py-3 px-12 mb-6 mt-4 rounded-md transition duration-300 transform hover:-translate-y-1 shadow-md">
-            Request a Free Price Quote
-          </a>
-        </Link>
-      </div>
-
+          <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+            <div className="px-6 py-4">
+              <h3 className="text-2xl font-semibold mb-2 text-gray-800">{pkg.name}</h3>
+              <p className="text-md font-medium text-gray-600 mb-2">{pkg.price}</p>
+              <hr className="my-2 border-t-2 border-gray-300" />
+              <p className="text-gray-700 mb-4">{pkg.description}</p>
             </div>
-        );
-    };
+            <ul className="px-6 py-4">
+              {pkg.features.map((feature, featureIndex) => (
+                <li
+                  key={featureIndex}
+                  className="flex items-center mb-2 text-gray-700"
+                >
 
-export default SEOPackages;
+                  <span className="font-medium">{feature}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      };
+      
+      const SEOPackages = () => {
+        return (
+          <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+            {packages.map((pkg, index) => (
+              <PackageCard key={index} pkg={pkg} />
+            ))}
+        
+          </div>
+       
+        );
+      };
+      
+      export default SEOPackages;
