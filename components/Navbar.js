2600c6a84e2f779c5ecac381a004fc198ad01c5c
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-04 13:34:05 -0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -60,10 +60,6 @@ const NavBar = () => {
                             <li>
                                 <Link href="/PackagePage" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Website Design</Link>
                             </li>
-
-                            <li>
-                                <Link href="/digitalmarketingg" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Digital Marketing</Link>
-                            </li>
                             <li>
                                 <Link href="/seopackagepage" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">SEO Package</Link>
                             </li>
