c85abab2cd97fb2d9bbe01d9fbdb05e5cce23364
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-04 10:40:38 -0600

    Add files via upload

---
+++ pages/billingTerms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PaymentTerms from '../components/PaymentTerms';
+
+const BillingTerms = () => {
+        return (
+          <div>
+            <PaymentTerms />
+          </div>
+        );
+      }
+      
+      export default BillingTerms;
