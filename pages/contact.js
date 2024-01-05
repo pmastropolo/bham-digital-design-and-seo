77580621f7830fc862fb5c36fe5530b694102af7
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-05 12:02:56 -0600

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,51 +1,62 @@
 import React from 'react';
-import ContactForm from '../components/ContactForm';    // Contact Form Component
+import { NextSeo } from 'next-seo';
+import ContactForm from '../components/ContactForm'; // Import Contact Form Component
 
 function Contact() {
-
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <>
+      <NextSeo
+        title="Contact Us | Bham SEO & Design Co."
+        description="Get in touch with Bham SEO & Design Co. for your web design and SEO needs. Contact us today to discuss how we can help grow your business."
+        canonical="https://bhamseoanddesignco.com/contact"
+        openGraph={{
+          url: 'https://bhamseoanddesignco.com/contact',
+          title: 'Contact Us | Bham SEO & Design Co.',
+          description: 'Get in touch with Bham SEO & Design Co. for your web design and SEO needs. Contact us today to discuss how we can help grow your business.',
+        }}
+      />
 
-      {/* Page Header */}
-      <header className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 py-4">
-        <div className="container mx-auto text-white text-center max-w-xl mx-auto">
-          <h1 className="text-4xl font-bold mb-4">Reach Out to Us</h1>
-          <p className="mb-6">Connect with Our Team</p>
-        </div>
-      </header>
+      <div className="bg-gray-100 min-h-screen">
+        {/* Page Header */}
+        <header className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 py-4">
+          <div className="container mx-auto text-white text-center max-w-xl">
+            <h1 className="text-4xl font-bold mb-4">Reach Out to Us</h1>
+            <p className="mb-6">Connect with Our Team</p>
+          </div>
+        </header>
 
-      {/* Contact Form Section */}
-      <section className="container mx-auto py-8">
-        <div className="max-w-3xl mx-auto bg-white p-8 rounded-md shadow-lg">
-          <h2 className="text-2xl font-bold mb-4">Contact Form</h2>
-          <p className="mb-6">
-            Please fill out the form below to get in contact with us. If you dont have information for some lines, you can keep them 
-            <span style={{ textDecoration: 'underline' }}>blank</span>.
-          </p>
+        {/* Contact Form Section */}
+        <section className="container mx-auto py-8">
+          <div className="max-w-3xl mx-auto bg-white p-8 rounded-md shadow-lg">
+            <h2 className="text-2xl font-bold mb-4">Contact Form</h2>
+            <p className="mb-6">
+              Please fill out the form below to get in contact with us. If you dont have information for some lines, you can keep them <span style={{ textDecoration: 'underline' }}>blank</span>.
+            </p>
 
-          <ContactForm />
+            <ContactForm />
 
-          <p className="mt-6 text-gray-500 text-center">
-            We will get back to you as soon as possible.
-          </p>
-        </div>
-      </section>
+            <p className="mt-6 text-gray-500 text-center">
+              We will get back to you as soon as possible.
+            </p>
+          </div>
+        </section>
 
-      {/* Contact Info Section */}
-      <section className="bg-gradient-to-b from-blue-700 via-blue-600 to-blue-500 py-8">
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-semibold text-white mb-4">
-            Reach Us Today
-          </h2>
-          <p className="text-lg text-white mb-2">
-            Email: <a href="mailto:services@bhamseoanddesignco.com" className="text-purple-300 hover:underline">services@bhamseoanddesignco.com</a>
-          </p>
-          <p className="text-lg text-white mb-4">
-            Phone: <a href="tel:(205) 705-7145" className="text-purple-300 hover:underline">(205) 705-7145</a>
-          </p>
-        </div>
-      </section>
-    </div>
+        {/* Contact Info Section */}
+        <section className="bg-gradient-to-b from-blue-700 via-blue-600 to-blue-500 py-8">
+          <div className="container mx-auto text-center">
+            <h2 className="text-3xl font-semibold text-white mb-4">
+              Reach Us Today
+            </h2>
+            <p className="text-lg text-white mb-2">
+              Email: <a href="mailto:services@bhamseoanddesignco.com" className="text-purple-300 hover:underline">services@bhamseoanddesignco.com</a>
+            </p>
+            <p className="text-lg text-white mb-4">
+              Phone: <a href="tel:(205) 705-7145" className="text-purple-300 hover:underline">(205) 705-7145</a>
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
