3a1aad48ead523888bd9960f83f28ab6523d713d
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-05 12:19:29 -0600

    Update seo.config.js

--- seo.config.js
+++ seo.config.js
@@ -1,12 +1,10 @@
-
 export default {
-        title: "Bham SEO and Web Design Co",
-        description: "Bham SEO and Design Co offers expert SEO and web design services to elevate your online presence. Discover innovative strategies tailored for your success.",
-        openGraph: {
-          type: 'website',
-          locale: 'en_IE',
-          url: 'https://bhamseoanddesignco.com/',
-          site_name: 'Bham SEO and Web Design Co',
-        },
-
-      };
+  title: "Bham SEO and Web Design Co",
+  description: "Bham SEO and Design Co offers expert SEO and web design services to elevate your online presence. Discover innovative strategies tailored for your success.",
+  openGraph: {
+    type: 'website',
+    locale: 'en_US', 
+    url: 'https://bhamseoanddesignco.com/',
+    site_name: 'Bham SEO and Web Design Co',
+  },
+};
