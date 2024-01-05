93854415565fc7cf01d2ffb95ee99103c07cb5b9
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-05 11:52:33 -0600

    Update billingTerms.js

--- pages/billingTerms.js
+++ pages/billingTerms.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import PaymentTerms from '../components/PaymentTerms';
 
 const BillingTerms = () => {
-        return (
-          <div>
-            <PaymentTerms />
-          </div>
-        );
-      }
-      
-      export default BillingTerms;
+  return (
+    <>
+      <NextSeo
+        title="Billing Terms | Bham SEO & Design Co."
+        description="Explore the billing terms and conditions at Bham SEO & Design Co. Understand our payment processes and policies in detail."
+        canonical="https://bhamseoanddesignco.com/billingTerms"
+        openGraph={{
+          url: 'https://bhamseoanddesignco.com/billingTerms',
+          title: 'Billing Terms | Bham SEO & Design Co.',
+          description: 'Explore the billing terms and conditions at Bham SEO & Design Co. Understand our payment processes and policies in detail.'
+        }}
+        additionalMetaTags={[
+          {
+            property: 'keywords',
+            content: 'Billing, Payment Terms, Bham SEO & Design Co., Payment Policies, Invoice Terms'
+          },
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1'
+          }
+        ]}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: 'none',
+          maxVideoPreview: -1
+        }}
+      />
+      <div>
+        <PaymentTerms />
+      </div>
+    </>
+  );
+}
+
+export default BillingTerms;
