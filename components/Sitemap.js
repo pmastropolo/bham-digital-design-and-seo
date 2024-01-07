f80db312e009472dc2d6e6d137e25289f07d8986
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-07 11:12:12 -0600

    Update Sitemap.js

--- components/Sitemap.js
+++ components/Sitemap.js
@@ -7,15 +7,18 @@ const Sitemap = () => {
   <h2 className="text-3xl font-semibold text-cyan-400 mb-12 text-center">Website Navigation</h2>
   <ul className="list-none p-0">
     <li className="mb-8">
+        {/* Homepage */}
       <Link href="/" legacyBehavior>
         <a className="text-2xl text-blue-700 hover:text-blue-900 transition duration-300 font-medium">Homepage</a>
       </Link>
     </li>
+    {/* About Us & FAQ */}
     <li className="mb-8">
       <Link href="/apply" legacyBehavior>
         <a className="text-2xl text-blue-700 hover:text-blue-900 transition duration-300 font-medium">About Us & FAQ</a>
       </Link>
     </li>
+    {/* Contact */}
     <li className="mb-8">
       <Link href="/contact" legacyBehavior>
         <a className="text-2xl text-blue-700 hover:text-blue-900 transition duration-300 font-medium">Contact</a>
@@ -23,6 +26,7 @@ const Sitemap = () => {
     </li>
     <h3 className="text-3xl font-semibold text-cyan-400 mb-12 text-center">Website Design & Development</h3>
     <li className="mb-8">
+      {/* Web Design */}
       <Link href="/PackagePage" legacyBehavior>
         <a className="text-2xl text-blue-700 hover:text-blue-900 transition duration-300 font-medium">Web Design</a>
       </Link>
