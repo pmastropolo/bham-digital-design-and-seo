85b7e1fb039662b1121081ae1a473d79168cd876
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-07 10:39:55 -0600

    Update site-map.js

--- pages/site-map.js
+++ pages/site-map.js
@@ -3,7 +3,6 @@ import Sitemap from '../components/Sitemap';
 export default function SiteMapsPage() {
         return (
             <div>
-                <h1>Site Map</h1>
                 <Sitemap />
             </div>
         );
