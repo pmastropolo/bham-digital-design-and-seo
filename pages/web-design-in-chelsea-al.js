bbac31f9241e1b89257a8e811e381f780712bba8
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-07 23:24:44 -0600

    Update web-design-in-chelsea-al.js

--- pages/web-design-in-chelsea-al.js
+++ pages/web-design-in-chelsea-al.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const WebDesignChelsea = () => {
     return (
@@ -15,21 +16,96 @@ const WebDesignChelsea = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Chelsea, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Welcome to Bham Web Design and SEO Co. - your destination for bespoke web design solutions in Chelsea, AL. We specialize in creating websites that are visually appealing, user-friendly, and optimized for search engines.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Our Chelsea Web Design Services?</h2>
-    <p className="text-gray-600">Our team at Bham Web Design and SEO Co. is dedicated to understanding the Chelsea market. We provide custom web design services that align with your business goals and resonate with your local audience in Chelsea.</p>
-  </section>
+            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Chelsea, AL</h2>
+                <p className="text-gray-600 mb-4">Fun Fact | Chelsea used to be called Melrose</p>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Contact Us for Your Web Design Needs</h2>
-    <p className="text-gray-600">Are you ready to start your web design project in Chelsea, AL? Get in touch with Bham Web Design and SEO Co. today, and lets build a website that sets your business apart.</p>
-  </section>
-</div>
+                <section className="mb-4">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Explore Chelsea, AL Heritage</h2>
+                            <p className="text-red-600 mb-4 text-left"><b>County</b> | Shelby</p>
+                            <p className="mb-2 text-left">Named after Chelsea, London, its now a thriving suburban community.</p>
+                            <p className="mb-2 text-left">Known for excellent schools like Chelsea High School and a strong sense of community.</p>
+                            <p className="mb-2 text-left">Offers outdoor activities in scenic landscapes, including hiking trails.</p>
+                            <p className="mb-2 text-left">Frequent community events and festivals, such as Chelsea Fest.</p>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                            <Image
+                                src="/images/ccccccc.jpg"
+                                alt="Image of Chelsea, Alabama"
+                                width={400}
+                                height={300}
+                                className="rounded-lg"
+                            />
+                        </div>
+                    </div>
+                </section>
 
+                <section className="mt-8">
+                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex justify-center items-center">
+                            <Image
+                                src="/images/chelsealabama.jpg"
+                                alt="Best things to do in Alabama"
+                                width={400}
+                                height={300}
+                                className="rounded-lg max-w-sm"
+                            />
+                        </div>
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center">
+                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Best Things to Do in Chelsea, AL</h2>
+                            <div className="mt-4">
+                                <p className="mb-2 text-indigo-500"><b>Oak Mountain State Park</b> | Vast green space with activities for all ages.</p>
+                                <p className="mb-2 text-indigo-500"><b>TreeTop Family Adventure</b> | Fun-filled family amusement center.</p>
+                                <p className="mb-2 text-indigo-500"><b>Cat-n-Bird Winery</b> | Local wine tasting and charming vineyard.</p>
+                                <p className="mb-2 text-indigo-500"><b>Aldridge Gardens</b> | Tranquil woodland garden with diverse birdlife.</p>
+                                <p className="mb-2 text-indigo-500"><b>Oak Mountain Amphitheatre</b> | Premier outdoor music venue.</p>
+                                <p className="mb-2 text-indigo-500"><b>Statue of Liberty Replica</b> | A familiar and iconic sight in nearby Vestavia Hills.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mb-8 mt-2">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Chelsea with Our Web Design Expertise</h2>
+                            <p>In the digital age, a compelling online presence is more than just a luxury. Its a necessity. At Bham Web Design and SEO Co., we understand the heartbeat of Chelsea, AL. Our web design solutions are not just aesthetically pleasing but also strategically crafted to meet the unique needs and goals of your local business.</p>
+                            <p className="mt-4">We blend innovative design with cutting-edge functionality, ensuring your site stands out in a crowded digital landscape. With our finger on the pulse of current trends and timeless design principles, we promise a website that not only looks stunning but also drives growth, engages your audience, and elevates your brand.</p>
+                            <div className="mt-6 gap-8">
+                                <Link href="/contact" legacyBehavior>
+                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
+                                        Contact Us
+                                    </a>
+                                </Link>
+                                {/* Web Design Packages Button with Link */}
+                                <Link href="/PackagePage" legacyBehavior>
+                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
+                                        View Web Design Packages
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                            <Image
+                                src="/images/sweethomealabama.jpg"
+                                alt="SEO Visual"
+                                width={400}
+                                height={300}
+                                className="rounded-lg"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </>
     );
 }
