a1de9ce70cb88c12f30c2c7f2eecb6488b4027c1
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-08 07:52:42 -0600

    Update seo-services-in-wilsonville-al.js

--- pages/seo-services-in-wilsonville-al.js
+++ pages/seo-services-in-wilsonville-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const SEOWilsonville = () => {
     return (
@@ -16,14 +15,26 @@ const SEOWilsonville = () => {
                     type: 'website',
                 }}
             />
-    <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-      <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Wilsonville, AL</h2>
-      <p className="text-gray-500">Looking to boost your online presence in Wilsonville, Alabama? Our SEO services can help!</p>
-      <p className="text-gray-500">We specialize in driving organic traffic to your website by optimizing your content and improving your search engine rankings.</p>
-      <p className="text-gray-500">Our team of experienced professionals will work closely with you to understand your business goals and develop a tailored SEO strategy that aligns with your objectives.</p>
-      <p className="text-gray-500">Contact us today to learn more about how our SEO services can benefit your business in Wilsonville, AL.</p>
-
-    </div>
+            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Wilsonville, AL</h2>
+                <div className="text-center mt-8">
+  <p className="text-4xl mb-4 text-slate-900 font-semibold">
+  Looking to Enhance Your Websites SEO?
+  </p>
+  <p className="text-lg text-gray-600">
+  Discover how our specialized SEO services can elevate your online presence.
+  </p> 
+  <p className="text-lg text-gray-600">
+  Begin with an initial consultation to understand the unique needs of your business.
+  </p>
+  <Link href="/seoConsultation" legacyBehavior>
+    <a className="inline-block bg-slate-600 hover:bg-slate-950 text-white text-lg py-3 px-12 mb-6 mt-4 rounded-md transition duration-300 transform hover:-translate-y-1 shadow-md">
+     Start SEO Form
+    </a>
+  </Link>
+</div>
+        <div className="border-bottom-gray"></div>
+            </div>
         </>
     );
 }
