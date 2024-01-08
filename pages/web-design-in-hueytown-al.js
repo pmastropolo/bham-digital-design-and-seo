0725e695e2fec7788d7f7666702b70c6ae508c96
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-08 15:24:42 -0600

    Update web-design-in-hueytown-al.js

--- pages/web-design-in-hueytown-al.js
+++ pages/web-design-in-hueytown-al.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const WebDesignHueytown = () => {
     return (
@@ -16,20 +17,136 @@ const WebDesignHueytown = () => {
                 }}
             />
 <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Hueytown, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Welcome to Bham Web Design and SEO Co., your trusted source for professional web design services in Hueytown, AL. Our team specializes in creating websites that effectively showcase your brand and cater to your specific Hueytown audience.</p>
+    <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Hueytown, AL</h2>
+    <p className="text-gray-600 mb-4">Fun Fact | Home of the Alabama Gang, a group of famous NASCAR stock car racers.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Us for Web Design in Hueytown?</h2>
-    <p className="text-gray-600">In Hueytown, AL, we understand the importance of a compelling web presence. Our web design solutions are tailored to meet the unique needs and goals of your Hueytown business, ensuring a strong and engaging online presence.</p>
-  </section>
+    <section className="mb-4">
+        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+            {/* Text Section */}
+            <div className="w-full md:w-1/2 text-gray-700">
+                <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Hueytown, AL</h2>
+                <p className="text-slate-600 mb-4 text-left"><b>County</b> | Jefferson</p>
+<p className="mb-2 text-left">Hueytown, part of the Birmingham metro area in Jefferson County, Alabama, developed through heavy industry in the 20th century. Its population was 16,776 as of the 2020 census.</p>
+<p className="mb-2 text-left">Known for the Alabama Gang in NASCAR, Hueytown gained attention in 1992 due to the Hueytown Hum, linked to nearby coal mine ventilation fans.</p>
+<p className="mb-2 text-left">The area has historical roots in farming, steel, and coal mining industries. The Woodward Iron Company, established in 1881, significantly influenced its development.</p>
+<p className="mb-2 text-left">Coal mining began around the 20th century in the Virginia Mines area, now mostly residential, but some original mining buildings remain.</p>
+<p className="mb-2 text-left">Hueytown was incorporated on May 6, 1960, and operates under a Mayor-Council government system. It transitioned to single-member districts in the 1990s.</p>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Contact Us to Get Started</h2>
-    <p className="text-gray-600">If youre ready to enhance your digital footprint in Hueytown, AL, reach out to Bham Web Design and SEO Co. today. Lets discuss your web design project and set you on the path to online success.</p>
-  </section>
-</div>
+                <p className="text-slate-700 mb-4 text-left"><b>Zip Codes</b> | 35023, 35061</p>
+            </div>
+            {/* Image Section */}
+            <div className="w-full md:w-1/2 flex items-center justify-center">
+                <a href="https://www.homes.com/hueytown-al/neighborhood/hueytown/" target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src="https://imagescdn.homes.com/i2/iaoGIRJkoOgMk0X-h1MnIJfmhgbJjzMKyC7SigxxSoc/117/hueytown-hueytown-al-4.jpg?p=1"
+                        alt="Hueytown Alabama Sign"
+                        width={400} 
+                        height={300}
+                        className="rounded-lg max-w-full"
+                    />
+                </a>
+            </div>
+        </div>
+    </section>
 
+    <section className="mt-4">
+    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+        {/* Image Section */}
+        <div className="w-full md:w-1/2 flex justify-center items-center">
+            <a href="https://www.onlyinyourstate.com/alabama/bbq-hotspot-al/" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://img-aws.ehowcdn.com/700x/www.onlyinyourstate.com/wp-content/uploads/2022/01/bob81.jpg"
+                    alt="facebook/Bob Sykes Bar B Q"
+                    width={400}
+                    height={300}
+                    className="rounded-lg max-w-full mr-4"
+                />
+            </a>
+
+            <a href="https://www.homes.com/hueytown-al/neighborhood/hueytown/" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://imagescdn.homes.com/i2/2BBHiJXBT5Wo_GxwRlEag-95IgHuse7M4mQlIYIMuEA/117/hueytown-hueytown-al-11.jpg?p=1"
+                    alt="Hueytown Alabama allison bonnet sign"
+                    width={400}
+                    height={300}
+                    className="rounded-lg max-w-full ml-4"
+                />
+            </a>
+        </div>
+
+{/* Text Section */}
+<div className="w-full md:w-1/2 text-gray-700 text-center mt-4">
+<h2 className="text-4xl font-bold text-gray-800 mb-4 mt-2">Things to Do | Hueytown</h2>
+<div className="mt-4">
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.alabamaadventure.com/">Alabama Adventure & Splash Adventure</a></b><br></br>A theme park offering thrilling rides and water adventures.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="#">High Ore Line Trail</a></b><br></br>Ideal for cycling and walking, connecting to Red Mountain Park and Jones Valley Trail.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="#">Gips Place</a></b><br></br>A popular blues venue in nearby Bessemer for live music and entertainment.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://autobahnspeed.com/">Autobahn Indoor Speedway</a></b><br></br>Indoor go-kart racing and adrenaline-packed sports activities.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.rtjgolf.com/oxmoorvalley/">RTJ at Oxmoor Valley Golf Course</a></b><br></br>A picturesque golf course on former mining land, perfect for golf enthusiasts.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="#">John Wesley Hall Grist Mill</a></b><br></br>Historical gristmill offering insights into early industrial practices.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="#">The Bessemer Hall of History Museum</a></b><br></br>Museum located in an old railway terminal, showcasing regional history.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://redmountainpark.org/">Red Mountain Park</a></b><br></br>One of Americas largest urban parks with hiking, biking, and zip-lining.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="#">Doughboy Monument at DeBardeleben Park</a></b><br></br>Historic monument honoring World War I soldiers, located in Bessemer.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.thebrightstar.com/">Bright Star Restaurant</a></b><br></br>Famous for its Southern home-cooking and seafood in nearby Bessemer.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.tannehill.org/">Tannehill Ironworks Historical State Park</a></b><br></br>A park spread across 1500 acres showcasing Alabamas industrial past.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://cat-n-bird.com/">Cat-n-Bird Winery</a></b><br></br>Boutique winery offering unique wine blends and tastings near Hueytown.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.rickwood.com/">Rickwood Field</a></b><br></br>The oldest professional baseball park in the USA, located northeast of Hueytown.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="#">Allison Bonnett Park</a></b><br></br>A family-friendly park in Downtown Hueytown with recreational facilities.</p>
+<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://bobsykes.com/">Bob Sykes Bar-B-Q</a></b><br></br>Legendary barbecue restaurant in Bessemer known for its authentic Southern dishes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="mb-8 mt-4">
+    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+        {/* Text Section */}
+        <div className="w-full md:w-1/2 text-gray-700">
+            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Hueytown with Our Web Design Expertise</h2>
+            <p>In the digital age, a compelling online presence is more than just a luxury. Its a necessity. At Bham Web Design and SEO Co., we understand the heartbeat of Hueytown, AL. Our web design solutions are not just aesthetically pleasing but also strategically crafted to meet the unique needs and goals of your local business.</p>
+            <p className="mt-4">We blend innovative design with cutting-edge functionality, ensuring your site stands out in a crowded digital landscape. With our finger on the pulse of current trends and timeless design principles, we promise a website that not only looks stunning but also drives growth, engages your audience, and elevates your brand.</p>
+            <div className="mt-6 gap-8">
+                <Link href="/contact" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
+                        Contact Us
+                    </a>
+                </Link>
+                {/* Web Design Packages Button with Link */}
+                <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-6 py-2 px-4 rounded">
+                        View Web Design Packages
+                    </a>
+                </Link>
+            </div>
+        </div>
+        {/* External Image with Caption */}
+        <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
+            <a href="https://www.homes.com/hueytown-al/neighborhood/hueytown/" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://imagescdn.homes.com/i2/J0BKKTVlMdE2sQV8qU_EkFj2aAF2fPXb6xW8AGyffE0/117/hueytown-hueytown-al-14.jpg?p=1"
+                    alt="Bryants seadfood"
+                    width={350}
+                    height={300}
+                    className="rounded-lg max-w-full"
+                />
+            </a>
+            <p className="text-center mt-2 mb-4 text-gray-600">Bryants Seafood</p>
+        </div>
+
+        <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
+            <a href="https://www.homes.com/hueytown-al/neighborhood/hueytown/" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://imagescdn.homes.com/i2/VJ6ellQq24xm5hzHm9r_1bkAk1ZGigBN0YTDugjsxzY/117/hueytown-hueytown-al-22.jpg?p=1"
+                    alt="Hueytown High School"
+                    width={350}
+                    height={300}
+                    className="rounded-lg max-w-full"
+                />
+            </a>
+            <p className="text-center mt-2 mb-4 text-gray-600">Hueytown High School</p>
+        </div>
+                    </div>
+                </section>
+            </div>
         </>
     );
 }
