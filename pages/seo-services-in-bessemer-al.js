2f741e791506468e7dc3ccd221e43a9957f23b78
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-07 22:46:38 -0600

    Update seo-services-in-bessemer-al.js

--- pages/seo-services-in-bessemer-al.js
+++ pages/seo-services-in-bessemer-al.js
@@ -83,7 +83,7 @@ const SEOBessemer = () => {
                         Contact Us
                     </a>
                 </Link>
-                <Link href="/PackagePage" legacyBehavior>
+                <Link href="/seopackagepage" legacyBehavior>
                     <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
                         View SEO Services
                     </a>
