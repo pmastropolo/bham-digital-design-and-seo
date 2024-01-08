492c889c0abe3fb56c53f6e9238976d2d3221b0b
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-08 07:04:28 -0600

    Add files via upload

---
+++ pages/web-design-in-vestavia-hills-al.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import { NextSeo } from 'next-seo';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const WebDesignVestaviaHills = () => {
+     
+        
+                return (
+                    <>
+                        <NextSeo
+                            title="Web Design Services in , AL | Bham Web Design and SEO Co."
+                            description="Bham Web Design and SEO Co. offers top-notch web design services in Vestavia Hills, AL. Explore our customized web solutions designed to elevate your Vestavia Hills business's online presence."
+                            openGraph={{
+                                title: 'Web Design Services in Vestavia Hills, AL | Bham Web Design and SEO Co.',
+                                description: 'Bham Web Design and SEO Co. offers top-notch web design services in Vestavia Hills, AL. Explore our customized web solutions designed to elevate your Vestavia Hills businesss online presence.',
+                                url: 'https://bhamseoanddesignco.com/web-design-vestavia hills-al',
+                                type: 'website',
+                            }}
+                        />
+
+<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Vestavia Hills, AL</h2>
+                <p className="text-gray-600 mb-4">Nickname | The City of Gold</p>
+
+                <section className="mb-4">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Vestavia Hills, AL</h2>
+                            <p className="text-red-600 mb-4 text-left"><b>County</b> | Jefferson</p>
+                            <p className="mb-2 text-left">Rich history of iron ore mining, contributing to the areas growth.</p>
+                            <p className="mb-2 text-left">Officially incorporated as a city on November 8, 1950, through the merger of several communities.</p>
+                            <p className="mb-2 text-left">Named after the historic estate Vestavia, believed to be a combination of Vesta | Roman goddess of the hearth & via meaning way.</p>
+                            <p className="mb-2 text-left">Known for picturesque landscapes, including lush green hills and scenic views.</p>
+                            <p className="mb-2 text-left">Home to highly-rated schools, making it a desirable place for families.</p>
+                            <p className="mb-2 text-left">Offers various recreational opportunities, including parks, hiking trails, and sports facilities.</p>
+                            <p className="mb-2 text-left">Has a diverse local economy with businesses, retail centers, and restaurants.</p>
+                            <p className="mb-2 text-left">Known for strong community involvement and civic engagement with numerous events and activities.</p>
+                            <p className="text-gray-600 mb-4 text-left"><b>Zip Codes</b> | 35216, 35226, 35242, 35243</p>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:Welcome_sign_to_Vestavia_Hills.jpg" target="_blank" rel="noopener noreferrer">
+                        <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Welcome_sign_to_Vestavia_Hills.jpg?20120624021607"
+                alt="Welcome Sign on Montgomery Highway coming into Vestavia Hills Alabama"
+                width={400} 
+                height={300} 
+                className="rounded-lg"
+            />
+            </a>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mt-4">
+                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex justify-center items-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:LibertyParkAl.jpg" target="_blank" rel="noopener noreferrer">
+                        <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/1/10/LibertyParkAl.jpg?20190621014412"
+                alt="Statue of Liberty replica in Vestavia Hills, Alabama."
+                width={400} 
+                height={300} 
+                className="rounded-lg max-w-sm"
+            />
+            </a>
+                        </div>
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center mt-4">
+                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Best Things to Do in Vestavia Hills, AL</h2>
+                            <div className="mt-4">
+                            <p className="mb-2 text-indigo-500"><b>Explore Parks</b> | Visit Wald Park, Vestavia Hills Park, and McCallum Park for outdoor fun.</p>
+<p className="mb-2 text-indigo-500"><b>Hiking Adventures</b> | Hike Red Mountain Parks scenic trails.</p>
+<p className="mb-2 text-indigo-500"><b>Shop</b> | Discover boutiques and dining at Vestavia Hills City Center.</p>
+<p className="mb-2 text-indigo-500"><b>Golf</b> | Tee off at Vestavia Country Club or Vestavia Hills Golf Course.</p>
+<p className="mb-2 text-indigo-500"><b>Culture</b> | Explore art and culture at Vestavia Hills Library in the Forest.</p>
+<p className="mb-2 text-indigo-500"><b>Dine Out</b> | Enjoy diverse cuisine at local restaurants.</p>
+<p className="mb-2 text-indigo-500"><b>Community Events</b> | Attend local festivals and connect with neighbors.</p>
+<p className="mb-2 text-indigo-500"><b>Fitness</b> | Stay active at fitness centers and yoga studios.</p>
+<p className="mb-2 text-indigo-500"><b>Altadena Valley Animal Clinic</b> | Visit for animal encounters and programs.</p>
+<p className="mb-2 text-indigo-500"><b>Relax and Enjoy Views</b> | Take in scenic beauty of the hills and valleys.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mb-8 mt-4">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Vestavia Hills with Our Web Design Expertise</h2>
+                            <p>In the digital age, a compelling online presence is more than just a luxury. Its a necessity. At Bham Web Design and SEO Co., we understand the heartbeat of Vestavia Hills, AL. Our web design solutions are not just aesthetically pleasing but also strategically crafted to meet the unique needs and goals of your local business.</p>
+                            <p className="mt-4">We blend innovative design with cutting-edge functionality, ensuring your site stands out in a crowded digital landscape. With our finger on the pulse of current trends and timeless design principles, we promise a website that not only looks stunning but also drives growth, engages your audience, and elevates your brand.</p>
+                            <div className="mt-6 gap-8">
+                                <Link href="/contact" legacyBehavior>
+                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
+                                        Contact Us
+                                    </a>
+                                </Link>
+
+                                {/* Web Design Packages Button with Link */}
+                                <Link href="/PackagePage" legacyBehavior>
+                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
+                                        View Web Design Packages
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
+                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
+                width={400} 
+                height={300} 
+                className="rounded-lg"
+            />
+              </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+
+
+
+              <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+              <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Vestavia Hills, AL</h2>
+              <p className="text-gray-600 mb-4">Nickname | The Jewel City</p>
+
+             
+            </div>
+            
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+    );
+}
+
+export default WebDesignVestaviaHills;
