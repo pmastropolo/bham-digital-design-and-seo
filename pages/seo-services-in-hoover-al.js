4385ddbd0d9ec1141ecbd50ca50850c187f03fa0
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-08 12:37:41 -0600

    Update seo-services-in-hoover-al.js

--- pages/seo-services-in-hoover-al.js
+++ pages/seo-services-in-hoover-al.js
@@ -1,24 +1,151 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const SEOHoover = () => {
     return (
         <>
             <NextSeo
-                title="SEO Services in Hoover, AL | YourCompanyName"
+                title="SEO Services in Hoover, AL | Bham Web Design and SEO Co."
                 description="YourCompanyName offers top-notch SEO services in Hoover, AL. Explore our customized SEO solutions designed to elevate your Hoover business's online presence."
                 openGraph={{
-                    title: 'SEO Services in Hoover, AL | YourCompanyName',
+                    title: 'SEO Services in Hoover, AL | Bham Web Design and SEO Co.',
                     description: 'YourCompanyName offers top-notch SEO services in Hoover, AL. Explore our customized SEO solutions designed to elevate your Hoover businesss online presence.',
-                    url: 'https://yourcompanywebsite.com/seo-services-hoover-al',
+                    url: 'https://bhamseoanddesignco.com/seo-services-hoover-al',
                     type: 'website',
                 }}
             />
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+
+<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
                 <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Hoover, AL</h2>
-                {/* Add your SEO services content for Hoover here */}
+                <p className="text-gray-600 mb-4">Fun Fact | Named after William H. Hoover</p>
+
+                <section className="mb-4">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Hoover, AL</h2>
+                            <p className="text-slate-600 mb-4 text-left"><b>County</b> | Shelby</p>
+                            <p className="mb-2 text-left">Incorporated in 1967 | Rapid Suburban Growth</p>
+                            <p className="mb-2 text-left">Notable Hoover City School System</p>
+                            <p className="mb-2 text-left">Scenic Foothills Location | Abundance of Parks</p>
+                            <p className="mb-2 text-left">Diverse and vibrant community | Active Community Involvement | Annual Hoover Hayride & More</p>
+                            <p className="mb-2 text-left">Strategic economic hub</p>
+                            <p className="text-slate-700 mb-4 text-left"><b>Zip Codes</b> | 35216, 35226, 35236, 35244</p>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:Hoover_Board_of_Education.jpg" target="_blank" rel="noopener noreferrer">
+                        <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hoover_Board_of_Education.jpg/1200px-Hoover_Board_of_Education.jpg?20160622133629"
+                alt="Farr Administration Building/Hoover Board of Education. Main administrative building for Hoover City Schools."
+                width={400} 
+                height={300} 
+                className="rounded-lg max-w-sm"
+            />
+            </a>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mt-4">
+    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+        {/* Image Section */}
+        <div className="w-full md:w-1/2 flex justify-center items-center">
+            <a href="https://commons.wikimedia.org/wiki/File:Moss_Rock_Preserve_2.jpg" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Moss_Rock_Preserve_2.jpg/675px-Moss_Rock_Preserve_2.jpg?20160803134717"
+                    alt="Waterfall at Moss Rock Preserve in Hoover, AL"
+                    width={400}
+                    height={300}
+                    className="rounded-lg max-w-full mr-4"
+                />
+            </a>
+
+            <a href="https://commons.wikimedia.org/wiki/File:Moss_Rock_Preserve_sign.jpg" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Moss_Rock_Preserve_sign.jpg/675px-Moss_Rock_Preserve_sign.jpg?20160803134718"
+                    alt="Sign at Moss Rock Preserve in Hoover, AL"
+                    width={400}
+                    height={300}
+                    className="rounded-lg max-w-full ml-4"
+                />
+            </a>
+        </div>
+        
+
+{/* Text Section */}
+<div className="w-full md:w-1/2 text-gray-700 text-right mt-4">
+<h2 className="text-4xl font-bold text-gray-800 mb-4 mt-4">Things to Do | Hoover</h2>
+<div className="mt-4">
+
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="https://www.riverchasegalleria.com/en.html">Riverchase Galleria</a></b> | Largest Shopping Mall in Alabama.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="https://aldridgegardens.com/">Aldridge Gardens</a></b> | Beautiful Botanical Gardens.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="https://hoovermetcomplex.com/">The Hoover Met Complex</a></b> | Sports Venue.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="https://www.cityofalabaster.com/facilities/facility/details/Veterans-Park-10">Veterans Park</a></b> | Scenic Park With Trails & A Lake.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="http://www.hooveral.org/214/Moss-Rock-Preserve">Moss Rock Preserve</a></b> | Nature Preserve for Hiking.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="https://www.hooverlibrary.org/">Hoover Public Library</a></b> | Modern Library & Cultural Events.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="https://www.stardome.com/">Comedy Club Stardome</a></b> | Comedy Shows & Performances.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="http://www.hooveral.org/660/Georgetown-Park">Georgetown Park</a></b> | 8 Acre Park With A Lake.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="http://www.hooveral.org/1097/EXPLORE-Playground-Splashpad">EXPLORE Playground & Splashpad</a></b> | Free of Charge For All Ages.</p>
+<p className="mb-2 text-indigo-500"><b className="underline text-lg"><a href="http://www.hooveral.org/661/Hoover-Lake-House">Hoover Lake House</a></b> | Available for Rent.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mb-8 mt-4">
+    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+        {/* Text Section */}
+        <div className="w-full md:w-1/2 text-gray-700">
+            <h2 className="text-3xl font-bold text-gray-800 mb-4">Enhance Your Hoover Business with Our SEO Expertise</h2>
+            <p>In the digital age, a robust online presence is a necessity, not a luxury. At Bham Web Design and SEO Co., we have a deep understanding of Hoover, AL. Our SEO solutions are strategically crafted to meet your local businesss unique needs and goals.</p>
+            <p className="mt-4">We implement innovative strategies to boost your websites visibility and engagement. With our expertise in current trends and timeless SEO principles, we promise to elevate your brand, drive growth, and connect with your audience effectively.</p>
+            <div className="mt-6 gap-8">
+                <Link href="/contact" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
+                        Contact Us
+                    </a>
+                </Link>
+
+                {/* SEO Services Button with Link */}
+                <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
+                        Explore SEO Services
+                    </a>
+                </Link>
             </div>
+        </div>
+
+{/* External Image */}
+<div className="w-full md:w-1/2 flex items-center justify-center">
+    <a href="https://commons.wikimedia.org/wiki/File:Center_Court_of_the_Riverchase_Galleria.jpg" target="_blank" rel="noopener noreferrer">
+        <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Center_Court_of_the_Riverchase_Galleria.jpg?20090517180511"
+            alt="Riverchase Galleria Center Court in Hoover, AL"
+            width={300}
+            height={200}
+            className="rounded-lg max-w-sm"
+        />
+    </a>
+</div>
+
+{/* Local Image */}
+<div className="w-full md:w-1/2 flex items-center justify-center">
+    <Image
+        src="/images/hooverorange.jpg" 
+        alt="Alridge Gardens Orange Flower"
+        width={340}
+        height={300}
+        className="rounded-lg max-w-sm"
+    />
+</div>
+                    </div>
+                </section>
+            </div>
+
         </>
     );
 }
