282cb5c61d6deb663f72909cc7d3868c17b9f5c8
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-08 07:10:18 -0600

    Update web-design-in-wilsonville-al.js

--- pages/web-design-in-wilsonville-al.js
+++ pages/web-design-in-wilsonville-al.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const WebDesignWilsonville = () => {
     return (
@@ -15,10 +16,110 @@ const WebDesignWilsonville = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Wilsonville, AL</h2>
 
-</div>
+    <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Wilsonville, AL</h2>
+                <p className="text-gray-600 mb-4">Wilsonville has six active churches inside the town limits.</p>
+
+                <section className="mb-4">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Wilsonville, AL</h2>
+                            <p className="text-red-600 mb-4 text-left"><b>County</b> | Shelby</p>
+                            <p className="mb-2 text-left">Founded in the 19th century as Cahaba Pump, Wilsonville was later named after its first postmaster, Mortimer Wilson.</p>
+                            <p className="mb-2 text-left">Situated along Lay Lake, Wilsonville offers scenic views and water-based recreational activities.</p>
+                            <p className="mb-2 text-left">With a rich agricultural heritage, farming and forestry have been integral to Wilsonvilles economy.</p>
+                            <p className="mb-2 text-left">Wilsonville Park is a popular spot for outdoor gatherings and events in the community.</p>
+                            <p className="mb-2 text-left">The town hosts various annual events, fostering a sense of community among residents.</p>
+                            <p className="mb-2 text-left">Wilsonvilles small and close-knit community welcomes both residents and newcomers.</p>
+    <p className="mb-2 text-left">Surrounded by natural beauty, including forests and wildlife, the town is ideal for outdoor enthusiasts.</p>
+    <p className="mb-2 text-left">Local businesses play a vital role in Wilsonvilles economy and provide essential services.</p>
+    <p className="mb-2 text-left">Wilsonvilles historical significance as a transportation hub and its proximity to major highways make it accessible to nearby cities.</p>
+                            <p className="text-gray-600 mb-4 text-left"><b>Zip Codes</b> | 35186</p>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
+                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
+                width={400} 
+                height={300} 
+                className="rounded-lg"
+            />
+              </a>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mt-4">
+                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex justify-center items-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
+                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
+                width={400} 
+                height={300} 
+                className="rounded-lg"
+            />
+              </a>
+                        </div>
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center mt-4">
+                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Best Things to Do in Wilsonville, AL</h2>
+                            <div className="mt-4">
+                            <p className="mb-2 text-indigo-500"><b>Explore Lay Lake</b> | Enjoy boating, fishing, and water sports on the scenic Lay Lake.</p>
+    <p className="mb-2 text-indigo-500"><b>Visit Wilsonville Park</b> | Relax, picnic, and enjoy outdoor gatherings at Wilsonville Park.</p>
+    <p className="mb-2 text-indigo-500"><b>Attend Local Events</b> | Check out community events and festivals that bring the town to life.</p>
+    <p className="mb-2 text-indigo-500"><b>Experience Outdoor Adventures</b> | Explore hiking trails, birdwatching, and wildlife in the natural surroundings.</p>
+    <p className="mb-2 text-indigo-500"><b>Discover Wilsonvilles History</b> | Learn about the towns history at local historical sites and museums.</p>
+    <p className="mb-2 text-indigo-500"><b>Support Local Businesses</b> | Shop and dine at local establishments to experience the towns charm.</p>
+    <p className="mb-2 text-indigo-500"><b>Relax by the Lake</b> | Find a peaceful spot by Lay Lake to unwind and take in the serene views.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="mb-8 mt-4">
+                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
+                        {/* Text Section */}
+                        <div className="w-full md:w-1/2 text-gray-700">
+                            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Wilsonville with Our Web Design Expertise</h2>
+                            <p>In the digital age, a compelling online presence is more than just a luxury. Its a necessity. At Bham Web Design and SEO Co., we understand the heartbeat of Wilsonville, AL. Our web design solutions are not just aesthetically pleasing but also strategically crafted to meet the unique needs and goals of your local business.</p>
+                            <p className="mt-4">We blend innovative design with cutting-edge functionality, ensuring your site stands out in a crowded digital landscape. With our finger on the pulse of current trends and timeless design principles, we promise a website that not only looks stunning but also drives growth, engages your audience, and elevates your brand.</p>
+                            <div className="mt-6 gap-8">
+                                <Link href="/contact" legacyBehavior>
+                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
+                                        Contact Us
+                                    </a>
+                                </Link>
+
+                                {/* Web Design Packages Button with Link */}
+                                <Link href="/PackagePage" legacyBehavior>
+                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
+                                        View Web Design Packages
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                        {/* Image Section */}
+                        <div className="w-full md:w-1/2 flex items-center justify-center">
+                        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
+                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
+                width={400} 
+                height={300} 
+                className="rounded-lg"
+            />
+              </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
 
         </>
     );
