0cea256caa131f163ba3ab3ee137a8d6344c8483
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:18:45 -0600

    Update web-design-in-fultondale-al.js

--- pages/web-design-in-fultondale-al.js
+++ pages/web-design-in-fultondale-al.js
@@ -15,20 +15,109 @@ const WebDesignFultondale = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Fultondale, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Looking for exceptional web design in Fultondale, AL? At Bham Web Design and SEO Co., we specialize in creating custom, impactful web solutions that cater to the specific needs of businesses in Fultondale.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Custom Web Design in Fultondale</h2>
-    <p className="text-gray-600">Our approach to web design in Fultondale is centered around delivering tailored solutions that align with your business objectives and target audience. We focus on creating user-friendly, aesthetically pleasing, and SEO-optimized websites.</p>
-  </section>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Fultondale, AL
+    </h1>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Get in Touch for Your Web Design Needs</h2>
-    <p className="text-gray-600">Ready to elevate your online presence in Fultondale, AL? Contact Bham Web Design and SEO Co. today, and lets discuss how we can create a website that drives your business forward.</p>
-  </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Founded in the early 20th century under the name Fulton Springs.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Fultondale, AL</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
+                    <p className="mb-2 text-left"><span className="font-semibold text-indigo-900">Fultondale</span>, with its <span className="font-semibold text-indigo-900">friendly community</span>, is a welcoming city in <span className="font-semibold text-indigo-900">Jefferson County</span>.</p>
+<p className="mb-2 text-left">Founded on <span className="font-semibold text-indigo-900">strong community values</span>, Fultondale has a history rooted in <span className="font-semibold text-indigo-900">unity</span> and <span className="font-semibold text-indigo-900">cooperation</span>.</p>
+<p className="mb-2 text-left">Over the years, Fultondale has grown to become a <span className="font-semibold text-indigo-900">vibrant</span> and <span className="font-semibold text-indigo-900">close-knit city</span> in <span className="font-semibold text-indigo-900">Alabama</span>.</p>
+<p className="mb-2 text-left">With a focus on <span className="font-semibold text-indigo-900">family</span>, Fultondale offers a range of <span className="font-semibold text-indigo-900">recreational activities</span> and <span className="font-semibold text-indigo-900">parks</span> for residents.</p>
+<p className="mb-2 text-left">The citys commitment to <span className="font-semibold text-indigo-900">education</span> is evident through its <span className="font-semibold text-indigo-900">schools</span> and <span className="font-semibold text-indigo-900">community involvement</span>.</p>
+<p className="mb-2 text-left">Fultondale continues to <span className="font-semibold text-indigo-900">develop</span> and <span className="font-semibold text-indigo-900">thrive</span>, making it a great place to <span className="font-semibold text-indigo-900">live</span> and <span className="font-semibold text-indigo-900">do business</span>.</p>
+
+<p className="text-gray-600"><b>County</b> | <span className="font-semibold text-indigo-900">Jefferson</span></p>
+<p className="text-gray-600"><b>Zip Code</b> | <span className="font-semibold text-indigo-900">35068, 35207, 35215, 35217</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Fultondale Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+<div className="flex-1 flex flex-col">
+            <div className="bg-white p-6 rounded-lg shadow-md mb-6 flex-grow">
+            <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Best Things to Do in Fultondale, AL</h2>
+
+            <p className="text-indigo-950 text-lg mb-1"><b>Fultondale Park</b></p>
+<p className="mb-2 text-white text-lg">Enjoy outdoor activities and picnics.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Black Creek Park</b></p>
+<p className="mb-2 text-white text-lg">Scenic trails and nature exploration.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Fultondale Skating Rink</b></p>
+<p className="mb-2 text-white text-lg">Fun roller skating for all ages.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Local Dining</b></p>
+<p className="mb-2 text-white text-lg">Savor delicious local cuisine.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Fultondale Arts Council</b></p>
+<p className="mb-2 text-white text-lg">Explore local art and culture.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Festivals and Events</b></p>
+<p className="mb-2 text-white text-lg">Join community celebrations.</p>
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
