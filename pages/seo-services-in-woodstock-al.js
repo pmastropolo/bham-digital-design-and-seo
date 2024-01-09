7c14a00edc934061aa515c3c084a9b1dd6f0a340
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 17:22:05 -0600

    Update seo-services-in-woodstock-al.js

--- pages/seo-services-in-woodstock-al.js
+++ pages/seo-services-in-woodstock-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const SEOWoodstock = () => {
     return (
@@ -17,107 +16,110 @@ const SEOWoodstock = () => {
                 }}
             />
 
-    <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Woodstock, AL</h2>
-                <p className="text-gray-600 mb-4">Fun Fact | The town of Woodstock was formally named North Bibb until 2000</p>
+  <div className="bg-gradient-to-br from-slate-700 to-indigo-800 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Woodstock, AL
+    </h1>
 
-                <section className="mb-4">
-                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700">
-                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Woodstock, AL</h2>
-                            <p className="text-red-600 mb-4 text-left"><b>County</b> | Bibb</p>
-                            <p className="mb-2 text-left">Woodstock was incorporated as a town in 1996.</p>
-                            <p className="mb-2 text-left">The towns name, Woodstock, is believed to have been inspired by the famous music festival Woodstock that took place in 1969 in New York.</p>
-                            <p className="mb-2 text-left">Woodstock is in Bibb County, part of the Birmingham area.</p>
-                            <p className="mb-2 text-left">Since incorporation, it has steadily grown as a suburban community.</p>
-                            <p className="mb-2 text-left">Woodstock has a diverse local economy, with businesses ranging from retail to manufacturing.</p>
-                            <p className="mb-2 text-left">Known for its strong community spirit and local pride.</p>
-                            <p className="text-gray-600 mb-4 text-left"><b>Zip Code</b> | 35188</p>
-                        </div>
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex items-center justify-center">
-                            <a href="https://commons.wikimedia.org/wiki/File:WoodstockAlabama.jpg" target="_blank" rel="noopener noreferrer">
-                                <Image
-                                    src="https://upload.wikimedia.org/wikipedia/commons/2/2c/WoodstockAlabama.jpg?20091002212557"
-                                    alt="Businesses in Woodstock, Alabama"
-                                    width={400}
-                                    height={300}
-                                    className="rounded-lg"
-                                />
-                            </a>
-                        </div>
-                    </div>
-                </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            The town of Woodstock was Formally Named North Bibb Until 2000
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
 
-                <section className="mt-4">
-                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex justify-center items-center">
-                            <a href="https://commons.wikimedia.org/wiki/File:Red-Tailed_Hawk_Spreading_Wings.jpg" target="_blank" rel="noopener noreferrer">
-                                <Image
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Red-Tailed_Hawk_Spreading_Wings.jpg/732px-Red-Tailed_Hawk_Spreading_Wings.jpg?20200603204128"
-                                    alt=" A Red Tail Hawk spreading its wings"
-                                    width={400}
-                                    height={300}
-                                    className="rounded-lg max-w-sm"
-                                />
-                            </a>
-                        </div>
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center mt-4">
-                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Best Things to Do in Woodstock, AL</h2>
-                            <div className="mt-4">
-                                <p className="mb-2 text-indigo-500"><b>Tannehill Ironworks Historical State Park</b> | Explore Alabamas iron history.</p>
-                                <p className="mb-2 text-indigo-500"><b>Fishing at Buck Creek</b> | Enjoy angling in Woodstock.</p>
-                                <p className="mb-2 text-indigo-500"><b>Downtown Stroll</b> | Discover local shops.</p>
-                                <p className="mb-2 text-indigo-500"><b>Local Dining</b> | Savor Southern cuisine.</p>
-                                <p className="mb-2 text-indigo-500"><b>Attend Local Events</b> | Connect with the community.</p>
-                                <p className="mb-2 text-indigo-500"><b>Bibb County Heritage Museum</b> | Explore regional history.</p>
-                                <p className="mb-2 text-indigo-500"><b>Bird Watching</b> | Observe diverse species.</p>
-                                <p className="mb-2 text-indigo-500"><b>Relax by Buck Creek</b> | Enjoy natures beauty.</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
 
-                <section className="mb-8 mt-4">
-    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-        {/* Text Section */}
-        <div className="w-full md:w-1/2 text-gray-700">
-            <h2 className="text-3xl font-bold text-gray-800 mb-4">Boost Your Online Presence in Woodstock with Our SEO Expertise</h2>
-            <p>In todays digital landscape, having a strong online presence is not a luxury; its a necessity. At Bham Web Design and SEO Co., we understand the essence of Woodstock, ALs online marketplace. Our web design solutions go beyond aesthetics; they are strategically designed to cater to the unique needs and objectives of your local business.</p>
-            <p className="mt-4">We seamlessly integrate innovative design with cutting-edge SEO strategies, ensuring that your website not only looks visually appealing but also excels in the competitive digital environment. With our finger on the pulse of the latest industry trends and timeless SEO principles, we guarantee a website that not only captures attention but also drives growth, engages your target audience, and elevates your brand.</p>
-            <div className="mt-6 gap-8">
-                <Link href="/contact" legacyBehavior>
-                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
-                        Get in Touch
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Woodstock, AL</h2>  
+                    <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-1 transform transition duration-500 hover:shadow-2xl">
+            <p className="mb-4">Woodstock was incorporated as a town in <span className="font-semibold">1996</span>.</p>
+            <p className="mb-4">The towns name, <span className="font-semibold">Woodstock</span>, is believed to have been inspired by the famous music festival Woodstock that took place in <span className="font-semibold">1969</span> in New York.</p>
+            <p className="mb-4">Woodstock is in <span className="font-semibold">Bibb County</span>, part of the Birmingham area.</p>
+            <p className="mb-4">Since incorporation, it has steadily grown as a suburban community.</p>
+            <p className="mb-4 animate-fade-in-up delay-400">Woodstock has a diverse local economy, with businesses ranging from retail to manufacturing.</p>
+            <p className="mb-4 animate-fade-in-up delay-500">Known for its strong community spirit and local pride.</p>
+            <p className="text-gray-600"><b>County</b> | Bibb</p>
+            <p className="text-gray-600"><b>Zip Code</b> | 35188</p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Woodstock Business Online
                     </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+<div className="flex-1 flex flex-col">
+            <div className="bg-white p-6 rounded-lg shadow-md mb-6 flex-grow">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">Best Things to Do in Woodstock, AL</h2>
+
+<p className="text-indigo-500"><b>Tannehill Ironworks Historical State Park</b></p>
+<p className="mb-3 text-indigo-500">Explore Alabamas Iron History</p>
+
+<p className="text-indigo-500"><b>Fishing at Buck Creek</b></p>
+<p className="mb-3 text-indigo-500">Enjoy Angling in Woodstock</p>
+
+<p className="text-indigo-500"><b>Downtown Stroll</b></p>
+<p className="mb-3 text-indigo-500">Discover Local Shops</p>
+
+<p className="text-indigo-500"><b>Local Dining</b></p>  
+<p className="mb-3 text-indigo-500">Savor Southern Cuisine</p>   
+
+<p className="text-indigo-500"><b>Attend Local Events</b></p>
+<p className="mb-3 text-indigo-500">Connect With The Community</p>
+
+<p className="text-indigo-500"><b>Bibb County Heritage Museum</b></p>
+<p className="mb-3 text-indigo-500">Explore Regional History</p>
+
+<p className="text-indigo-500"><b>Bird Watching</b></p>
+<p className="mb-3 text-indigo-500">Observe Diverse Species</p>
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+
+<div className="text-center mt-4">
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
                 </Link>
 
-                                {/* Web Design Packages Button with Link */}
-                                <Link href="/seopackagepage" legacyBehavior>
-                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
-                                        View Web Design Packages
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex items-center justify-center">
-                            <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
-                                <Image
-                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
-                                    alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
-                                    width={400}
-                                    height={300}
-                                    className="rounded-lg"
-                                />
-                            </a>
-                        </div>
-                    </div>
-                </section>
+                {/* Web Design Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
             </div>
+        </div>
+    </div>
+    </div>
         </>
     );
 }
