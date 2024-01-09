d99b98e43d90366e7062715a33efca4465051933
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 16:49:21 -0600

    Update web-design-in-wilsonville-al.js

--- pages/web-design-in-wilsonville-al.js
+++ pages/web-design-in-wilsonville-al.js
@@ -17,10 +17,159 @@ const WebDesignWilsonville = () => {
             />
 
             
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Wilsonville, AL</h2>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Wilsonville, AL
+    </h1>
 
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Wilsonville has six active churches inside the town limits.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Wilsonville</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+
+                    <p className="mb-2 text-left">Founded in the 19th century as Cahaba Pump, Wilsonville was later named after its first postmaster, Mortimer Wilson.</p>
+                            <p className="mb-2 text-left">Situated along Lay Lake, Wilsonville offers scenic views and water-based recreational activities.</p>
+                            <p className="mb-2 text-left">With a rich agricultural heritage, farming and forestry have been integral to Wilsonvilles economy.</p>
+                            <p className="mb-2 text-left">Wilsonville Park is a popular spot for outdoor gatherings and events in the community.</p>
+                            <p className="mb-2 text-left">The town hosts various annual events, fostering a sense of community among residents.</p>
+                            <p className="mb-2 text-left">Wilsonvilles small and close-knit community welcomes both residents and newcomers.</p>
+                            <p className="mb-2 text-left">Surrounded by natural beauty, including forests and wildlife, the town is ideal for outdoor enthusiasts.</p>
+                            <p className="mb-2 text-left">Local businesses play a vital role in Wilsonvilles economy and provide essential services.</p>
+                            <p className="mb-2 text-left">Wilsonvilles historical significance as a transportation hub and its proximity to major highways make it accessible to nearby cities.</p>
+
+  
+<p className="text-gray-600">Countys | <span className="text-indigo-900">Shelby</span></p>
+<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35186</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Wilsonville Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Wilsonville</h2>
+        <p className="text-indigo-950 text-lg mb-1">
+    <strong>Inverness Country Club</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    A private club known for hosting major golf events, offering an elite golf course to its members and guests in a serene, natural setting.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Historic Sites</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Explore the industrial history of Birmingham at Sloss Furnaces National Historic Landmark, Vulcan Park and Museum, and Tannehill Ironworks Historical State Park.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Outdoor Activities</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Enjoy nature parks and trails, visit the Cahaba Beach Dog Park, and engage in outdoor recreational activities such as bass fishing and hiking.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Trampoline Parks and Free Fall Attractions</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Great for adrenaline seekers and families, offering activities like trampoline dodgeball, basketball, and obstacle courses.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Local Events and Festivals</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Attend community events and festivals like the Magic City Wine Fest, the Biggest Networking Event of the Year, and the Inaugural Derby Day Social.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Wine Tasting and Vineyards</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Participate in wine tasting events and visit vineyards in and around Meadowbrook, perfect for wine enthusiasts.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Shopping and Dining</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Explore downtown shopping and services in nearby Vestavia Hills, and enjoy diverse dining options like Capella Pizzeria and local cafes.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Family-Friendly Activities</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Visit family fun centers, attend carnivals and community events, and explore zoos and wildlife areas for a delightful family experience.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Spiritual and Cultural Experiences</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Engage in spiritual and cultural activities in historic churches and participate in local cultural events.
+</p>
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
