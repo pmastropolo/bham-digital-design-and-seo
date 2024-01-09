73b1aaed28fe693881d0331a85ce1935e2c19c24
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:26:02 -0600

    Update serviceareas.js

--- pages/serviceareas.js
+++ pages/serviceareas.js
@@ -3,8 +3,6 @@ import Link from 'next/link';
 
 const ServiceAreas = () => {
         return (
-         
-
             <div>
 <h1 className="text-5xl lg:text-6xl font-bold text-center mt-6 mb-8 underline decoration-emerald-200 decoration-4 underline-offset-8">Web Design Service Areas</h1>
 
@@ -12,214 +10,197 @@ const ServiceAreas = () => {
               <div class="grid-container">
 
   <Link href="/web-design-in-alabaster-al" legacyBehavior>
-    <a className="grid-item">Website Design & Development in Alabaster AL</a>
+    <a className="grid-item">Website Design & Development in <b className="text-blue-500">Alabaster</b> AL</a>
   </Link>
   <Link href="/web-design-in-bessemer-al" legacyBehavior>
-    <a className="grid-item">Website Design & Development in Bessemer AL</a>
+    <a className="grid-item">Website Design & Development in <b className="text-blue-500">Bessemer</b> AL</a>
   </Link>
   <Link href="/web-design-in-birmingham-al" legacyBehavior>
-    <a className="grid-item">Website Design & Development in Birmingham AL</a>
+    <a className="grid-item">Website Design & Development in <b className="text-blue-500">Birmingham</b> AL</a>
   </Link>
-
   <Link href="/web-design-in-chelsea-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Chelsea AL</a>
+          <a className="grid-item">Website Design & Development in <b className="text-blue-500">Chelsea</b> AL</a>
         </Link>
         <Link href="/web-design-in-clanton-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Clanton AL</a>
+          <a className="grid-item">Website Design & Development in <b className="text-blue-500">Clanton</b> AL</a>
         </Link>
         <Link href="/web-design-in-columbiana-al" legacyBehavior>
-          <a className="grid-item">Website site Design & Development in Columbiana AL</a>
+          <a className="grid-item">Website site Design & Development in <b className="text-blue-500">Columbiana</b> AL</a>
         </Link>
         <Link href="/web-design-in-fultondale-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Fultondale AL</a>
+          <a className="grid-item">Website Design & Development in <b className="text-blue-500">Fultondale</b> AL</a>
         </Link>
         <Link href="/web-design-in-gardendale-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Gardendale AL</a>
+          <a className="grid-item">Website Design & Development in <b className="text-blue-500">Gardendale</b> AL</a>
         </Link>
         <Link href="/web-design-in-greystone-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Greystone AL</a>
+          <a className="grid-item">Website Design & Development in <b className="text-blue-500">Greystone</b> AL</a>
         </Link>
 
-
-        <Link href="/web-design-in-helena-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Helena AL</a>
-        </Link>
-        <Link href="/web-design-in-homewood-al" legacyBehavior>
-          <a className="grid-item">Website Design & Development in Homewood AL</a>
-        </Link>
-        <Link href="/web-design-in-hoover-al" legacyBehavior>
-          <a className="grid-item">Web Design & Development in Hoover AL</a>
-        </Link>
-        <Link href="/web-design-in-hueytown-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Hueytown AL</a>
-        </Link>
-        <Link href="/web-design-in-irondale-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Irondale AL</a>
-        </Link>
-        <Link href="/web-design-in-jemison-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Jemison AL</a>
-        </Link>
-
-
-
-
-  <Link href="/web-design-in-leeds-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Leeds AL</a>
-        </Link>
-        <Link href="/web-design-in-mccalla-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in McCalla AL</a>
-        </Link>
-        <Link href="/web-design-in-meadowbrook-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Meadowbrook AL</a>
-        </Link>
-        <Link href="/web-design-in-montevallo-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Montevallo AL</a>
-        </Link>
-        <Link href="/web-design-in-moody-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Moody AL</a>
-        </Link>
-        <Link href="/web-design-in-mountain-brook-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Mountain Brook AL</a>
-        </Link>
-        <Link href="/web-design-in-pelham-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Pelham AL</a>
-        </Link>
-        <Link href="/web-design-in-pinson-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Pinson AL</a>
-        </Link>
-  <Link href="/web-design-in-pleasant-grove-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Pleasant Grove AL</a>
-        </Link>
-        <Link href="/web-design-in-shelby-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Shelby AL</a>
-        </Link>
-        <Link href="/web-design-in-sylacauga-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Sylacauga AL</a>
-        </Link>
-        <Link href="/web-design-in-trussville-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Trussville AL</a>
-        </Link>
-        <Link href="/web-design-in-vestavia-hills-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Vestavia Hills AL</a>
-        </Link>
-        <Link href="/web-design-in-wilsonville-al" legacyBehavior>
-          <a className="grid-item">Web site Design & Development in Wilsonville AL</a>
-        </Link>
-
-
+<Link href="/web-design-in-helena-al" legacyBehavior>
+    <a className="grid-item">Website Design & Development in <b className="text-blue-500">Helena</b> AL</a>
+</Link>
+<Link href="/web-design-in-homewood-al" legacyBehavior>
+    <a className="grid-item">Website Design & Development in <b className="text-blue-500">Homewood</b> AL</a>
+</Link>
+<Link href="/web-design-in-hoover-al" legacyBehavior>
+    <a className="grid-item">Web Design & Development in <b className="text-blue-500">Hoover</b> AL</a>
+</Link>
+<Link href="/web-design-in-hueytown-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Hueytown</b> AL</a>
+</Link>
+<Link href="/web-design-in-irondale-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Irondale</b> AL</a>
+</Link>
+<Link href="/web-design-in-jemison-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Jemison</b> AL</a>
+</Link>
+<Link href="/web-design-in-leeds-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Leeds</b> AL</a>
+</Link>
+<Link href="/web-design-in-mccalla-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">McCalla</b> AL</a>
+</Link>
+<Link href="/web-design-in-meadowbrook-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Meadowbrook</b> AL</a>
+</Link>
+<Link href="/web-design-in-montevallo-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Montevallo</b> AL</a>
+</Link>
+<Link href="/web-design-in-moody-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Moody</b> AL</a>
+</Link>
+<Link href="/web-design-in-mountain-brook-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Mountain Brook</b> AL</a>
+</Link>
+<Link href="/web-design-in-pelham-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Pelham</b> AL</a>
+</Link>
+<Link href="/web-design-in-pinson-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Pinson</b> AL</a>
+</Link>
+<Link href="/web-design-in-pleasant-grove-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Pleasant Grove</b> AL</a>
+</Link>
+<Link href="/web-design-in-shelby-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Shelby</b> AL</a>
+</Link>
+<Link href="/web-design-in-sylacauga-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Sylacauga</b> AL</a>
+</Link>
+<Link href="/web-design-in-trussville-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Trussville</b> AL</a>
+</Link>
+<Link href="/web-design-in-vestavia-hills-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Vestavia Hills</b> AL</a>
+</Link>
+<Link href="/web-design-in-wilsonville-al" legacyBehavior>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Wilsonville</b> AL</a>
+</Link>
   <Link href="/web-design-in-woodstock-al" legacyBehavior>
-    <a className="grid-item">Web site Design & Development in Woodstock AL</a>
+    <a className="grid-item">Web site Design & Development in <b className="text-blue-500">Woodstock</b> AL</a>
   </Link>
 </div>
 </ul>
 
-
-
-
-
-
-
-
 <h1 className="text-5xl lg:text-6xl font-bold text-center mt-6 mb-10 underline decoration-emerald-200 decoration-4 underline-offset-8">SEO | Search Engine Service Areas</h1>
 <div class="grid-container">
+
 <Link href="/seo-services-in-alabaster-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Alabaster AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Alabaster</b> AL</a>
 </Link>
 <Link href="/seo-services-in-bessemer-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Bessemer AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Bessemer</b> AL</a>
 </Link>
 <Link href="/seo-services-in-birmingham-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Birmingham AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Birmingham</b> AL</a>
 </Link>
 <Link href="/seo-services-in-chelsea-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Chelsea AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Chelsea</b> AL</a>
 </Link>
 <Link href="/seo-services-in-clanton-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Clanton AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Clanton</b> AL</a>
 </Link>
 <Link href="/seo-services-in-columbiana-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Columbiana AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Columbiana</b> AL</a>
 </Link>
 <Link href="/seo-services-in-fultondale-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Fultondale AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Fultondale</b> AL</a>
 </Link>
 <Link href="/seo-services-in-gardendale-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Gardendale AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Gardendale</b> AL</a>
 </Link>
 <Link href="/seo-services-in-greystone-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Greystone AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Greystone</b> AL</a>
 </Link>
 <Link href="/seo-services-in-helena-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Helena AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Helena</b> AL</a>
 </Link>
 <Link href="/seo-services-in-homewood-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Homewood AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Homewood</b> AL</a>
 </Link>
 <Link href="/seo-services-in-hoover-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Hoover AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Hoover</b> AL</a>
 </Link>
 <Link href="/seo-services-in-hueytown-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Hueytown AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Hueytown</b> AL</a>
 </Link>
 
-<Link href="/seo-services-in-irondale-al" legacyBehavior >
-  <a className="grid-item">SEO Services in Irondale AL</a>
+<Link href="/seo-services-in-irondale-al" legacyBehavior>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Irondale</b> AL</a>
 </Link>
 <Link href="/seo-services-in-jemison-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Jemison AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Jemison</b> AL</a>
 </Link>
 <Link href="/seo-services-in-leeds-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Leeds AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Leeds</b> AL</a>
 </Link>
 <Link href="/seo-services-in-mccalla-al" legacyBehavior>
-  <a className="grid-item">SEO Services in McCalla AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">McCalla</b> AL</a>
 </Link>
 <Link href="/seo-services-in-meadowbrook-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Meadowbrook AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Meadowbrook</b> AL</a>
 </Link>
 <Link href="/seo-services-in-montevallo-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Montevallo AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Montevallo</b> AL</a>
 </Link>
 <Link href="/seo-services-in-moody-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Moody AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Moody</b> AL</a>
 </Link>
 <Link href="/seo-services-in-mountain-brook-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Mountain Brook AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Mountain Brook</b> AL</a>
 </Link>
 <Link href="/seo-services-in-pelham-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Pelham AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Pelham</b> AL</a>
 </Link>
 <Link href="/seo-services-in-pinson-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Pinson AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Pinson</b> AL</a>
 </Link>
 <Link href="/seo-services-in-pleasant-grove-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Pleasant Grove AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Pleasant Grove</b> AL</a>
 </Link>
 <Link href="/seo-services-in-shelby-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Shelby AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Shelby</b> AL</a>
 </Link>
 <Link href="/seo-services-in-sylacauga-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Sylacauga AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Sylacauga</b> AL</a>
 </Link>
 <Link href="/seo-services-in-trussville-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Trussville AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Trussville</b> AL</a>
 </Link>
 <Link href="/seo-services-in-vestavia-hills-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Vestavia Hills AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Vestavia Hills</b> AL</a>
 </Link>
 <Link href="/seo-services-in-wilsonville-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Wilsonville AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Wilsonville</b> AL</a>
 </Link>
 <Link href="/seo-services-in-woodstock-al" legacyBehavior>
-  <a className="grid-item">SEO Services in Woodstock AL</a>
+    <a className="grid-item">SEO Services in <b className="text-blue-500">Woodstock</b> AL</a>
 </Link>
-
 
 </div>
 
-
-
-            </div>
+          </div>
       
         );
       }
