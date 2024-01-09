427392441a7a5d79b397628f40d839b7ac11729b
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:32:36 -0600

    Update seo-services-in-chelsea-al.js

--- pages/seo-services-in-chelsea-al.js
+++ pages/seo-services-in-chelsea-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const SEOChelsea = () => {
     return (
@@ -16,102 +15,112 @@ const SEOChelsea = () => {
                     type: 'website',
                 }}
             />
-   <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Chelsea, AL</h2>
-                <p className="text-gray-600 mb-4">Fun Fact | Chelsea used to be called Melrose</p>
+    
+  <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Chelsea, AL
+    </h1>
 
-                <section className="mb-4">
-                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700">
-                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Explore Chelsea, AL Heritage</h2>
-                            <p className="text-red-600 mb-4 text-left"><b>County</b> | Shelby</p>
-                            <p className="mb-2 text-left">Named after Chelsea, London, its now a thriving suburban community.</p>
-                            <p className="mb-2 text-left">Known for excellent schools like Chelsea High School and a strong sense of community.</p>
-                            <p className="mb-2 text-left">Offers outdoor activities in scenic landscapes, including hiking trails.</p>
-                            <p className="mb-2 text-left">Frequent community events and festivals, such as Chelsea Fest.</p>
-                            <p className="text-slate-800 mb-4 text-left"><b>Zip Codes</b> | 35043, 35051</p>
-                        </div>
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex items-center justify-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:Chelsea,_Alabama.JPG" target="_blank" rel="noopener noreferrer">
-                            <Image
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Chelsea%2C_Alabama.JPG/1200px-Chelsea%2C_Alabama.JPG?20120225194048"
-                                alt="This is a photograph of Chelsea, Alabama"
-                                width={400}
-                                height={300}
-                                className="rounded-lg"
-                            />
-                            </a>
-                        </div>
-                    </div>
-                </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Chelsea used to be called Melrose
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
 
-                <section className="mt-4">
-                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex justify-center items-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:Chelsea,_Alabama_City_Hall.JPG" target="_blank" rel="noopener noreferrer">
-                            <Image
-                                src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Chelsea%2C_Alabama_City_Hall.JPG"
-                                alt="This is a photograph of the City Hall in Chelsea, Alabama."
-                                width={400}
-                                height={300}
-                                className="rounded-lg max-w-sm"
-                            />
-                            </a>
-                        </div>
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center">
-                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Best Things to Do in Chelsea, AL</h2>
-                            <div className="mt-4">
-                                <p className="mb-2 text-indigo-500"><b>Oak Mountain State Park</b> | Vast green space with activities for all ages.</p>
-                                <p className="mb-2 text-indigo-500"><b>TreeTop Family Adventure</b> | Fun-filled family amusement center.</p>
-                                <p className="mb-2 text-indigo-500"><b>Cat-n-Bird Winery</b> | Local wine tasting and charming vineyard.</p>
-                                <p className="mb-2 text-indigo-500"><b>Aldridge Gardens</b> | Tranquil woodland garden with diverse birdlife.</p>
-                                <p className="mb-2 text-indigo-500"><b>Oak Mountain Amphitheatre</b> | Premier outdoor music venue.</p>
-                                <p className="mb-2 text-indigo-500"><b>Statue of Liberty Replica</b> | A familiar and iconic sight in nearby Vestavia Hills.</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
 
-                <section className="mt-4">
-                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700">
-                            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Chelsea with Our Web Design Expertise</h2>
-                            <p>In todays digital landscape, a strong online presence is crucial for success. At Bham Web Design and SEO Co., we are deeply rooted in the Chelsea, AL community. Our web design and development services are meticulously crafted to maximize the online visibility and performance of your Bessemer-based business.</p>
-                            <p className="mt-4">We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.</p>
-                            <div className="mt-6 gap-8">
-                                <Link href="/contact" legacyBehavior>
-                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
-                                        Contact Us
-                                    </a>
-                                </Link>
-                                {/* Web Design Packages Button with Link */}
-                                <Link href="/seopackagepage" legacyBehavior>
-                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
-                                        View SEO Packages
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex items-center justify-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
-            <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
-                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
-                width={400} 
-                height={300} 
-                className="rounded-lg"
-            />
-              </a>
-                        </div>
-                    </div>
-                </section>
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Chelsea, AL</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
+                    <p className="mb-2 text-left">Named after <span className="font-semibold text-indigo-900">Chelsea</span>, <span className="font-semibold text-indigo-900">London</span>, its now a thriving <span className="text-indigo-500">suburban community</span>.</p>
+<p className="mb-2 text-left">Known for <span className="text-indigo-500">excellent schools</span> like <span className="font-semibold text-indigo-900">Chelsea High School</span> and a <span className="text-indigo-500">strong sense of community</span>.</p>
+<p className="mb-2 text-left">Offers <span className="text-indigo-900">outdoor activities</span> in <span className="text-indigo-500">scenic landscapes</span>, including <span className="text-indigo-900">hiking trails</span>.</p>
+<p className="mb-5 text-left"><span className="text-indigo-500">Frequent community events</span> and <span className="text-indigo-500">festivals</span>, such as <span className="font-semibold text-indigo-900">Chelsea Fest</span>.</p>
+
+                    <p className="text-gray-600"><b>County</b> | <span className="font-semibold text-indigo-800">Shelby</span></p>
+                    <p className="text-gray-600"><b>Zip Code</b> | <span className="font-semibold text-indigo-800">35043, 35051, 35147, 35186</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Chelsea Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+            <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Best Things to Do in Chelsea</h2>
+
+
+            <p className="text-indigo-950 text-lg mb-1">
+<b className="underline"><a href="https://www.alapark.com/parks/oak-mountain-state-park" target="_blank">Oak Mountain State Park</a></b>
+</p>
+<p className="mb-2 text-white text-lg">
+    Vast green space with activities for all ages.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>TreeTop Family Adventure</b></p>
+<p className="mb-2 text-white text-lg">Fun-filled family amusement center.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Cat-n-Bird Winery</b></p>
+<p className="mb-2 text-white text-lg">Local wine tasting and charming vineyard.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Aldridge Gardens</b></p>
+<p className="mb-2 text-white text-lg">Tranquil woodland garden with diverse birdlife.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Oak Mountain Amphitheatre</b></p>
+<p className="mb-2 text-white text-lg">Premier outdoor music venue.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Statue of Liberty Replica</b></p>
+<p className="mb-2 text-white text-lg">A familiar and iconic sight in nearby Vestavia Hills.</p>
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
         </>
     );
 }
