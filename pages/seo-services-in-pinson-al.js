5292a19450932aa3d4dc113c97e032897fa2767b
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 17:06:07 -0600

    Update seo-services-in-pinson-al.js

--- pages/seo-services-in-pinson-al.js
+++ pages/seo-services-in-pinson-al.js
@@ -2,25 +2,119 @@ import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
 
-const SEOPinson = () => {
+const WebDesignPinson = () => {
     return (
         <>
             <NextSeo
-                title="SEO Services in Pinson, AL | Bham Web Design and SEO Co."
-                description="Bham Web Design and SEO Co. offers top-notch SEO services in Pinson, AL. Explore our customized SEO solutions designed to elevate your Pinson business's online presence."
+                title="Web Design Services in Pinson, AL | Bham Web Design and SEO Co."
+                description="Bham Web Design and SEO Co. specializes in web design services in Pinson, AL. Explore our customized web solutions designed to enhance your online presence in Pinson."
                 openGraph={{
-                    title: 'SEO Services in Pinson, AL | Bham Web Design and SEO Co.',
-                    description: 'Bham Web Design and SEO Co. offers top-notch SEO services in Pinson, AL. Explore our customized SEO solutions designed to elevate your Pinson businesss online presence.',
-                    url: 'https://bhamseoanddesignco.com/seo-services-pinson-al',
+                    title: 'Web Design Services in Pinson, AL | Bham Web Design and SEO Co.',
+                    description: 'Bham Web Design and SEO Co. specializes in web design services in Pinson, AL. Explore our customized web solutions designed to enhance your online presence in Pinson.',
+                    url: 'https://bhamseoanddesignco.com/web-design-pinson-al',
                     type: 'website',
                 }}
             />
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Pinson, AL</h2>
-                {/* Add your SEO services content for Pinson here */}
+
+<div className="bg-gradient-to-br from-slate-700 to-indigo-800 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Pinson, AL
+    </h1>
+
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+        Home to One of Alabamas Oldest Caves | Pinson Cave
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Pinson, AL</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
+
+                    <p className="mb-4">Pinson, originally <span className="font-semibold text-indigo-900">Hagoods Crossroads</span>, was settled by General Andrew Jacksons soldiers in the 19th century. The town was renamed to Pinson in <span className="font-semibold text-indigo-900">1837</span>, evolving from its early beginnings in the Creek War.</p>
+<p className="mb-4">During the Civil War, Pinson formed the <span className="font-semibold text-indigo-900">Jefferson Warriors</span>, with a local ironworks contributing to the Confederacy.</p>
+<p className="mb-4">The <span className="font-semibold text-indigo-800">Louisville & Nashville Railroad</span> in 1889 marked Pinsons growth as a coal shipping hub.</p>
+<p className="mb-4">The <span className="font-semibold text-indigo-800">Palmerdale Homestead Community</span> was established in Pinson during the <span className="font-semibold text-indigo-400">Great Depression</span>.</p>
+<p className="mb-4">The <span className="font-semibold text-indigo-800">Turkey Creek Nature Preserve</span> in Pinson protects <span className="font-semibold text-indigo-400">endangered species</span> and highlights natural conservation.</p>
+
+<p className="text-gray-600"><b>County</b> | <span className="font-semibold text-indigo-900">Jefferson</span></p>
+<p className="text-gray-600"><b>Zip Code</b> | <span className="font-semibold text-indigo-900">35216, 35215</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Pinson Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+
+
+<div className="flex-1 flex flex-col">
+            <div className="bg-white p-6 rounded-lg shadow-md mb-6 flex-grow">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-6">Best Things to Do in Pinson, AL</h2>
+
+<p className="text-indigo-500"><b className="underline text-lg"><a href="https://turkeycreeknp.com/">Turkey Creek Nature Preserve</a></b></p>
+<p className="mb-3">A well-maintained natural area with beautiful trails and scenery, ideal for summer visits.</p>
+
+<p className="text-indigo-500"><b className="underline text-lg"><a href="https://alabamabutterbeanfestival.com/">Pinson Butterbean Festival</a></b></p>
+<p className="mb-3">An annual celebration that showcases local heritage and community spirit.</p>
+
+<p className="text-indigo-500"><b className="underline text-lg"><a href="https://www.facebook.com/pinsonmethodist/">Pinson United Methodist Church</a></b></p>
+<p className="mb-3">Another prominent church in Pinson, known for its architecture and community activities.</p>
+
+<p className="text-indigo-500"><b className="underline text-lg"><a href="https://www.facebook.com/p/Pinson-Bicentennial-Park-100082076586471/">Pinson Bicentennial Park</a></b></p>
+<p className="mb-3">A park that provides a tranquil environment for relaxation and recreation.</p>
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+
+
         </>
     );
 }
 
-export default SEOPinson;
+export default WebDesignPinson;
