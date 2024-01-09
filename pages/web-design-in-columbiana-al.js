cf99ee6c18d5f510e7568002e1309b0843db9726
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:18:16 -0600

    Update web-design-in-columbiana-al.js

--- pages/web-design-in-columbiana-al.js
+++ pages/web-design-in-columbiana-al.js
@@ -15,21 +15,110 @@ const WebDesignColumbiana = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Columbiana, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Discover our bespoke web design services for businesses in Columbiana, Alabama. Were dedicated to creating websites that not only look great but also drive results.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Our Columbiana Web Design Services Stand Out</h2>
-    <p className="text-gray-600">In Columbiana, we understand that every business is unique. Thats why our web design approach is tailored to meet your specific business needs, ensuring a digital presence that truly stands out.</p>
-  </section>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Columbiana, AL
+    </h1>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Get in Touch</h2>
-    <p className="text-gray-600">Ready to start your web design project in Columbiana, AL? Contact us today, and let’s discuss how we can help your business shine online.</p>
-  </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Columbiana was first called Columbia.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
 
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Columbiana</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
+
+<p className="mb-2 text-left">First settled around 1821, <span className="font-semibold text-indigo-900">Columbiana, Alabama</span> was incorporated on December 5, 1837. Originally named Columbia, it was renamed to <span className="font-semibold text-indigo-900">Columbiana</span> in 1832.</p>
+<p className="mb-2 text-left">Chosen as the <span className="font-semibold text-indigo-900">county seat</span> of Shelby County in 1826, Columbiana celebrated this decision with notable enthusiasm. Its early justice system was led by Justices of the Peace, including the first Chief Justice, <span className="text-indigo-500">George Phillips</span>.</p>
+<p className="mb-2 text-left">The towns initial courthouse, serving for about 28 years, was also utilized as a school. <span className="text-indigo-500">Joab Lawler</span> notably served as the first Judge of the County Court after its relocation to Columbiana.</p>
+<p className="mb-2 text-left">Historically, the citys economy included <span className="font-semibold text-indigo-900">foundries</span> and saw the inception of newspapers such as <span className="text-indigo-500">The Peoples Advocate</span> and <span className="text-indigo-500">Shelby County Sun</span>.</p>
+<p className="mb-2 text-left">The city is celebrated for its annual <span className="font-semibold text-indigo-900">Liberty Day celebration</span>, started in 1986. Notable landmarks include the <span className="text-indigo-500">Shelby County Museum and Archives</span> and the <span className="text-indigo-500">Karl C. Harrison Museum of George Washington</span>.</p>
+                    
+
+                   
+
+<p className="text-gray-600"><b>County</b> | <span className="font-semibold text-indigo-900">Shelby</span></p>
+<p className="text-gray-600"><b>Zip Code</b> | <span className="font-semibold text-indigo-900">35051</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Columbiana Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+            <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Things To Do in Columbiana</h2>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Shelby County Museum and Archives</b></p>
+<p className="mb-2 text-white text-lg">Explore local history and heritage.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Karl C. Harrison Museum of George Washington</b></p>
+<p className="mb-2 text-white text-lg">Discover artifacts from the first U.S. President.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Beeswax Creek Park</b></p>
+<p className="mb-2 text-white text-lg">Enjoy outdoor activities and nature trails.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Shops of Columbiana</b></p>
+<p className="mb-2 text-white text-lg">Find unique gifts and local crafts.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Scott Vaughn Owen Studio</b></p>
+<p className="mb-2 text-white text-lg">Experience the local art scene.</p>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
         </>
     );
 }
