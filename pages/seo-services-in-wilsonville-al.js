16802e9b6d72c3b07e47fa1608be782015552f8d
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 16:51:30 -0600

    Update seo-services-in-wilsonville-al.js

--- pages/seo-services-in-wilsonville-al.js
+++ pages/seo-services-in-wilsonville-al.js
@@ -15,26 +15,161 @@ const SEOWilsonville = () => {
                     type: 'website',
                 }}
             />
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Wilsonville, AL</h2>
-                <div className="text-center mt-8">
-  <p className="text-4xl mb-4 text-slate-900 font-semibold">
-  Looking to Enhance Your Websites SEO?
-  </p>
-  <p className="text-lg text-gray-600">
-  Discover how our specialized SEO services can elevate your online presence.
-  </p> 
-  <p className="text-lg text-gray-600">
-  Begin with an initial consultation to understand the unique needs of your business.
-  </p>
-  <Link href="/seoConsultation" legacyBehavior>
-    <a className="inline-block bg-slate-600 hover:bg-slate-950 text-white text-lg py-3 px-12 mb-6 mt-4 rounded-md transition duration-300 transform hover:-translate-y-1 shadow-md">
-     Start SEO Form
-    </a>
-  </Link>
+
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Wilsonville, AL
+    </h1>
+
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Wilsonville has six active churches inside the town limits.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
-        <div className="border-bottom-gray"></div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Wilsonville</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+
+                    <p className="mb-2 text-left">Founded in the 19th century as Cahaba Pump, Wilsonville was later named after its first postmaster, Mortimer Wilson.</p>
+                            <p className="mb-2 text-left">Situated along Lay Lake, Wilsonville offers scenic views and water-based recreational activities.</p>
+                            <p className="mb-2 text-left">With a rich agricultural heritage, farming and forestry have been integral to Wilsonvilles economy.</p>
+                            <p className="mb-2 text-left">Wilsonville Park is a popular spot for outdoor gatherings and events in the community.</p>
+                            <p className="mb-2 text-left">The town hosts various annual events, fostering a sense of community among residents.</p>
+                            <p className="mb-2 text-left">Wilsonvilles small and close-knit community welcomes both residents and newcomers.</p>
+                            <p className="mb-2 text-left">Surrounded by natural beauty, including forests and wildlife, the town is ideal for outdoor enthusiasts.</p>
+                            <p className="mb-2 text-left">Local businesses play a vital role in Wilsonvilles economy and provide essential services.</p>
+                            <p className="mb-2 text-left">Wilsonvilles historical significance as a transportation hub and its proximity to major highways make it accessible to nearby cities.</p>
+
+  
+<p className="text-gray-600">Countys | <span className="text-indigo-900">Shelby</span></p>
+<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35186</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Wilsonville Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Wilsonville</h2>
+        <p className="text-indigo-950 text-lg mb-1">
+    <strong>Inverness Country Club</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    A private club known for hosting major golf events, offering an elite golf course to its members and guests in a serene, natural setting.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Historic Sites</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Explore the industrial history of Birmingham at Sloss Furnaces National Historic Landmark, Vulcan Park and Museum, and Tannehill Ironworks Historical State Park.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Outdoor Activities</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Enjoy nature parks and trails, visit the Cahaba Beach Dog Park, and engage in outdoor recreational activities such as bass fishing and hiking.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Trampoline Parks and Free Fall Attractions</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Great for adrenaline seekers and families, offering activities like trampoline dodgeball, basketball, and obstacle courses.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Local Events and Festivals</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Attend community events and festivals like the Magic City Wine Fest, the Biggest Networking Event of the Year, and the Inaugural Derby Day Social.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Wine Tasting and Vineyards</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Participate in wine tasting events and visit vineyards in and around Meadowbrook, perfect for wine enthusiasts.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Shopping and Dining</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Explore downtown shopping and services in nearby Vestavia Hills, and enjoy diverse dining options like Capella Pizzeria and local cafes.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Family-Friendly Activities</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Visit family fun centers, attend carnivals and community events, and explore zoos and wildlife areas for a delightful family experience.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Spiritual and Cultural Experiences</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Engage in spiritual and cultural activities in historic churches and participate in local cultural events.
+</p>
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+    
         </>
     );
 }
