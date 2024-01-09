95a5e92c416cfb5a5a7c0346e18d983e5ac6ac45
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 17:32:13 -0600

    Update web-design-in-leeds-al.js

--- pages/web-design-in-leeds-al.js
+++ pages/web-design-in-leeds-al.js
@@ -15,20 +15,124 @@ const WebDesignLeeds = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Leeds, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Welcome to Bham Web Design and SEO Co., your go-to source for professional web design services in Leeds, AL. We focus on creating websites that effectively showcase your brand and resonate with your Leeds audience.</p>
+ <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Leeds, AL
+    </h1>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Us for Web Design in Leeds?</h2>
-    <p className="text-gray-600">In Leeds, AL, a strong web presence is crucial. Our web design solutions are tailored to meet the unique needs and goals of your Leeds business, ensuring a powerful and engaging online presence.</p>
-  </section>
-
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Ready to Elevate Your Online Presence?</h2>
-    <p className="text-gray-600">If youre ready to enhance your digital footprint in Leeds, AL, contact Bham Web Design and SEO Co. today. Lets discuss your web design project and take the first steps towards online success.</p>
-  </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Leeds is home to the world-famous Barber Motorsports Museum & Racetrack.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Leeds</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+<p className="mb-2 text-left">Incorporated in <span className="text-indigo-800">1887</span>, Leeds, Alabama, was named after the English city and is located in <span className="text-indigo-500">Jefferson County</span>.</p>
+<p className="mb-2 text-left">Leeds growth was driven by the discovery of <span className="text-indigo-800">iron ore</span> and its rail connections to major cities.</p>
+<p className="mb-2 text-left">The city is celebrated for the <span className="text-indigo-800">John Henry folklore</span> and hosts an <span className="text-indigo-500">annual festival</span> in his honor.</p>
+<p className="mb-2 text-left">Leeds 2020 population was <span className="text-indigo-800">12,324</span>, with a diverse demographic profile.</p>
+<p className="mb-2 text-left">Governed by a <span className="text-indigo-800">Mayor-Council system</span>, Leeds is undergoing downtown revitalization projects.</p>
+<p className="mb-2 text-left">Leeds hosts cultural events and has historical sites like the <span className="font-semibold text-indigo-800">Jonathan Bass House Museum</span>.</p>
+<p className="text-gray-600">Countys | <span className="text-indigo-900">Jefferson, St. Clair, & Shelby</span></p>
+<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35004, 35094, 35173</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Leeds Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Leeds</h2>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://leedsalabama.org/rowan-house/" target="_blank">Rowan House Historical Home</a></b></p>
+        <p className="mb-2 text-white text-lg">Historical Site Displaying Furnishings & Stained Glass from 1846.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://www.bamafleamall.com/" target="_blank">Bama Flea Mall & Antiques Center</a></b></p>
+        <p className="mb-2 text-white text-lg">An Unique Shopping Destination.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://www.barbermuseum.org/" target="_blank">Barber Vintage Motorsports Museum</a></b></p>
+        <p className="mb-2 text-white text-lg">Worlds largest collection of motorbikes and rare racing cars.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b>Lee Branch Farmers Market</b></p>
+        <p className="mb-2 text-white text-lg">Open-air market offering local produce and handmade crafts near Lake Purdy.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://www.ruffnermountain.org/" target="_blank">Ruffner Mountain</a></b></p>
+        <p className="mb-2 text-white text-lg">Nature reserve with hiking trails, wildlife watching, and historic mining sites.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b>Lake Purdy</b></p>
+        <p className="mb-2 text-white text-lg">Picturesque reservoir for bird-watching, hiking, and fishing.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://www.shopsofgrandriver.com/" target="_blank">The Outlet Shops of Grand River</a></b></p>
+        <p className="mb-2 text-white text-lg">Shopping mall with designer stores and diverse dining options.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://rustysbarbq.com/" target="_blank">Rustys Bar-B-Q</a></b></p>
+        <p className="mb-2 text-white text-lg">Local diner offering a variety of delicious barbeque dishes.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://www.grandriverdrive-in.com/" target="_blank">Grand River Drive-In Theatre</a></b></p>
+        <p className="mb-2 text-white text-lg">Outdoor movie experience with a mini-golf course and snack bar.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b>Earthborn Pottery Studio</b></p>
+        <p className="mb-2 text-white text-lg">Custom-designed, hand-made pottery studio.</p>
+
+        <p className="text-indigo-950 text-lg mb-1"><b className="underline"><a href="https://stores.basspro.com/us/al/leeds/5000-bass-pro-blvd.html" target="_blank">Bass Pro Shop</a></b></p>
+        <p className="mb-2 text-white text-lg">Outdoor equipment store ideal for fishing & camping enthusiasts.</p>
+
+<p className="text-indigo-950 text-lg"><b className="underline"><a href="https://www.barbermotorsports.com/" target="_blank">Barber Motorsports Park</a></b></p>
+<p className="mb-2 text-white">An 880-acre park with an Indycar and MotoAmerica road course.</p>
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
