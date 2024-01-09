0531ea0e30b6f7366996a794b744b09828af4d4e
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:59:53 -0600

    Update web-design-in-shelby-al.js

--- pages/web-design-in-shelby-al.js
+++ pages/web-design-in-shelby-al.js
@@ -15,10 +15,123 @@ const WebDesignShelby = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Shelby, AL</h2>
- 
+
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Shelby, AL
+    </h1>
+
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Leeds is home to the world-famous Barber Motorsports Museum & Racetrack.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Shelby</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+                        
+
+<p className="mb-4"><span className="font-semibold text-indigo-900">Establishment:</span> Shelby County, Alabama, was established on <span className="font-semibold text-indigo-900">February 7, 1818</span>, named after Isaac Shelby, a hero of the Revolutionary War.</p>
+
+<p className="mb-4">
+<span className="font-semibold text-indigo-900">Demographics:</span> As of the <span className="font-semibold text-indigo-900">2020 Census</span>, the population was <span className="font-semibold text-indigo-900">216,350</span>. The largest city is Alabaster, and the county seat is Columbiana.
+</p>
+
+<p className="mb-4">
+<span className="font-semibold text-indigo-900">Economy:</span> Historically, the countys economy was based on <span className="font-semibold text-indigo-900">agriculture</span> and <span className="font-semibold text-indigo-900">mineral extraction</span>, including cotton farming, iron ore, and coal mining.
+</p>
+
+<p className="mb-4">
+<span className="font-semibold text-indigo-900">Geography:</span> Shelby County is part of the <span className="font-semibold text-indigo-900">Appalachian mountain range</span>, featuring the Coosa and Cahaba rivers. It has diverse terrain suitable for agriculture and pastures.
+</p>
+
+<p className="mb-4">
+<span className="font-semibold text-indigo-900">Transportation:</span> Major transport routes include <span className="font-semibold text-indigo-900">Interstate 65</span> and <span className="font-semibold text-indigo-900">U.S. Highways 31 and 280</span>.
+</p>
+
+<p className="mb-4">
+<span className="font-semibold text-indigo-900">Workforce:</span> The largest employment sectors are <span className="font-semibold text-indigo-900">educational services, healthcare, and retail trade</span>.
+</p>
+<p className="text-gray-600">County | <span className="text-indigo-900">Shelby</span></p>
+<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35051, 35143</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Shelby Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Shelby</h2>
+
+        <p className="text-indigo-950 text-lg mb-1">Cahaba River & Shelby Iron Works Parks</p>
+<p className="mb-2 text-white text-lg">Outdoor exploration and relaxation.</p>
+
+<p className="text-indigo-950 text-lg mb-1">Karl C. Harrison Museum</p>
+<p className="mb-2 text-white text-lg">Historical artifacts from the 18th and 19th centuries.</p>
+
+<p className="text-indigo-950 text-lg mb-1">The American Village</p>
+<p className="mb-2 text-white text-lg">Immersive early American history experience.</p>
+
+<p className="text-indigo-950 text-lg mb-1">Birmingham Bulls Hockey</p>
+<p className="mb-2 text-white text-lg">Exciting local hockey games.</p>
+
+<p className="text-indigo-950 text-lg mb-1">Rusted Roof Barn</p>
+<p className="mb-2 text-white text-lg">Horseback riding in Oak Mountain State Park.</p>
+
+<p className="text-indigo-950 text-lg mb-1">Morgan Creek Winery</p>
+<p className="mb-2 text-white text-lg">Wine tasting in a scenic vineyard.</p>
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
