a3b9bd800c313b93c01f642b4f86edf90ab54e64
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 17:33:57 -0600

    Update web-design-in-montevallo-al.js

--- pages/web-design-in-montevallo-al.js
+++ pages/web-design-in-montevallo-al.js
@@ -15,20 +15,138 @@ const WebDesignMontevallo = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Montevallo, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Welcome to Bham Web Design and SEO Co., your trusted partner for professional web design services in Montevallo, AL. Our team specializes in crafting websites that effectively represent your brand and resonate with your Montevallo audience.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Us for Web Design in Montevallo?</h2>
-    <p className="text-gray-600">In Montevallo, AL, a compelling web presence is essential. Our web design solutions are tailored to meet the unique needs and objectives of your Montevallo business, ensuring a strong and engaging online presence.</p>
-  </section>
+    <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Montevallo, AL
+    </h1>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Ready to Enhance Your Online Presence?</h2>
-    <p className="text-gray-600">If youre ready to enhance your digital footprint in Montevallo, AL, reach out to Bham Web Design and SEO Co. today. Lets discuss your web design project and take the first steps towards digital success.</p>
-  </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Nick Name</span>
+      
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Montevallo</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+                    <p className="mb-2 text-left">
+    Montevallo is Shelby Countys oldest community, established in 1817 as Wilsons Hill and renamed in 1826. The name likely means on a mound in the valley in Italian, reflecting its geographic features.
+</p>
+<p className="mb-2 text-left">
+    The city developed as a farming, mining, and railroad community. Significant growth occurred with the founding of the Alabama Girls Industrial School in 1896, now the University of Montevallo, a key economic and educational influence.
+</p>
+<p className="mb-2 text-left">
+    The University of Montevallo, established for womens education in various fields, evolved into a public liberal arts university, shaping the citys culture and economy.
+</p>
+<p className="mb-2 text-left">
+    Montevallos rich history is preserved in 73 buildings listed on the National Historic Register, including a New Deal-era adorned historic post office.
+</p>
+<p className="mb-2 text-left">
+    The citys population has steadily grown, with a diverse community. The presence of the university contributes to a younger median age compared to the national average.
+</p>
+
+<p className="text-gray-600">Countys | <span className="text-indigo-900">Shelby</span></p>
+<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35115</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Montevallo Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Montevallo</h2>
+
+        <p className="text-indigo-950 text-lg mb-1">
+    <strong>Orr Park</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Visit Orr Park for its unique tree carvings, walking trails, and picnic areas. The park showcases local artist Tim Tingles impressive tree artwork, including the notable Tinglewood carvings.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Shoal Creek Park</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Shoal Creek Park offers fishing, picnicking, and scenic hiking trails. Enjoy its well-stocked fishing lake and explore the natural beauty of the surrounding landscape on various trails.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Alabama Wildlife Center</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    The center is dedicated to the care and rehabilitation of Alabamas native birds. Visitors can volunteer, engage in conservation efforts, and learn about avian ecology.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Main Street Montevallo</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Explore the historic downtown area, with a variety of local shops, restaurants, and cultural events reflecting Montevallos rich heritage.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>University of Montevallo Campus</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Visit the universitys picturesque campus, known for its beautiful architecture, historical buildings, and vibrant student life.
+</p>
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
