df48f50d7683d3974a903fd1ae6b131cb2fd2224
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:57:11 -0600

    Update web-design-in-sylacauga-al.js

--- pages/web-design-in-sylacauga-al.js
+++ pages/web-design-in-sylacauga-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const WebDesignSylacauga = () => {
     return (
@@ -17,40 +16,162 @@ const WebDesignSylacauga = () => {
                 }}
             />
 
-    <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-    <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Sylacauga, AL</h2>
-    <p className="text-gray-600 mb-4">Long known as The Marble City</p>
+<div className="bg-gradient-to-br from-slate-700 to-indigo-800 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Sylacauga, AL
+    </h1>
 
-    <section className="mb-4">
-        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-            {/* Text Section */}
-            <div className="w-full md:w-1/2 text-gray-700">
-                <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Sylacauga, AL</h2>
-                <p className="text-slate-600 mb-4 text-left"><b>County</b> | Talladega</p>
-                <p className="mb-2 text-left">Sylacauga is renowned for its fine white marble bedrock, forming the basis of the areas first industry.</p>
-                <p className="mb-2 text-left">Its the site of the first documented incident of a person being hit by a meteorite in 1954.</p>
-                <p className="mb-2 text-left">The name Sylacauga is derived from Native American words, meaning The Place of the Chalaka Tribe.</p>
-                <p className="mb-2 text-left">Early infrastructure included important roads and a plank road connecting Montgomery to Winterboro through Sylacauga.</p>
-                <p className="mb-2 text-left">The citys first newspaper, the Sylacauga Argus, began publication in 1887.</p>
-                <p className="mb-2 text-left">Nicknamed The Marble City, Sylacaugas marble is acclaimed as the finest in the world, used in several prominent buildings.</p>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Long Known as The Marble City
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
 
-                <p className="text-slate-700 mb-4 text-left"><b>Zip Codes</b> | 35150, 35151</p>
-            </div>
-            {/* Image Section */}
-            <div className="w-full md:w-1/2 flex items-center justify-center">
-                <a href="https://commons.wikimedia.org/wiki/File:Sylacauga_Alabama.JPG" target="_blank" rel="noopener noreferrer">
-                    <Image
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Sylacauga_Alabama.JPG/1199px-Sylacauga_Alabama.JPG?20110219195414"
-                        alt="This is a photograph of Sylacauga, Alabama"
-                        width={400} 
-                        height={300}
-                        className="rounded-lg max-w-full"
-                    />
-                </a>
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Sylacauga, AL</h2>  
+                    <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-1 transform transition duration-500 hover:shadow-2xl">
+                <p className="mb-4">Sylacauga is renowned for its <span className="text-indigo-800">fine white marble bedrock,</span> forming the basis of the areas first industry.</p>
+                <p className="mb-4">Its the site of the first documented incident of a person being hit by a meteorite in <span className="text-indigo-800">1954</span>.</p>
+                <p className="mb-4">The name Sylacauga is derived from Native American words, meaning <span className="text-indigo-800">The Place of the Chalaka Tribe</span>.</p>
+                <p className="mb-4">Early infrastructure included important roads and a plank road connecting <span className="text-indigo-800">Montgomery</span> to <span className="text-indigo-800">Winterboro</span> through Sylacauga.</p>
+                <p className="mb-4">The citys first newspaper, <span className="text-indigo-800">The Sylacauga Argus,</span> began publication in 1887.</p>
+                <p className="mb-4">Nicknamed <span className="text-indigo-800">The Marble City,</span> Sylacaugas marble is acclaimed as the finest in the world, used in several prominent buildings.</p>
+            <p className="text-gray-600"><b>County</b> | Talladega</p>
+            <p className="text-gray-600"><b>Zip Code</b> | 35150, 35151</p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Sylacauga Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
         </div>
-    </section>
 
+
+<div className="flex-1 flex flex-col">
+            <div className="bg-white p-6 rounded-lg shadow-md mb-6 flex-grow">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">Best Things to Do in Sylacauga, AL</h2>
+
+                <p className="text-indigo-950 text-lg mb-1">
+    <strong>FarmLinks Golf Course</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Enjoy a round of golf at this beautifully designed course with manicured fairways and pristine greens. It offers a scenic and challenging experience for golfers.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Comer Museum & Arts Center</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    This museum is a great place to explore local history and culture, featuring exhibits of historical artifacts, photographs, and locally quarried marble sculptures.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Lake Howard</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Ideal for outdoor activities, Lake Howard offers excellent opportunities for fishing, hiking, and mountain biking. Its known for its abundance of bass, crappie, and bream.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Sylacauga Farmers Market</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Open seasonally, this market in Blue Bell Park offers farm-fresh produce, homemade jams, and jellies. A perfect spot for picnic food and local delicacies.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Buttermilk Hill Restaurant</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Set in a historic Victorian house, this restaurant offers Southern-style fine dining with a selection of delicious dishes and desserts.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Morgan Creek Vineyards</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Specializing in Muscadine and fruit wines, this vineyard is ideal for wine tasting and pairing with various cuisines.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Talladega Superspeedway</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Located nearby, its a hub for motorsports enthusiasts, offering exciting NASCAR series races.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Blue Bell Creamery</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Enjoy a delightful factory tour at this creamery, especially recommended for ice cream lovers.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>Sylacauga Speedway</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    Experience the thrill of racing at this local speedway, which hosts various types of races including stock car and dirt track events.
+</p>
+
+<p className="text-indigo-950 text-lg mb-1">
+    <strong>B.B. Comer Memorial Library</strong>
+</p>
+<p className="mb-2 text-white text-lg">
+    A significant historical site in downtown Sylacauga, it houses a wealth of local history and serves as a community hub.
+</p>
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <div className="text-center mt-4">
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 </div>
 
         </>
