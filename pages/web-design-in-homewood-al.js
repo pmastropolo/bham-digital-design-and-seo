d21c6a7026a7227dd202c6668ab03e0abae85ffd
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 17:28:21 -0600

    Update web-design-in-homewood-al.js

--- pages/web-design-in-homewood-al.js
+++ pages/web-design-in-homewood-al.js
@@ -15,21 +15,107 @@ const WebDesignHomewood = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Homewood, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Welcome to Bham Web Design and SEO Co., your partner for exceptional web design services in Homewood, AL. We specialize in crafting websites that represent your brand and resonate with your Homewood audience.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Us for Web Design in Homewood?</h2>
-    <p className="text-gray-600">In Homewood, AL, we understand the significance of a standout web presence. Our web design solutions are tailored to meet the unique demands and goals of your Homewood business, ensuring a powerful and effective online presence.</p>
-  </section>
+             
+<h2 className="text-5xl text-center font-extrabold text-gold-700 mt-8 mb-6">Web Design Services in Homewood, AL</h2>
+<p className="text-silver-700 mb-4 text-center">Fun Fact | The Jacks Family Restaurants chain was founded in Homewood in 1960</p>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Ready to Kickstart Your Web Design Project?</h2>
-    <p className="text-gray-600">If youre looking to enhance your digital footprint in Homewood, AL, Bham Web Design and SEO Co. is here to assist. Contact us today to explore your web design needs and take your online presence to new heights.</p>
-  </section>
+<div className="bg-gradient-to-br from-slate-900 to-indigo-800 text-center py-12 px-6 shadow-xl rounded-2xl mt-14 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
 
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-slate-700 to-indigo-900 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl mb-4 font-bold text-white">Discover Homewood</h2>  
+                    <div className="bg-gradient-to-br from-gray-50 to-blue-50 p-8 rounded-2xl shadow-lg mb-8 flex-1 transform transition duration-500 hover:shadow-2xl">
+                    <p className="mb-2 text-left">
+    <span className="font-semibold text-indigo-900">Homewood</span>, a Birmingham suburb in <span className="text-indigo-500">Jefferson County</span>, had a population of <span className="font-semibold text-indigo-900">26,414</span> in 2020.
+</p>
+<p className="mb-2 text-left">
+    Formed in <span className="font-semibold text-indigo-900">1926</span>, it includes areas like <span className="text-indigo-500">Edgewood</span> and <span className="text-indigo-500">Rosedale</span>.
+</p>
+<p className="mb-2 text-left">
+    Features historic <span className="font-semibold text-indigo-900">Hollywood district</span>, annexed in <span className="font-semibold text-indigo-900">1929</span>.
+</p>
+<p className="mb-2 text-left">
+    Overcame <span className="text-indigo-500">Great Depression</span> and <span className="text-indigo-500">polio</span>; rebounded with <span className="font-semibold text-indigo-900">WWII</span>.
+</p>
+<p className="mb-2 text-left">
+    Created its own <span className="font-semibold text-indigo-900">school system</span> in <span className="font-semibold text-indigo-900">1970</span>.
+</p>
+
+            <p className="text-gray-600"><b>County</b> | Jefferson</p>
+            <p className="text-gray-600"><b>Zip Code</b> | 35209, 35219, 35259, 35226</p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Homewood Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+    <h2 className="text-3xl font-semibold text-rich-purple-900 mt-5 mb-6">Best Things to Do in Homewood, AL</h2>
+
+<p className="text-indigo-950 text-lg mb-1"><b><a href="https://visitvulcan.com/" class="underline">Vulcan Park and Museum</a></b></p>
+<p className="mb-2 text-white text-lg">Iconic park with a statue of Vulcan, the Roman God of fire and forge.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b><a href="https://www.birminghamzoo.com/" class="underline">Birmingham Zoo</a></b></p>
+<p className="mb-2 text-white text-lg">A 122-acre zoo with over 230 species from six continents.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b><a href="https://www.bbgardens.org/" class="underline">Birmingham Botanical Gardens</a></b></p>
+<p className="mb-2 text-white text-lg">Beautiful gardens with diverse floral displays adjacent to the zoo.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b><a href="https://redmountainpark.org/" class="underline">Red Mountain Park</a></b></p>
+<p className="mb-2 text-white text-lg">A historic park offering outdoor activities and rich industrial history.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b><a href="https://www.samford.edu/planetarium/" class="underline">Christenberry Planetarium</a></b></p>
+<p className="mb-2 text-white text-lg">Interactive Space & Science Education at Samford University.</p>
+
+    </div>
+</div>
+
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-deep-blue-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+<div className="text-center mt-4">
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-gold-500 hover:bg-gold-700 text-black py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-silver-600 hover:bg-silver-800 text-black py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+    </div>
         </>
     );
 }
