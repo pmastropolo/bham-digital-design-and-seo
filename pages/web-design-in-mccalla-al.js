7189b030772f245b956aa1c62c1959b494e84f59
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 17:25:47 -0600

    Update web-design-in-mccalla-al.js

--- pages/web-design-in-mccalla-al.js
+++ pages/web-design-in-mccalla-al.js
@@ -15,20 +15,112 @@ const WebDesignMcCalla = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in McCalla, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Bham Web Design and SEO Co. is your partner for exceptional web design services in McCalla, AL. We specialize in crafting websites that represent your brand and connect with your McCalla audience.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Us for Web Design in McCalla?</h2>
-    <p className="text-gray-600">In McCalla, AL, a compelling web presence is essential. Our web design solutions are personalized to meet the unique demands and goals of your McCalla business, ensuring an impactful and effective online presence.</p>
-  </section>
+ <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in McCalla, AL
+    </h1>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Ready to Kickstart Your Web Design Project?</h2>
-    <p className="text-gray-600">If youre looking to elevate your digital footprint in McCalla, AL, Bham Web Design and SEO Co. is here to assist. Contact us today to discuss your web design needs and embark on your journey to digital success.</p>
-  </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Bo Jackson, raised in Martin Town, set multiple state sports records at McAdory schools.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover McCalla</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
+
+<p className="mb-2 text-left"><span className="font-semibold text-indigo-900">McCalla</span> is the childhood home of famous athlete <span className="font-semibold text-indigo-900">Bo Jackson</span>, known for his remarkable multi-sport career.</p>
+<p className="mb-2 text-left">McCalla has several <span className="font-semibold text-indigo-900">historic churches</span>, some dating back to the <span className="text-indigo-500">19th century</span>, reflecting its cultural heritage.</p>
+<p className="mb-2 text-left">McCalla was initially settled in the <span className="font-semibold text-indigo-900">early 19th century</span>, with agriculture as its main economic activity.</p>
+<p className="mb-2 text-left">In the <span className="font-semibold text-indigo-900">20th century</span>, coal mining became a significant industry in the McCalla area.
+</p>
+<p className="mb-2 text-left">
+    The development of <span className="font-semibold text-indigo-900">railroads</span> in the late <span className="text-indigo-500">19th century</span> significantly boosted McCallas growth and connectivity.
+</p>
+
+                   
+
+<p className="text-gray-600"><b>County</b> | <span className="font-semibold text-indigo-900">Jefferson</span></p>
+<p className="text-gray-600"><b>Zip Code</b> | 35111<span className="font-semibold text-indigo-900"></span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your McCalla Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+            <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Things To Do in McCalla</h2>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Shelby County Museum and Archives</b></p>
+<p className="mb-2 text-white text-lg">Explore local history and heritage.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Karl C. Harrison Museum of George Washington</b></p>
+<p className="mb-2 text-white text-lg">Discover artifacts from the first U.S. President.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Beeswax Creek Park</b></p>
+<p className="mb-2 text-white text-lg">Enjoy outdoor activities and nature trails.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Shops of Columbiana</b></p>
+<p className="mb-2 text-white text-lg">Find unique gifts and local crafts.</p>
+
+<p className="text-indigo-950 text-lg mb-1"><b>Scott Vaughn Owen Studio</b></p>
+<p className="mb-2 text-white text-lg">Experience the local art scene.</p>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
