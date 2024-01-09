191882ecce554900e1df5377adb40bb7bd57bb1a
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 15:48:33 -0600

    Update seo-services-in-trussville-al.js

--- pages/seo-services-in-trussville-al.js
+++ pages/seo-services-in-trussville-al.js
@@ -15,10 +15,143 @@ const SEOTrussville = () => {
                     type: 'website',
                 }}
             />
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Trussville, AL</h2>
-                {/* Add your SEO services content for Trussville here */}
+
+<div className="bg-gradient-to-br from-slate-700 to-indigo-800 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Trussville, AL
+    </h1>
+
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+        Trussville Has Over 75 Restaurants
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Trussville, AL</h2>  
+                    <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-1 transform transition duration-500 hover:shadow-2xl">
+                    <p className="mb-4">
+    <span className="font-semibold text-indigo-900">Trussville</span>, settled by <span className="text-indigo-500">Warren Truss</span> in <span className="font-semibold text-indigo-900">1821</span>, was initially an agricultural community, growing significantly post-<span className="text-indigo-500">Civil War</span>.
+</p>
+<p className="mb-4">
+    The <span className="font-semibold text-indigo-900">Cahaba Project</span>, a <span className="text-indigo-500">1930s</span> development by <span className="font-semibold text-indigo-900">FDRs administration</span>, dramatically shaped Trussvilles growth, adding over <span className="font-semibold text-indigo-900">250 homes</span>.
+</p>
+<p className="mb-4">
+    Homes in Trussville featured the <span className="text-indigo-500">American four-square style</span>, complete with modern amenities and <span className="font-semibold text-indigo-900">two oak saplings</span> per household.
+</p>
+<p className="mb-4">
+    Trussville was incorporated as a town in <span className="font-semibold text-indigo-900">1947</span> and became a city in <span className="font-semibold text-indigo-900">1957</span>.
+</p>
+<p className="mb-4">
+    From <span className="font-semibold text-indigo-900">1980 to 2010</span>, Trussvilles population grew by over <span className="font-semibold text-indigo-900">500%</span>, marked by substantial residential and retail development.
+</p>
+
+            <p className="text-gray-600"><b>County</b> | Jefferson</p>
+            <p className="text-gray-600"><b>Zip Code</b> | 35004, 35173, 35210, 35235</p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Woodstock Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+
+
+<div className="flex-1 flex flex-col">
+            <div className="bg-white p-6 rounded-lg shadow-md mb-6 flex-grow">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">Best Things to Do in Trussville, AL</h2>
+
+                <p className="text-indigo-500"><b>Cosby Lake Park</b></p>
+<p className="mb-3 text-indigo-500">A serene lake surrounded by lush forests, ideal for walking, cycling, and bird-watching.</p>
+
+<p className="text-indigo-500"><b>Turkey Creek Nature Preserve</b></p>
+<p className="mb-3 text-indigo-500">Home to beautiful creeks, waterfalls, and diverse wildlife. Perfect for hiking and swimming.</p>
+
+<p className="text-indigo-500"><b>Barber Vintage Motorsports Museum</b></p>
+<p className="mb-3 text-indigo-500">Features an extensive collection of over 1,600 motorcycles and sports cars.</p>
+
+<p className="text-indigo-500"><b>Trussville Antique Mall</b></p>
+<p className="mb-3 text-indigo-500">A large mall with a diverse range of antiques and collectibles.</p>
+
+<p className="text-indigo-500"><b>Paint All The Things</b></p>
+<p className="mb-3 text-indigo-500">A creative space offering painting classes for all ages.</p>
+
+<p className="text-indigo-500"><b>Grand River Drive-In Movie Theatre</b></p>
+<p className="mb-3 text-indigo-500">Enjoy films under the stars with food and entertainment amenities.</p>
+
+<p className="text-indigo-500"><b>The Outlet Shops of Grand River</b></p>
+<p className="mb-3 text-indigo-500">A shopping destination with a variety of designer and high-street brands.</p>
+
+<p className="text-indigo-500"><b>Ruffner Mountain Nature Preserve</b></p>
+<p className="mb-3 text-indigo-500">Explore the trails and learn about the regions iron ore mining history.</p>
+
+<p className="text-indigo-500"><b>Southern Museum of Flight</b></p>
+<p className="mb-3 text-indigo-500">An aviation museum showcasing the history of flight with numerous exhibits.</p>
+
+<p className="text-indigo-500"><b>Porsche Sport Driving School</b></p>
+<p className="mb-3 text-indigo-500">Experience driving a variety of Porsche models on the track.</p>
+
+<p className="text-indigo-500"><b>Sloss Furnaces National Historic Landmark</b></p>
+<p className="mb-3 text-indigo-500">A monument to the industrial revolution, offering tours and events.</p>
+
+<p className="text-indigo-500"><b>Birmingham Civil Rights Institute</b></p>
+<p className="mb-3 text-indigo-500">An educational and cultural research center in Birmingham dedicated to Civil Rights history.</p>
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+
+
+                {/* Contact Us Button */}
+                <div className="text-center mt-4">
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
         </>
     );
 }
