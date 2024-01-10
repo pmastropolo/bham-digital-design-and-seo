5cd6cc1b94af12b6e8eb1fd576831b7a52e4cfa7
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 17:38:13 -0600

    Update seo-services-in-leeds-al.js

--- pages/seo-services-in-leeds-al.js
+++ pages/seo-services-in-leeds-al.js
@@ -15,10 +15,130 @@ const SEOLeeds = () => {
                     type: 'website',
                 }}
             />
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Leeds, AL</h2>
-                {/* Add your SEO services content for Leeds here */}
+
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Leeds, AL
+    </h1>
+
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Leeds is home to the world-famous Barber Motorsports Museum & Racetrack.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Leeds</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Leeds, Alabama, has a rich history dating back to the early 19th century. was founded in 1877 by Francis Elias Whitfield and was named after the city of Leeds in West Yorkshire, England. Initially, it was a mining town with significant deposits of coal, iron, and limestone. Over the years, Leeds has evolved into a diverse community with a blend of historical charm and modern development.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Leeds is located in central Alabama, about 15 miles east of Birmingham. Covering approximately 22 square miles, it lies in both Jefferson and Shelby counties, nestled in the Appalachian foothills, known for its natural beauty.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">Leeds operates under a Mayor-Council government system. The mayor, alongside a five-member council elected by residents, is responsible for city governance and municipal services.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">The citys economy is varied, with retail, manufacturing, healthcare, and construction sectors. Leeds also boasts the Leeds Industrial Park, attracting a range of businesses.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Leeds City Schools district serves the city, including an elementary, middle, and high school, emphasizing high-quality education and diverse extracurricular activities.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Leeds offers excellent connectivity with Interstate 20 and proximity to Birmingham-Shuttlesworth International Airport, facilitating easy travel and access.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Key attractions in Leeds include the Barber Vintage Motorsports Museum and the nearby Talladega Superspeedway, a major NASCAR racing venue.</p>
+
+<p className="text-gray-600 text-left"><b>Countys</b> : <span className="font-semibold text-indigo-900">Jefferson | St. Clair | Shelby</span></p>
+<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900">35004 | 35094 | 35173</span></p>
+
+
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Leeds Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Leeds</h2>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Barber Vintageports Museum</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">If youre a fan of motorcycles or motorsports, this museum is a must-visit. It houses the worlds largest collection of vintage motorcycles and features over 1,400 motorcycles on display.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Oak Mountain State Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">A short drive from Leeds, Oak Mountain State Park offers outdoor activities like hiking, fishing, boating, picnicking, and camping in a scenic environment.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Outlet Shops of Grand River</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">This open-air outlet mall near Leeds features numerous brand-name stores, offering great deals on clothing, accessories, and more for a delightful shopping experience.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Leeds Downtown Historical District</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Explore the historic downtown area of Leeds with its well-preserved buildings, unique shops, restaurants, cafes, and beautiful architecture from the late 1800s.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Leeds Arts Council</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Visit the Leeds Arts Council for art exhibitions and events showcasing local artists work, offering a cultural experience throughout the year.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Leeds Memorial Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">This park in Leeds is perfect for relaxation, picnics, walks, and features playgrounds, trails, sports fields, and a pavilion.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Talladega Superspeedway</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Near Leeds, the Talladega Superspeedway is a haven for racing enthusiasts, hosting NASCAR races and other thrilling motorsport events.</p>
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Leeds Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+
+    
         </>
     );
 }
