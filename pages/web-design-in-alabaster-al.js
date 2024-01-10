017c9b06e74438e41c2918d020b0172bc9484a33
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 12:13:41 -0600

    Update web-design-in-alabaster-al.js

--- pages/web-design-in-alabaster-al.js
+++ pages/web-design-in-alabaster-al.js
@@ -16,23 +16,22 @@ const WebDesignAlabaster = () => {
                 }}
             />
 
-<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
-    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-6 sm:py-12 px-4 sm:px-6 shadow-xl rounded-2xl mt-5 sm:mt-10 mb-12 sm:mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-3xl sm:text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-3 sm:my-4 animate-gradient">
         Web Design Services in Alabaster, AL
     </h1>
 
-    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+    <p className="text-white text-opacity-90 mb-4 sm:mb-8 mt-6 text-base sm:text-lg md:text-xl leading-normal sm:leading-loose max-w-md sm:max-w-xl md:max-w-3xl mx-auto">
         Empowering your businesss online presence with our expert web design and SEO solutions.
     </p>
-    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
-        <p className="text-lg md:text-xl text-white">
-            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
-             Known for its abundant limestone deposits, the city derives its name from the alabaster appearance of the rock.
+    <div className="py-3 sm:py-4 px-4 sm:px-6 mb-3 sm:mb-4 mt-3 sm:mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-base sm:text-lg md:text-xl text-white">
+            <span className="block mb-1 sm:mb-2 text-xl sm:text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Known for its abundant limestone deposits, the city derives its name from the alabaster appearance of the rock.
         </p>
     </div>
     <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
-</div>
-
+    </div>
 
 {/* Discover Section */}
 <div className="max-w-7xl mx-auto">
@@ -43,46 +42,30 @@ const WebDesignAlabaster = () => {
                 {/* Discover Title */}
                 <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Alabaster</h2>  
                     <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
-                        
-                      
+    
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Established in 1953 in Shelby County, Alabama, the city gets its name from the presence of extensive limestone deposits in the area, which were once used to produce plaster powder, giving it an alabaster appearance</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">History</span>
-  Alabaster is a city located in Shelby County, Alabama. It was initially incorporated in 1953 and has since grown into a thriving community. The city gets its name from the presence of extensive limestone deposits in the area, which were once used to produce plaster powder, giving it an alabaster appearance.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Situated in central Alabama, covering 25 square miles, it is 20 miles south of Birmingham, featuring rolling hills near the Cahaba River.</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">Geography</span>
-  Alabaster is situated in the central part of Alabama and covers a total area of approximately 25 square miles. It is located about 20 miles south of downtown Birmingham and is part of the Birmingham-Hoover Metropolitan Area. The city is characterized by rolling hills and is bordered by the Cahaba River.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">The city operates under a mayor-council government system, with Mayor Marty Handlon currently at the helm.</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">Government</span>
-  Alabaster operates under a mayor-council form of government. The citys government consists of a mayor and a city council that is responsible for making policy decisions and overseeing city services. The current mayor of Alabaster is Marty Handlon.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">Diverse in its economic makeup, the area is a hub for healthcare, retail, and manufacturing. Highlights include the Promenade shopping centers and a University of Alabama medical clinic.</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">Economy</span>
-  Alabaster has a diverse economy that includes sectors such as healthcare, retail, manufacturing, and professional services. The city is home to several retail centers, including the Alabaster Promenade and Colonial Promenade Alabaster, which provide shopping and dining options for residents and visitors. The University of Alabama Health Services Foundation, a medical clinic, is also located in Alabaster.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Led by the Alabaster City School District, the city also offers private educational options and proximity to Jefferson State Community College.</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">Education</span>
-  The city of Alabaster is primarily served by the Alabaster City School District, which operates public schools in the area. The district includes multiple elementary, middle, and high schools. There are also private school options available in the city. Additionally, Jefferson State Community College has a campus located in nearby Hoover, providing higher education opportunities for residents.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Alabaster is conveniently located near major highways, including Interstate 65, which runs through the city, providing easy access to other parts of Alabama and beyond. The Birmingham-Shuttlesworth International Airport is located approximately 25 miles north of Alabaster, offering air travel options.</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">Transportation</span>
-  Alabaster is conveniently located near major highways, including Interstate 65, which runs through the city, providing easy access to other parts of Alabama and beyond. The Birmingham-Shuttlesworth International Airport is located approximately 25 miles north of Alabaster, offering air travel options.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Known for Buck Creek Park, Veterans Park, and the annual CityFest, the city offers a vibrant mix of recreational and cultural events.</p>
 
-<p className="mb-2 text-left">
-  <span className="font-semibold text-indigo-900">Attractions and Events</span>
-  Alabaster offers a range of recreational activities and attractions for residents and visitors. Buck Creek Park is a popular destination with walking trails, pavilions, and a creek for fishing. Veterans Park features a memorial to honor local veterans and hosts community events. The city also holds several annual events, such as the CityFest, which showcases live music, food vendors, and activities for all ages.
-</p>
-
-<p className="text-gray-600"><b>County</b> | <span className="font-semibold text-indigo-900">Shelby</span></p>
-<p className="text-gray-600"><b>Zip Code</b> | <span className="font-semibold text-indigo-900">35071, 35073</span></p>
+<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900">Shelby</span></p>
+<p className="text-gray-600 text-left"><b>Zip Code</b> : <span className="font-semibold text-indigo-900">35071 | 35073</span></p>
         </div>
 
         <div className="mt-auto">
@@ -95,69 +78,52 @@ const WebDesignAlabaster = () => {
             </div>
         </div>
 
+<div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Alabaster</h2>
 
-        <div className="flex-1 flex flex-col">
-<div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
-            <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Best Things to Do in Gardendale</h2>
+{/* Buck Creek Trail */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Buck Creek Trail</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Enjoy walking or biking along the scenic Buck Creek Trail, offering a serene environment and nature views.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.alapark.com/parks/oak-mountain-state-park">Oak Mountain State Park</a></b></p>
-<p className="mb-2 text-white text-lg">Alabamas largest state park with picturesque landscapes and recreational activities.</p>
+{/* Alabaster CityFest */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Alabaster CityFest</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">This annual music festival features live performances, arts and crafts vendors, food trucks, and childrens activities for family fun.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.cityofhelena.org/">Joe Tucker Park</a></b></p>
-<p className="mb-2 text-white text-lg">A local park offering a variety of recreational facilities and activities.</p>
+{/* Siluria Brewing Company */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Siluria Brewing Company</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">A haven for beer enthusiasts, offering a variety of craft beers in their welcoming taproom.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.cityofhelena.org/museum/">Kenneth R. Penhale City of Helena Museum</a></b></p>
-<p className="mb-2 text-white text-lg">A museum showcasing Helenas history through photographs, artifacts, and documents.</p>
+{/* Limestone Park */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Limestone Park</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Enjoy outdoor activities with a disc golf course, walking trails, and a pavilion in this community park.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://bluewaterparkal.com/">Blue Water Park</a></b></p>
-<p className="mb-2 text-white text-lg">A unique park in Pelham offering scuba diving experiences and training.</p>
+{/* Shelby County Museum & Archives */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Shelby County Museum & Archives</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Explore Shelby Countys history and heritage through diverse artifacts, photographs, and exhibits.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://hoovermetcomplex.com/">Hoover Metropolitan Complex</a></b></p>
-<p className="mb-2 text-white text-lg">A sports and events complex hosting a variety of sporting events and concerts.</p>
+{/* Dogwood Trail */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Dogwood Trail</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Witness the beauty of blooming dogwood trees in spring along this scenic trail.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b>Hot Air Balloon Rides</b></p>
-<p className="mb-2 text-white text-lg">Experience breathtaking views of Alabama with a hot air balloon ride.</p>
+{/* Alabaster YMCA */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Alabaster YMCA</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Offers a range of fitness facilities and programs for all ages to stay active and healthy.</p>
 
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.theoakmountainamphitheater.com/">Oak Mountain Amphitheatre</a></b></p>
-<p className="mb-2 text-white text-lg">A popular music venue hosting famous artists and bands.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.ozanwine.com/">Ozan Winery & Vineyard</a></b></p>
-<p className="mb-2 text-white text-lg">An award-winning winery and vineyard near Helena.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg">Old Town Helena</b></p>
-<p className="mb-2 text-white text-lg">A charming area with great dining options, bars, and shops.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.jimnnicks.com/">Jim n Nicks Bar-B-Q</a></b></p>
-<p className="mb-2 text-white text-lg">A popular local barbecue joint known for delicious dishes.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://aldridgegardens.com/">Aldridge Gardens</a></b></p>
-<p className="mb-2 text-white text-lg">Beautiful gardens combining nature and art in nearby Hoover.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.hooveral.gov/parks-recreation/moss-rock-preserve">Moss Rock Preserve and Waterfalls</a></b></p>
-<p className="mb-2 text-white text-lg">An outdoor adventure spot with hiking trails and beautiful waterfalls.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.warehouse31.com/">Warehouse31</a></b></p>
-<p className="mb-2 text-white text-lg">One of Alabamas scariest haunted house attractions.</p>
-
-<p className="text-indigo-950 text-lg mb-1"><b className="underline text-lg"><a href="https://www.ballantraegolf.com/">Ballantrae Golf Club</a></b></p>
-<p className="mb-2 text-white text-lg">A beautifully designed golf course offering a challenging play experience.</p>
-
-
-
+{/* Mahan Creek Greenway */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Mahan Creek Greenway</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">A versatile trail ideal for walking, running, or cycling, surrounded by the areas natural beauty.</p>
         </div>
     </div>
 </div>
-
-
 {/* Elevate Your Business Title Section */}
 <div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
-<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Business with Web Design</h2>
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Alabaster Business with Web Design and Development</h2>
 
 {/* Elevate Your Business Section */}
 <p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
 In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
 </p>
-
                 {/* Contact Us Button */}
                 <Link href="/contact" legacyBehavior>
                 <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
@@ -174,11 +140,7 @@ In the digital age, a compelling online presence is more than just a luxury. Its
             </div>
         </div>
     </div>
-
-       
     
-    
-        
         </>
     );
 }
