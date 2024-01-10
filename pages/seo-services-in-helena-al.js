8f19f5e73cea3213004d5365ac13901703fc0241
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 15:42:38 -0600

    Update seo-services-in-helena-al.js

--- pages/seo-services-in-helena-al.js
+++ pages/seo-services-in-helena-al.js
@@ -15,10 +15,118 @@ const SEOHelena = () => {
                     type: 'website',
                 }}
             />
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Helena, AL</h2>
-                {/* Add your SEO services content for Helena here */}
+
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Helena, AL
+    </h1>
+
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            The Battle of Helena took place here in 1863, and remnants of that era can still be seen in the form of historical markers, the Helena Jail Museum, and other Civil War-related sites.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Helena</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Helena was originally incorporated in 1877 and has a rich history tied to the coal mining industry. It was an important coal mining town during the late 19th and early 20th centuries. Today, it is a thriving suburban community with a strong sense of history and community pride.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Helena is located in Shelby County, Alabama, along the Cahaba River. Covering an area of approximately 17 square miles, it is known for its scenic natural beauty, including rolling hills and forests.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">Helena operates under a mayor-council government system. The citys governance is led by a mayor and a city council responsible for decision-making and policy implementation, with citizen participation encouraged in public meetings and elections.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">The economy of Helena is diverse, encompassing healthcare, retail, manufacturing, and services. The city has experienced growth and continues to attract those seeking a suburban lifestyle near Birmingham.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Part of the Shelby County School District, Helena boasts several public schools including Helena High School, Helena Middle School, and elementary schools, with a community focus on supporting educational programs and initiatives.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Helenas location near major routes like Interstate 459 and Highway 261 offers easy access to surrounding areas. The city also has a well-maintained road system for convenient local travel.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Helena is home to attractions like Old Town Helena and events such as the Buck Creek Festival and the Cahaba Lily Festival, celebrating local culture, arts, music, and the rare Cahaba Lily blooms.</p>
+
+<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900">Shelby</span></p>
+<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900">35022 | 35080</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Helena Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Helena</h2>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Old Town Helena</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Take a stroll through the historic district of Old Town Helena. You can explore its charming streets lined with unique shops, boutiques, and restaurants. Dont miss the opportunity to visit Buck Creek Park, a scenic spot along Buck Creek, where you can enjoy picnicking, walking trails, and even catch a concert at the outdoor amphitheater.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Cahaba Lily Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Visit Cahaba Lily Park to see the stunning Cahaba lilies bloom in summer. The park offers hiking trails, bird-watching, and scenic views of the Cahaba River.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Helena Hollow</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Helena Hollow is a family-friendly venue with zip-lining, hayrides, a petting zoo, and a corn maze. Its perfect for a day of fun with family and friends.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Joe Tucker Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Joe Tucker Park features sports fields, a playground, walking trails, and a fishing pond. Its ideal for outdoor activities and picnics, with facilities for disc golf as well.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Heart of Dixie Railroad Museum</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Located near Helena in Calera, this museum offers train rides and exhibits on Alabamas railroad history, making it a fascinating visit for train enthusiasts and history buffs.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Helena Amphitheater</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">The Helena Amphitheater hosts various events and concerts, especially in summer. Enjoy live music and entertainment in this beautiful outdoor venue.</p>
+
+        </div>
+    </div>
+</div>
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Helena Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+    
         </>
     );
 }
