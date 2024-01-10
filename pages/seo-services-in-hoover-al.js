a1eb793192898eb5b22e9e5ec984ec845a383825
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 16:05:33 -0600

    Update seo-services-in-hoover-al.js

--- pages/seo-services-in-hoover-al.js
+++ pages/seo-services-in-hoover-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const SEOHoover = () => {
     return (
@@ -17,133 +16,139 @@ const SEOHoover = () => {
                 }}
             />
 
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-    <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Hoover, AL</h2>
-    <p className="text-gray-600 mb-4">Fun Fact | Named after William H. Hoover</p>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        SEO Services in Hoover, AL
+    </h1>
 
-    <section className="mb-4">
-        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-            {/* Text Section */}
-            <div className="w-full md:w-1/2 text-gray-700">
-                <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Hoover, AL</h2>
-                <p className="text-slate-600 mb-4 text-left"><b>County</b> | Shelby</p>
-                <p className="mb-2 text-left">Incorporated in 1967 | Rapid Suburban Growth</p>
-                <p className="mb-2 text-left">Notable Hoover City School System</p>
-                <p className="mb-2 text-left">Scenic Foothills Location | Abundance of Parks</p>
-                <p className="mb-2 text-left">Diverse and vibrant community | Active Community Involvement | Annual Hoover Hayride & More</p>
-                <p className="mb-2 text-left">Strategic economic hub</p>
-                <p className="text-slate-700 mb-4 text-left"><b>Zip Codes</b> | 35216, 35226, 35236, 35244</p>
-            </div>
-            {/* Image Section */}
-            <div className="w-full md:w-1/2 flex items-center justify-center">
-                <a href="https://commons.wikimedia.org/wiki/File:Hoover_Board_of_Education.jpg" target="_blank" rel="noopener noreferrer">
-                    <Image
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hoover_Board_of_Education.jpg/1200px-Hoover_Board_of_Education.jpg?20160622133629"
-                        alt="Farr Administration Building/Hoover Board of Education. Main administrative building for Hoover City Schools."
-                        width={400} 
-                        height={300}
-                        className="rounded-lg max-w-full"
-                    />
-                </a>
-            </div>
-        </div>
-    </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Named after William H. Hoover
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
 
-                <section className="mt-4">
-    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-        {/* Image Section */}
-        <div className="w-full md:w-1/2 flex justify-center items-center">
-            <a href="https://commons.wikimedia.org/wiki/File:Moss_Rock_Preserve_2.jpg" target="_blank" rel="noopener noreferrer">
-                <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Moss_Rock_Preserve_2.jpg/675px-Moss_Rock_Preserve_2.jpg?20160803134717"
-                    alt="Waterfall at Moss Rock Preserve in Hoover, AL"
-                    width={400}
-                    height={300}
-                    className="rounded-lg max-w-full mr-4"
-                />
-            </a>
 
-            <a href="https://commons.wikimedia.org/wiki/File:Moss_Rock_Preserve_sign.jpg" target="_blank" rel="noopener noreferrer">
-                <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Moss_Rock_Preserve_sign.jpg/675px-Moss_Rock_Preserve_sign.jpg?20160803134718"
-                    alt="Sign at Moss Rock Preserve in Hoover, AL"
-                    width={400}
-                    height={300}
-                    className="rounded-lg max-w-full ml-4"
-                />
-            </a>
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Hoover</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Hoover has a relatively short history compared to some neighboring cities. It was incorporated in 1967 and named after William H. Hoover, a local insurance company owner. The citys growth can be attributed to its proximity to the larger city of Birmingham.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Hoover is located in Shelby County, just south of Birmingham. Its set among rolling hills and bordered by the Cahaba River, covering around 48 square miles. The city features a diverse landscape of parks, neighborhoods, and commercial areas.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">Hoovers government operates under a mayor-council structure. Its led by a mayor, elected by residents, along with a nine-member council, focusing on services, infrastructure, and economic development.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">Hoovers economy is robust and diverse, with businesses ranging from corporate headquarters to retail centers like the Riverchase Galleria, healthcare facilities, and educational institutions.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">The city is served by Hoover City Schools, known for academic excellence. It includes several elementary, middle, and high schools, along with private schools and colleges nearby.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Hoover is accessible via major highways like I-65 and I-459, with the Birmingham-Shuttlesworth International Airport nearby. The city also offers public transportation and bike routes.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Attractions in Hoover include the Riverchase Galleria for shopping and entertainment, and parks like Veterans Park and Moss Rock Preserve for outdoor recreation and nature trails.</p>
+
+
+
+<p className="text-gray-600 text-left"><b>Countys</b> : <span className="font-semibold text-indigo-900">Jefferson | Shelby</span></p>
+<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900">35216 | 35226 | 35236 | 35244</span></p>
         </div>
 
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700 text-center mt-4">
-                            <h2 className="text-4xl font-bold text-gray-800 mb-4 mt-2">Things to Do | Hoover</h2>
-                            <div className="mt-4">
-
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.riverchasegalleria.com/en.html">Riverchase Galleria</a></b><br></br>Largest Shopping Mall in Alabama</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://aldridgegardens.com/">Aldridge Gardens</a></b><br></br>Beautiful Botanical Gardens</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://hoovermetcomplex.com/">The Hoover Met Complex</a></b><br></br>Sports Venue</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.cityofalabaster.com/facilities/facility/details/Veterans-Park-10">Veterans Park</a></b><br></br>Scenic Park With Trails</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="http://www.hooveral.org/214/Moss-Rock-Preserve">Moss Rock Preserve</a></b><br></br>Nature Preserve for Hiking</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.hooverlibrary.org/">Hoover Public Library</a></b><br></br>Modern Library & Cultural Events</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="https://www.stardome.com/">Comedy Club Stardome</a></b><br></br>Comedy Shows & Performances</p>
-<p className="mb-3 text-indigo-500"><b className="underline text-lg"><a href="http://www.hooveral.org/660/Georgetown-Park">Georgetown Park</a></b><br></br>8 Acre Park With A Lake</p>
-
-
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className="mb-8 mt-4">
-    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-        {/* Text Section */}
-        <div className="w-full md:w-1/2 text-gray-700">
-            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Hoover with Our SEO Expertise</h2>
-            <p>In the digital age, a compelling online presence is more than just a luxury. Its a necessity. At Bham Web Design and SEO Co., we understand the heartbeat of Hoover, AL. Our SEO solutions are strategically crafted to meet your local businesss unique needs and goals.</p>
-            <p className="mt-4">We implement innovative strategies to boost your websites visibility and engagement. With our expertise in current trends and timeless SEO principles, we promise to elevate your brand, drive growth, and connect with your audience effectively.</p>
-            <div className="mt-6 gap-8">
-                <Link href="/contact" legacyBehavior>
-                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
-                        Contact Us
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Hoover Business Online
                     </a>
-                </Link>
-
-                {/* SEO Packages Button with Link */}
-                <Link href="/seopackagepage" legacyBehavior>
-                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-6 py-2 px-4 rounded">
-                        View SEO Packages
-                    </a>
-                </Link>
+                    </Link>
+                </div>
             </div>
         </div>
 
-        {/* External Image with Caption */}
-        <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
-            <a href="https://commons.wikimedia.org/wiki/File:Center_Court_of_the_Riverchase_Galleria.jpg" target="_blank" rel="noopener noreferrer">
-                <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Center_Court_of_the_Riverchase_Galleria.jpg?20090517180511"
-                    alt="Riverchase Galleria Center Court in Hoover, AL"
-                    width={350}
-                    height={300}
-                    className="rounded-lg max-w-full"
-                />
-            </a>
-            <p className="text-center mt-2 mb-4 text-gray-600">Riverchase Galleria Center Court</p>
-        </div>
 
-        {/* Local Image with Caption */}
-        <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
-            <Image
-                src="/images/hooverorange.jpg"
-                alt="Alridge Gardens Orange Flower"
-                width={350}
-                height={300}
-                className="rounded-lg max-w-full ml-4"
-            />
-            <p className="text-center mt-2 text-gray-600">Alridge Gardens Orange Flower</p>
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Hoover</h2>
+
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b> Aldridge Gardens</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Explore the beautiful botanical gardens featuring stunning flowers, walking trails, and a serene lake.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Moss Rock Preserve</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Moss Rock Preserve offers hiking trails, boulder fields, and scenic views for outdoor enthusiasts to explore natures beauty.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Hoover Metropolitan Complex</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Enjoy baseball games at Hoover Met Stadium, or attend concerts and festivals hosted at this versatile complex.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Riverchase Galleria</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">One of the largest malls in the Southeast, the Galleria offers an extensive range of stores, restaurants, and entertainment options for a full day of shopping and fun.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Birmingham Barons</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Catch a game of the Birmingham Barons at Regions Field, a short drive from Hoover, and enjoy exciting minor league baseball action.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Hoover Public Library</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">The award-winning Hoover Public Library offers a vast collection of books, digital resources, and hosts various events and workshops for all ages.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Oak Mountain State Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Nearby Oak Mountain State Park provides outdoor adventure with hiking and biking trails, fishing, camping, and a beach area for swimming.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Urban Air Adventure Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">This indoor trampoline park is perfect for families, featuring trampolines, climbing walls, obstacle courses, and more for active fun.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Hoover Tactical Firearms</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Hoover Tactical Firearms offers a range of shooting experiences and classes for enthusiasts of shooting sports.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Local Dining</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Hoovers diverse food scene includes everything from barbecue joints to ethnic eateries, offering delicious options for all culinary tastes.</p>
+
+
         </div>
     </div>
-</section>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Hoover Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
             </div>
+        </div>
+    </div>
+
         </>
     );
 }
