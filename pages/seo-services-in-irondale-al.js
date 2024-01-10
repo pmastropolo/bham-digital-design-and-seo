041e8542e47eac9bfaf6477d958a76be06e6c27c
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 17:29:40 -0600

    Update seo-services-in-irondale-al.js

--- pages/seo-services-in-irondale-al.js
+++ pages/seo-services-in-irondale-al.js
@@ -1,145 +1,26 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
-import Link from 'next/link';
 
-const WebDesignIrondale = () => {
+
+const SEOIrondale = () => {
     return (
         <>
             <NextSeo
-                title="Web Design Services in Irondale, AL | Bham Web Design and SEO Co."
-                description="Bham Web Design and SEO Co. offers top-notch web design services in Irondale, AL. Explore our customized web solutions designed to elevate your Irondale business's online presence."
+                title="SEO Services in Irondale, AL | Bham Web Design and SEO Co."
+                description="Bham Web Design and SEO Co. offers top-notch SEO services in Irondale, AL. Explore our customized SEO solutions designed to elevate your Irondale business's online presence."
                 openGraph={{
-                    title: 'Web Design Services in Irondale, AL | Bham Web Design and SEO Co.',
-                    description: 'Bham Web Design and SEO Co. offers top-notch web design services in Irondale, AL. Explore our customized web solutions designed to elevate your Irondale businesss online presence.',
-                    url: 'https://bhamseoanddesignco.com/web-design-irondale-al',
+                    title: 'SEO Services in Irondale, AL | Bham Web Design and SEO Co.',
+                    description: 'Bham Web Design and SEO Co. offers top-notch SEO services in Irondale, AL. Explore our customized SEO solutions designed to elevate your Irondale businesss online presence.',
+                    url: 'https://bhamseoanddesignco.com/seo-services-in-irondale-al',
                     type: 'website',
                 }}
             />
-   
-   <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
-    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
-        Web Design Services in Irondale, AL
-    </h1>
-
-    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
-        Empowering your businesss online presence with our expert web design and SEO solutions.
-    </p>
-    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
-        <p className="text-lg md:text-xl text-white">
-            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
-            The name was taken from a furance located just west of what now is the city limits of Irondale.
-        </p>
-    </div>
-    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
-</div>
-
-
-{/* Discover Section */}
-<div className="max-w-7xl mx-auto">
-    <div className="flex flex-col md:flex-row gap-8">
-        <div className="flex-1 flex flex-col">
-            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
-
-                {/* Discover Title */}
-                <h2 className="text-4xl font-bold text-gray-800">Discover Irondale</h2>  
-                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
-<p className="mb-4 text-left">Irondale is a city located in Jefferson County, Alabama. It was originally settled in the late 1800s and became known for its iron industry, specifically iron ore mining and processing. The city experienced significant growth during the industrial boom of the early 20th century.</p>
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
-<p className="mb-4 text-left">Located in central Alabama, about 8 miles east of Birmingham, Irondale spans approximately 19 square miles. Its surrounded by scenic hills and forests, at the foothills of the Appalachian Mountains, offering beautiful views and outdoor activities.</p>
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
-<p className="mb-4 text-left">Irondale operates under a mayor-council government system. The mayor, as chief executive, and the city council, consisting of elected representatives, manage the citys policies and departments.</p>
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
-<p className="mb-4 text-left">The economy in Irondale is diverse, with a mix of retail, healthcare, services, small businesses, and restaurants contributing to the local economic landscape.</p>
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
-<p className="mb-4 text-left">Irondale is part of the Jefferson County School District and hosts various public and private schools. Nearby Birmingham also offers higher education opportunities with several colleges and universities.</p>
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
-<p className="mb-4 text-left">Irondale enjoys excellent connectivity through major highways like Interstate 20 and U.S. Route 11, and is near the Birmingham-Shuttlesworth International Airport for air travel.</p>
-
-<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
-<p className="mb-4 text-left">Attractions include the Ruffner Mountain Nature Preserve with its extensive hiking trails and wildlife. The annual Whistle Stop Festival celebrates the citys history with music, food, and activities for all ages.</p>
-
-<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900">Jefferson</span></p>
-<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900">35210 | 35213 | 35223</span></p>
-
-
-        </div>
-
-        <div className="mt-auto">
-        <Link href="/PackagePage" legacyBehavior>
-                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
-                        Elevate Your Irondale Business Online
-                    </a>
-                    </Link>
-                </div>
+            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
+                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO Services in Irondale, AL</h2>
+        
             </div>
-        </div>
-
-
-        <div className="flex-1 flex flex-col">
-    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
-        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Irondale</h2>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Whistle Stop Cafe</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Made famous by the novel and movie Fried Green Tomatoes, the Whistle Stop Café is a must-visit spot in Irondale. Enjoy a delicious Southern meal and try their famous fried green tomatoes.</p>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Ruffner Mountain Nature Preserve</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Located on Irondales eastern outskirts, Ruffner Mountain features over 14 miles of trails through forests and overlooks, ideal for nature enthusiasts and hikers.</p>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Pine Tree Country Club</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Pine Tree Country Club in Irondale offers an 18-hole championship golf course, tennis courts, a pool, and dining, perfect for golf lovers and leisure seekers.</p>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Irondale Municipal Park Events</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Irondale Municipal Park hosts a variety of events including concerts, festivals, and community gatherings. Check their schedule for upcoming happenings.</p>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Historical Sites in Irondale</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Visit historical landmarks like the Historic Irondale Furnace or the Cahaba Pumping Station to delve into Irondales rich history.</p>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Local Eateries in Irondale</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Sample Southern cuisine and other delicious dishes at Irondales local restaurants and eateries, offering classic comfort food and more.</p>
-
-<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Shop at Local Boutiques and Antique Stores</b></p>
-<p className="mb-4 text-white text-base sm:text-lg text-center">Explore unique gifts, antiques, and locally made products at the boutiques and antique stores in Irondale for a memorable shopping experience.</p>
-
-        </div>
-    </div>
-</div>
-
-
-{/* Elevate Your Business Title Section */}
-<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
-<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Irondale Business with Web Design</h2>
-
-{/* Elevate Your Business Section */}
-<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
-In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
-</p>
-
-                {/* Contact Us Button */}
-                <Link href="/contact" legacyBehavior>
-                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
-                    Contact Us
-                </a>
-                </Link>
-
-                {/* Web Design Form Button */}
-                <Link href="/webDesignConsultation" legacyBehavior>
-                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
-                    Start Web Design Initial Consultation
-                </a>
-                </Link>
-            </div>
-        </div>
-    </div>
-
         </>
     );
 }
 
-export default WebDesignIrondale;
+export default SEOIrondale;
