3b4ad86b1a0b60506ab041f8e254323938f5e9a3
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 12:15:21 -0600

    Update seo-services-in-alabaster-al.js

--- pages/seo-services-in-alabaster-al.js
+++ pages/seo-services-in-alabaster-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const SEOAlabaster = () => {
     return (
@@ -16,106 +15,132 @@ const SEOAlabaster = () => {
                     type: 'website',
                 }}
             />
+    
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-6 sm:py-12 px-4 sm:px-6 shadow-xl rounded-2xl mt-5 sm:mt-10 mb-12 sm:mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-3xl sm:text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-3 sm:my-4 animate-gradient">
+        SEO Services in Alabaster, AL
+    </h1>
 
-            <div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">SEO in Alabaster, AL</h2>
-                <p className="text-gray-600 mb-4">Nickname | The Good Neighbor City</p>
+    <p className="text-white text-opacity-90 mb-4 sm:mb-8 mt-6 text-base sm:text-lg md:text-xl leading-normal sm:leading-loose max-w-md sm:max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-3 sm:py-4 px-4 sm:px-6 mb-3 sm:mb-4 mt-3 sm:mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-base sm:text-lg md:text-xl text-white">
+            <span className="block mb-1 sm:mb-2 text-xl sm:text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Known for its abundant limestone deposits, the city derives its name from the alabaster appearance of the rock.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+    </div>
 
-                <section className="mb-4">
-                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700">
-                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Alabaster, AL</h2>
-                            <p className="text-red-600 mb-4 text-left"><b>County</b> | Shelby</p>
-                            <p className="mb-2 text-left">Named after the calcium carbonate mineral, Alabaster has a rich mining history.</p>
-                            <p className="mb-2 text-left">George L. Scott Sr. founded the Alabaster Lime Co., known as - Scott Rock.</p>
-                            <p className="mb-2 text-left">In 1952, the citys first bank was established, led by George L. Scott Jr., the first mayor.</p>
-                            <p className="mb-2 text-left">Officially incorporated in 1953, Alabaster transformed from a small community to Alabamas 16th largest city.</p>
-                            <p className="mb-2 text-left">The Main Street Medical Mile development began in 2015, signifying growth.</p>
-                            <p className="mb-2 text-left">District 31, a major retail complex, reflects Alabasters expansion at Highway 31 and Interstate 65.</p>
-                            <p className="text-gray-600 mb-4 text-left"><b>Zip Codes</b> | 35007, 35114, 35144</p>
-                        </div>
-        {/* Image Section with Next.js Image component */}
-        <div className="w-full md:w-1/2 flex items-center justify-center">
-        <a href="https://commons.wikimedia.org/wiki/File:Alabaster_City_Hall.jpg" target="_blank" rel="noopener noreferrer">
-                        <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/3/35/Alabaster_City_Hall.jpg?20160316195729"
-                alt="Alabaster City Hall"
-                width={400} 
-                height={300} 
-                className="rounded-lg"
-            />
-            </a>
-                        </div>
-                    </div>
-                </section>
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
 
-             
-                <section className="mt-4">
-                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex justify-center items-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:Alabaster,_Alabama_City_Hall.JPG" target="_blank" rel="noopener noreferrer">
-                        <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Alabaster%2C_Alabama_City_Hall.JPG/1200px-Alabaster%2C_Alabama_City_Hall.JPG?20120226115852"
-                alt="This is a photograph of the city hall in Alabaster, Alabama."
-                width={400} 
-                height={300} 
-                className="rounded-lg max-w-sm"
-            />
-            </a>
-                        </div>
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center mt-4">
-                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Best Things to Do in Alabaster, AL</h2>
-                            <div className="mt-4">
-                                <p className="mb-2 text-indigo-500"><b>Buck Creek Trail</b> | Scenic trails and events.</p>
-                                <p className="mb-2 text-indigo-500"><b>Siluria Brewing Company</b> | Craft brews in a historic setting.</p>
-                                <p className="mb-2 text-indigo-500"><b>American Village</b> | Interactive historical experience.</p>
-                                <p className="mb-2 text-indigo-500"><b>Ebenezer Swamp Ecological Preserve</b> | Nature lovers paradise.</p>
-                                <p className="mb-2 text-indigo-500"><b>Veterans Park</b> | Family-friendly outdoor activities.</p>
-                                <p className="mb-2 text-indigo-500"><b>Central Alabama Paintball Park</b> | Exciting paintball experience.</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800 mb-6">Discover Alabaster</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-8 flex-1 hover:shadow-2xl">
+    
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Established in 1953 in Shelby County, Alabama, the city gets its name from the presence of extensive limestone deposits in the area, which were once used to produce plaster powder, giving it an alabaster appearance</p>
 
-                <section className="mb-8 mt-4">
-    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-        {/* Text Section */}
-        <div className="w-full md:w-1/2 text-gray-700">
-            <h2 className="text-3xl font-bold text-gray-800 mb-4">Boost Your Online Visibility with Our SEO Expertise</h2>
-            <p>In todays digital landscape, having a strong online presence is not just an advantage; its essential. At Bham Web Design and SEO Co., we specialize in Search Engine Optimization (SEO) services in Alabaster, AL. We understand the heartbeat of your local business and can strategically enhance your websites visibility in search engines.</p>
-            <p className="mt-4">Our SEO solutions combine the latest industry trends with proven strategies, ensuring that your website not only looks appealing but also ranks higher in search results. With our expertise, we can drive organic traffic, engage your target audience, and elevate your brands online presence.</p>
-            <div className="mt-6 gap-8">
-                <Link href="/contact" legacyBehavior>
-                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
-                        Contact Us
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Situated in central Alabama, covering 25 square miles, it is 20 miles south of Birmingham, featuring rolling hills near the Cahaba River.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">The city operates under a mayor-council government system, with Mayor Marty Handlon currently at the helm.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">Diverse in its economic makeup, the area is a hub for healthcare, retail, and manufacturing. Highlights include the Promenade shopping centers and a University of Alabama medical clinic.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Led by the Alabaster City School District, the city also offers private educational options and proximity to Jefferson State Community College.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Alabaster is conveniently located near major highways, including Interstate 65, which runs through the city, providing easy access to other parts of Alabama and beyond. The Birmingham-Shuttlesworth International Airport is located approximately 25 miles north of Alabaster, offering air travel options.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Known for Buck Creek Park, Veterans Park, and the annual CityFest, the city offers a vibrant mix of recreational and cultural events.</p>
+
+<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900">Shelby</span></p>
+<p className="text-gray-600 text-left"><b>Zip Code</b> : <span className="font-semibold text-indigo-900">35071 | 35073</span></p>
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/seopackagepage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Alabaster Business Online
                     </a>
-                </Link>
-
-                {/* SEO Packages Button with Link */}
-                <Link href="/seopackagepage" legacyBehavior>
-                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
-                        View SEO Packages
-                    </a>
-                </Link>
+                    </Link>
+                </div>
             </div>
         </div>
-        <div className="w-full md:w-1/2 flex items-center justify-center">
-        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
-            <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
-                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
-                width={400} 
-                height={300} 
-                className="rounded-lg"
-            />
-              </a>
+
+<div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Alabaster</h2>
+
+{/* Buck Creek Trail */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Buck Creek Trail</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Enjoy walking or biking along the scenic Buck Creek Trail, offering a serene environment and nature views.</p>
+
+{/* Alabaster CityFest */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Alabaster CityFest</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">This annual music festival features live performances, arts and crafts vendors, food trucks, and childrens activities for family fun.</p>
+
+{/* Siluria Brewing Company */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Siluria Brewing Company</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">A haven for beer enthusiasts, offering a variety of craft beers in their welcoming taproom.</p>
+
+{/* Limestone Park */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Limestone Park</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Enjoy outdoor activities with a disc golf course, walking trails, and a pavilion in this community park.</p>
+
+{/* Shelby County Museum & Archives */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Shelby County Museum & Archives</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Explore Shelby Countys history and heritage through diverse artifacts, photographs, and exhibits.</p>
+
+{/* Dogwood Trail */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Dogwood Trail</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Witness the beauty of blooming dogwood trees in spring along this scenic trail.</p>
+
+{/* Alabaster YMCA */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Alabaster YMCA</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">Offers a range of fitness facilities and programs for all ages to stay active and healthy.</p>
+
+{/* Mahan Creek Greenway */}
+<p className="text-indigo-950 text-xl sm:text-2xl mb-1 text-center"><b>Mahan Creek Greenway</b></p>
+<p className="mb-6 text-white text-base sm:text-lg text-center">A versatile trail ideal for walking, running, or cycling, surrounded by the areas natural beauty.</p>
         </div>
     </div>
-</section>
+</div>
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-6 text-center">Elevate Your Alabaster Business with SEO</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+We seamlessly integrate cutting-edge web design with advanced SEO techniques, ensuring that your website not only captivates visitors but also effectively aligns with your business objectives. By staying at the forefront of the latest SEO trends while adhering to proven design principles, we deliver a website that is visually stunning, functionally powerful, and optimized for organic growth. Our approach centers on engaging your target audience, enhancing your brands online footprint, and generating measurable business results through SEO-driven strategies.
+</p>
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* SEO Form Button */}
+                <Link href="/seoConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start SEO Initial Consultation
+                </a>
+                </Link>
             </div>
+        </div>
+    </div>
+  
         </>
     );
 }
