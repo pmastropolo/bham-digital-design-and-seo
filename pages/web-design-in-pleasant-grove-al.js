212ba9cd5e96bcc8ceec54a2dd70d890b72ca970
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-09 20:34:02 -0600

    Update web-design-in-pleasant-grove-al.js

--- pages/web-design-in-pleasant-grove-al.js
+++ pages/web-design-in-pleasant-grove-al.js
@@ -28,7 +28,7 @@ const WebDesignPleasantGrove = () => {
     <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
         <p className="text-lg md:text-xl text-white">
             <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Nick Name</span>
-      
+      Originally known as Frog Pond because of the large number of frogs in the area, particularly during the rainy seasons. The name Pleasant Grove was eventually adopted in 1889 when the community established its first post office. 
         </p>
     </div>
     <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
