da8e602ac90bd0ca93f835368fd98ee37e984101
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 19:38:18 -0600

    Update web-design-in-montevallo-al.js

--- pages/web-design-in-montevallo-al.js
+++ pages/web-design-in-montevallo-al.js
@@ -16,21 +16,22 @@ const WebDesignMontevallo = () => {
                 }}
             />
 
- <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
-<h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
- Web Design Services in Montevallo, AL
-</h1>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Montevallo, AL
+    </h1>
 
-<p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">Empowering your businesss online presence with our expert web design and SEO solutions.</p>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
     <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
         <p className="text-lg md:text-xl text-white">
-        <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
-      Home to the American Village
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Home to the American Village
         </p>
     </div>
     <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
-
 
 {/* Discover Section */}
 <div className="max-w-7xl mx-auto">
@@ -41,24 +42,32 @@ const WebDesignMontevallo = () => {
                 {/* Discover Title */}
                 <h2 className="text-4xl font-bold text-gray-800">Discover Montevallo</h2>  
                     <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
-                    <p className="mb-2 text-left">
-    Montevallo is Shelby Countys oldest community, established in 1817 as Wilsons Hill and renamed in 1826. The name likely means on a mound in the valley in Italian, reflecting its geographic features.
-</p>
-<p className="mb-2 text-left">
-    The city developed as a farming, mining, and railroad community. Significant growth occurred with the founding of the Alabama Girls Industrial School in 1896, now the University of Montevallo, a key economic and educational influence.
-</p>
-<p className="mb-2 text-left">
-    The University of Montevallo, established for womens education in various fields, evolved into a public liberal arts university, shaping the citys culture and economy.
-</p>
-<p className="mb-2 text-left">
-    Montevallos rich history is preserved in 73 buildings listed on the National Historic Register, including a New Deal-era adorned historic post office.
-</p>
-<p className="mb-2 text-left">
-    The citys population has steadily grown, with a diverse community. The presence of the university contributes to a younger median age compared to the national average.
-</p>
 
-<p className="text-gray-600">Countys | <span className="text-indigo-900">Shelby</span></p>
-<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35115</span></p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Montevallo is located in Shelby County, Alabama, and has a rich history dating back to its incorporation in 1848. It was originally known for its agriculture and later became an industrial center with the establishment of manufacturing facilities.</p>       
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Montevallo is centrally located in Alabama, around 35 miles south of Birmingham. Spanning about 7 square miles, its surrounded by rolling hills and picturesque countryside.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">The city operates under a mayor-council system, with the Mayor and City Council responsible for administration, policy-making, and community development.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">Montevallos diverse economy includes education, manufacturing, retail, and healthcare. The University of Montevallo is a key contributor to local economic activity.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">The city is home to the University of Montevallo, offering a range of higher education programs, alongside several public and private schools serving the community.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Accessible via Interstate 65 and close to Birmingham-Shuttlesworth International Airport, Montevallos transportation is primarily via private vehicles or public services.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Montevallo offers attractions like the American Village, Orr Park, and the Alabama Wine Trail. Notable events include the annual College Night at the University of Montevallo.</p>
+
+<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900"></span></p>
+<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900"></span></p>
+
+
         </div>
 
         <div className="mt-auto">
@@ -71,64 +80,39 @@ const WebDesignMontevallo = () => {
             </div>
         </div>
 
-
-        <div className="flex-1 flex flex-col">
+<div className="flex-1 flex flex-col">
     <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
-        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Montevallo</h2>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Montevallo</h2>
 
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>University of Montevallo</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Explore the beautiful campus of the University of Montevallo, known for its historic buildings, lush landscapes, and vibrant arts community. You can take a leisurely stroll around the campus and even catch a performance or art exhibition.</p>
 
-<p className="text-indigo-950 text-lg mb-1">
-    <b>Orr Park</b>
-</p>
-<p className="mb-2 text-white text-lg">
-    Visit Orr Park for its unique tree carvings, walking trails, and picnic areas. The park showcases local artist Tim Tingles impressive tree artwork, including the notable Tinglewood carvings.
-</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Montevallo Main Street District</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Enjoy the ambiance of downtown Montevallo, featuring local shops, boutiques, and eateries. Its perfect for unique shopping experiences and savoring local cuisine.</p>
 
-<p className="text-indigo-950 text-lg mb-1">
-    <b>Shoal Creek Park</b>
-</p>
-<p className="mb-2 text-white text-lg">
-    Shoal Creek Park offers fishing, picnicking, and scenic hiking trails. Enjoy its well-stocked fishing lake and explore the natural beauty of the surrounding landscape on various trails.
-</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Orr Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Discover the tranquility of Orr Park with its unique Tinglewood Trail tree carvings. Ideal for relaxing, picnicking, or biking along Shoal Creek.</p>
 
-<p className="text-indigo-950 text-lg mb-1">
-    <b>Alabama Wildlife Center</b>
-</p>
-<p className="mb-2 text-white text-lg">
-    The center is dedicated to the care and rehabilitation of Alabamas native birds. Visitors can volunteer, engage in conservation efforts, and learn about avian ecology.
-</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Parnell Memorial Library</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Visit the Parnell Memorial Library for a vast book selection, community events, and a peaceful reading environment.</p>
 
-<p className="text-indigo-950 text-lg mb-1">
-    <b>Main Street Montevallo</b>
-</p>
-<p className="mb-2 text-white text-lg">
-    Explore the historic downtown area, with a variety of local shops, restaurants, and cultural events reflecting Montevallos rich heritage.
-</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Pea Ridge Mine</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Explore the Pea Ridge Mines trails and scenic views, offering insights into the regions mining heritage just outside Montevallo.</p>
 
-<p className="text-indigo-950 text-lg mb-1">
-    <b>University of Montevallo Campus</b>
-</p>
-<p className="mb-2 text-white text-lg">
-    Visit the universitys picturesque campus, known for its beautiful architecture, historical buildings, and vibrant student life.
-</p>
-
-
-
-
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Alabama Wildlife Center</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Tour the Alabama Wildlife Center, a refuge for injured and orphaned wildlife, where you can learn about native Alabama species and their rehabilitation.</p>
         </div>
     </div>
 </div>
 
-
 {/* Elevate Your Business Title Section */}
 <div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
-<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with Web Design</h2>
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Montevallo Business with Web Design</h2>
 
 {/* Elevate Your Business Section */}
 <p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
 In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
 </p>
-
                 {/* Contact Us Button */}
                 <Link href="/contact" legacyBehavior>
                 <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
