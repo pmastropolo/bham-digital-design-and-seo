cd993c306fa5683e90719546a17c78253af6cb97
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 20:15:42 -0600

    Update web-design-in-vestavia-hills-al.js

--- pages/web-design-in-vestavia-hills-al.js
+++ pages/web-design-in-vestavia-hills-al.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const WebDesignVestaviaHills = () => {
      
@@ -19,109 +18,130 @@ const WebDesignVestaviaHills = () => {
                             }}
                         />
 
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-                <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Vestavia Hills, AL</h2>
-                <p className="text-gray-600 mb-4">Nickname | The City of Gold</p>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Vestavia Hills, AL
+    </h1>
 
-                <section className="mb-4">
-                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700">
-                            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-left">Discover Vestavia Hills, AL</h2>
-                            <p className="text-slate-500 mb-4 text-left"><b>County</b> | Jefferson</p>
-                            <p className="mb-2 text-left">Rich history of iron ore mining, contributing to the areas growth.</p>
-                            <p className="mb-2 text-left">Officially incorporated as a city on November 8, 1950, through the merger of several communities.</p>
-                            <p className="mb-2 text-left">Named after the historic estate Vestavia, believed to be a combination of Vesta | Roman goddess of the hearth & via meaning way.</p>
-                            <p className="mb-2 text-left">Known for picturesque landscapes, including lush green hills and scenic views.</p>
-                            <p className="mb-2 text-left">Home to highly-rated schools, making it a desirable place for families.</p>
-                            <p className="mb-2 text-left">Offers various recreational opportunities, including parks, hiking trails, and sports facilities.</p>
-                            <p className="mb-2 text-left">Has a diverse local economy with businesses, retail centers, and restaurants.</p>
-                            <p className="mb-2 text-left">Known for strong community involvement and civic engagement with numerous events and activities.</p>
-                            <p className="text-gray-600 mb-4 text-left"><b>Zip Codes</b> | 35216, 35226, 35242, 35243</p>
-                        </div>
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex items-center justify-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:Welcome_sign_to_Vestavia_Hills.jpg" target="_blank" rel="noopener noreferrer">
-                        <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Welcome_sign_to_Vestavia_Hills.jpg?20120624021607"
-                alt="Welcome Sign on Montgomery Highway coming into Vestavia Hills Alabama"
-                width={400} 
-                height={300} 
-                className="rounded-lg"
-            />
-            </a>
-                        </div>
-                    </div>
-                </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Nick Name</span>
+            The City of Gold
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+</div>
 
-                <section className="mt-4">
-                    <div className="bg-slate-100 p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex justify-center items-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:LibertyParkAl.jpg" target="_blank" rel="noopener noreferrer">
-                        <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/1/10/LibertyParkAl.jpg?20190621014412"
-                alt="Statue of Liberty replica in Vestavia Hills, Alabama."
-                width={400} 
-                height={300} 
-                className="rounded-lg max-w-sm"
-            />
-            </a>
-                        </div>
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700 text-center md:text-center mt-4">
-                            <h2 className="text-4xl font-bold text-gray-800 mb-4 text-left mt-4">Best Things to Do in Vestavia Hills</h2>
-                            <div className="mt-4">
 
-            <p className="mb-2 text-indigo-500 text-left"><b className="underline text-lg"><a href="https://vhparksandrec.com/location/wald-park/">Wald Park</a></b> | Enjoy Outdoor Fun.</p>
-            <p className="mb-2 text-indigo-500 text-left"><b className="underline text-lg"><a href="https://redmountainpark.org/">Red Mountain Park</a></b> | Hike Scenic Trails.</p>
-            <p className="mb-2 text-indigo-500 text-left"><b className="underline text-lg"><a href="https://www.vestaviacc.com/">Vestavia Country Club</a></b> | 18-hole Golf Course.</p>
-            <p className="mb-2 text-indigo-500 text-left"><b className="underline text-lg"><a href="http://vestavialibrary.org/">Vestavia Hills Library</a></b> | Explore Art & Culture.</p>
-            <p className="mb-2 text-indigo-500 text-left"><b className="underline text-lg"><a href="https://vhal.org/sibyl-temple-history/">Temple of Sibyl</a></b> | Inspired by Italys Temple of Sibyl.</p>
-            <p className="mb-2 text-indigo-500 text-left"><b className="underline text-lg"><a href="https://www.altadenavalleyanimalclinic.com/">Altadena Valley Animal Clinic</a></b> | Visit For Animal Encounters & Programs.</p>
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
 
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Vestavia Hills</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
 
-                <section className="mb-8 mt-4">
-                    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap md:flex-nowrap">
-                        {/* Text Section */}
-                        <div className="w-full md:w-1/2 text-gray-700">
-                            <h2 className="text-3xl font-bold text-gray-800 mb-4">Elevate Your Business in Vestavia Hills with Our Web Design Expertise</h2>
-                            <p>In the digital age, a compelling online presence is more than just a luxury. Its a necessity. At Bham Web Design and SEO Co., we understand the heartbeat of Vestavia Hills, AL. Our web design solutions are not just aesthetically pleasing but also strategically crafted to meet the unique needs and goals of your local business.</p>
-                            <p className="mt-4">We blend innovative design with cutting-edge functionality, ensuring your site stands out in a crowded digital landscape. With our finger on the pulse of current trends and timeless design principles, we promise a website that not only looks stunning but also drives growth, engages your audience, and elevates your brand.</p>
-                            <div className="mt-6 gap-8">
-                                <Link href="/contact" legacyBehavior>
-                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mr-4 mb-4 py-2 px-4 rounded">
-                                        Contact Us
-                                    </a>
-                                </Link>
 
-                                {/* Web Design Packages Button with Link */}
-                                <Link href="/PackagePage" legacyBehavior>
-                                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white mb-4 py-2 px-4 rounded">
-                                        View Web Design Packages
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                        {/* Image Section */}
-                        <div className="w-full md:w-1/2 flex items-center justify-center">
-                        <a href="https://commons.wikimedia.org/wiki/File:Alabama-Tennessee-state-line-207-11-altn1.jpg" target="_blank" rel="noopener noreferrer">
-            <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alabama-Tennessee-state-line-207-11-altn1.jpg/1200px-Alabama-Tennessee-state-line-207-11-altn1.jpg?20161031223120"
-                alt="Alabama welcome sign Sweet Home, Alabama at the border between Giles County, Tennessee, and Lauderdale County, Alabama, in the southern United States."
-                width={400} 
-                height={300} 
-                className="rounded-lg"
-            />
-              </a>
-                        </div>
-                    </div>
-                </section>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Vestavia Hills, originally known as Vestavia which means hills in Italian, was incorporated as a city in 1950. The area was primarily rural until the 1940s when residential development began. Over the years, Vestavia Hills has grown into a thriving suburb of Birmingham, Alabama.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Vestavia Hills is located in Jefferson County, Alabama. It is situated on the southern slopes of Shades Mountain, providing picturesque views and natural beauty. The city covers an area of approximately 19 square miles.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">Vestavia Hills operates under a mayor-council form of government. The citys mayor and city council members are elected by residents to govern and make decisions on local matters. They work to enhance the quality of life for residents and ensure efficient municipal services.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">Vestavia Hills has a strong and diverse economy with several sectors contributing to its growth. The city has a mix of retail centers, restaurants, professional services, healthcare facilities, and small businesses. It is also home to corporate headquarters and office parks that provide employment opportunities.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Vestavia Hills is well-known for its exceptional school system. The Vestavia Hills City School District consists of several highly rated public schools, including elementary, middle, and high schools. These schools offer rigorous academic programs and a range of extracurricular activities.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">The city is well-connected through various transportation options. Major highways, including I-65 and US-31, pass through Vestavia Hills, providing easy access to downtown Birmingham and other neighboring cities. The Birmingham-Shuttlesworth International Airport is also within a reasonable driving distance.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Vestavia Hills offers a variety of attractions and events for residents and visitors alike. Some popular attractions include the Vestavia Hills Public Library, Cahaba River Walk, and the beautiful Vestavia Hills City Center. The city also hosts community events throughout the year, such as parades, festivals, and concerts.</p>
+
+
+
+
+<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900">Jefferson</span></p>
+<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900">35216 | 35226 | 35242 | 35243</span></p>
+
+
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Vestavia Hills Business Online
+                    </a>
+                    </Link>
+                </div>
             </div>
+        </div>
+
+
+        <div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Vestavia Hills</h2>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Explore the Natural Beauty</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Take a hike or enjoy a picnic at one of the citys beautiful parks, such as Wald Park or Vestavia Hills Central Park. These parks offer walking trails, sports fields, playgrounds, and serene green spaces.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Golfing</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">If youre a golf enthusiast, Vestavia Hills has a few golf courses where you can perfect your swing. The Vestavia Country Club and the Altadena Valley Country Club both offer challenging courses and stunning views.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Shopping and Dining</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Vestavia Hills has a variety of shopping centers and restaurants. The Cahaba Heights area has a mix of local boutiques, specialty shops, and restaurants offering diverse cuisines. The Vestavia Hills City Center is another shopping and dining destination with a selection of stores and eateries.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Attend Local Events</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Keep an eye out for local events happening in Vestavia Hills throughout the year. The city often hosts community festivals, farmers markets, outdoor concerts, and holiday celebrations. These events are great opportunities to connect with the local community and have fun.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Visit the Vestavia Hills Library</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">The Vestavia Hills Library in the Forest is a fantastic resource for book lovers and offers various activities for all ages. You can attend author signings, book clubs, workshops, and educational programs.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Discover Art and Culture</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">The Vestavia Hills Civic Center houses the Vestavia Hills Art Associations gallery, where you can appreciate local artwork. The center also hosts occasional performances, exhibitions, and cultural events.</p>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Vestavia Hills Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
         </>
     );
 }
