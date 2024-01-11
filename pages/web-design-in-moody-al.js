667b0abd5763058561f59d3b3fa3952a803f7791
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 19:46:13 -0600

    Update web-design-in-moody-al.js

--- pages/web-design-in-moody-al.js
+++ pages/web-design-in-moody-al.js
@@ -15,20 +15,131 @@ const WebDesignMoody = () => {
                     type: 'website',
                 }}
             />
-<div className="bg-white text-center p-10 shadow-lg rounded-lg mb-10 z-10 relative">
-  <h2 className="text-5xl font-extrabold text-gray-800 mt-4 mb-6">Web Design Services in Moody, AL</h2>
-  <p className="text-lg mb-8 text-gray-600">Bham Web Design and SEO Co. is your go-to destination for professional web design services in Moody, AL. We specialize in crafting websites that represent your brand and connect with your Moody audience.</p>
 
-  <section className="mb-8">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Why Choose Us for Web Design in Moody?</h2>
-    <p className="text-gray-600">In Moody, AL, a strong web presence is vital. Our web design solutions are personalized to meet the unique demands and goals of your Moody business, ensuring an impactful and effective online presence.</p>
-  </section>
+<div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
+    <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
+        Web Design Services in Moody, AL
+    </h1>
 
-  <section>
-    <h2 className="text-2xl font-semibold text-gray-800 mb-2">Ready to Kickstart Your Web Design Project?</h2>
-    <p className="text-gray-600">If youre looking to elevate your digital footprint in Moody, AL, Bham Web Design and SEO Co. is here to assist. Contact us today to discuss your web design needs and embark on your journey to digital success.</p>
-  </section>
+    <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
+        Empowering your businesss online presence with our expert web design and SEO solutions.
+    </p>
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+        <p className="text-lg md:text-xl text-white">
+            <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
+            Home of ADESA, the largest auto auction in the southeast.
+        </p>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
+
+
+{/* Discover Section */}
+<div className="max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col">
+            <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
+
+                {/* Discover Title */}
+                <h2 className="text-4xl font-bold text-gray-800">Discover Moody</h2>  
+                    <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Moody, Alabama was incorporated as a town in 1962. It was named after either James Moody, a prominent landowner in the area, or for the Moody family who were early settlers. The town has a rich history of agriculture and saw significant growth with the arrival of the railroad in the late 19th century.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Moody, located in St. Clair County, Alabama, is about 20 miles east of Birmingham. Covering approximately 24 square miles, it features rolling hills, forests, and open spaces.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">Moody operates under a Mayor-Council government, with elected officials overseeing the communitys needs and services, including public safety and recreation.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">The economy of Moody is diverse, encompassing manufacturing, retail, services, and healthcare, with various businesses and commercial centers providing employment opportunities.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Moody is part of the St. Clair County School District, offering kindergarten through high school education at its various public schools.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Located near major routes like Interstate 20 and State Route 411, Moody provides easy access to nearby cities, with Birmingham-Shuttlesworth International Airport about 20 miles away.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Moody offers parks and recreational areas for outdoor activities and hosts various community events throughout the year, including festivals and concerts.</p>
+
+<p className="text-gray-600 text-left"><b>County</b> : <span className="font-semibold text-indigo-900">St. Clair County</span></p>
+<p className="text-gray-600 text-left"><b>Zip Codes</b> : <span className="font-semibold text-indigo-900"></span> 35004 | 35094 | 35120 | 35173</p>
+
+        </div>
+
+        <div className="mt-auto">
+        <Link href="/PackagePage" legacyBehavior>
+                    <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
+                        Elevate Your Moody Business Online
+                    </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+
+<div className="flex-1 flex flex-col">
+    <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Moody</h2>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Alabama Motorsports Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">If youre a fan of motorsports, head to Alabama Motorsports Park in Moody. You can watch thrilling car races or even participate in racing events if youre feeling adventurous.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Ruffner Mountain Nature Preserve</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Visit Ruffner Mountain Nature Preserve near Moody for over 14 miles of hiking trails, stunning vistas, and diverse wildlife. Its a perfect spot for nature enthusiasts.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Eagle Point Golf Club</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Golfers will enjoy Eagle Point Golf Clubs 18-hole championship course, known for its picturesque landscapes and challenging play.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Moody Miracle League Field</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">The inclusive Moody Miracle League Field caters to individuals with special needs, offering a wonderful space for sports and community support.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Local Events in Moody</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Participate in Moodys vibrant local events, including festivals, parades, and concerts, showcasing community talent and culture.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Moody City Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Enjoy outdoor activities at Moody City Park, featuring walking trails, picnic areas, playgrounds, and sports fields for family fun.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Moody Historical Museum</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Explore Moodys history at the Moody Historical Museum, with exhibits and artifacts that chronicle the towns past.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Local Shopping and Dining</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Support Moodys local shops and restaurants, where you can enjoy unique finds and delicious meals, experiencing the towns local charm.</p>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+{/* Elevate Your Business Title Section */}
+<div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Moody Business with Web Design</h2>
+
+{/* Elevate Your Business Section */}
+<p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
+In the digital age, a compelling online presence is more than just a luxury. Its a necessity. Our web design solutions are strategically crafted to meet the unique needs and goals of your local business. Blending innovative design with cutting-edge functionality, we ensure your site stands out in a crowded digital landscape.
+</p>
+                {/* Contact Us Button */}
+                <Link href="/contact" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-cyan-800 text-white mb-4 py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Contact Us
+                </a>
+                </Link>
+
+                {/* Web Design Form Button */}
+                <Link href="/webDesignConsultation" legacyBehavior>
+                <a className="inline-block bg-blue-600 hover:bg-indigo-800 text-white py-2 px-6 rounded-full transition duration-300 transform hover:scale-110 mx-2">
+                    Start Web Design Initial Consultation
+                </a>
+                </Link>
+            </div>
+        </div>
+    </div>
 
         </>
     );
