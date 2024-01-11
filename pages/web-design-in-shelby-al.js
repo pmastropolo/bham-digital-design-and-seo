78fc22432fc08900b0194f77b0cee9d82df26b4a
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-10 20:16:58 -0600

    Update web-design-in-shelby-al.js

--- pages/web-design-in-shelby-al.js
+++ pages/web-design-in-shelby-al.js
@@ -18,19 +18,18 @@ const WebDesignShelby = () => {
 
 <div className="bg-gradient-to-br from-slate-950 to-indigo-950 text-center py-12 px-6 shadow-xl rounded-2xl mt-10 mb-12 transform hover:scale-105 transition duration-500 ease-in-out relative overflow-hidden">
     <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-700 via-white to-indigo-200 my-4 animate-gradient">
-        Web Design Services in Shelby, AL
+        Web Design Services in Shelby County, AL
     </h1>
 
     <p className="text-white text-opacity-90 mb-8 text-lg md:text-xl leading-loose max-w-xl md:max-w-3xl mx-auto">
         Empowering your businesss online presence with our expert web design and SEO solutions.
     </p>
-        <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
+    <div className="py-4 px-6 mb-4 md:mb-6 mt-4 bg-white bg-opacity-20 rounded-lg">
         <p className="text-lg md:text-xl text-white">
             <span className="block mb-2 text-2xl md:text-3xl text-indigo-300">Fun Fact</span>
-            p
+            The county was named for Isaac Shelby, a hero of the Kings Mountain Battle during the Revolutionary War.
         </p>
     </div>
-
     <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
 </div>
 
@@ -42,39 +41,38 @@ const WebDesignShelby = () => {
             <div className="bg-gradient-to-br from-gray-50 to-blue-100 p-8 rounded-2xl shadow-lg mb-8 flex-grow transform transition duration-500 hover:shadow-2xl">
 
                 {/* Discover Title */}
-                <h2 className="text-4xl font-bold text-gray-800">Discover Shelby</h2>  
+                <h2 className="text-4xl font-bold text-gray-800">Discover Shelby County</h2>  
                     <div className="p-8 rounded-2xl shadow-lg mb-4 flex-1 hover:shadow-2xl">
-                        
 
-<p className="mb-4"><span className="font-semibold text-indigo-900">Establishment:</span> Shelby County, Alabama, was established on <span className="font-semibold text-indigo-900">February 7, 1818</span>, named after Isaac Shelby, a hero of the Revolutionary War.</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">History</b></p>
+<p className="mb-4 text-left">Shelby County was established on February 7, 1818, and named after Isaac Shelby, the first governor of Kentucky. The county played a significant role in the Civil Rights Movement and was a site of pivotal events such as the Selma to Montgomery marches.</p>
 
-<p className="mb-4">
-<span className="font-semibold text-indigo-900">Demographics:</span> As of the <span className="font-semibold text-indigo-900">2020 Census</span>, the population was <span className="font-semibold text-indigo-900">216,350</span>. The largest city is Alabaster, and the county seat is Columbiana.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Geography</b></p>
+<p className="mb-4 text-left">Located in central Alabama, part of the Birmingham metro area. Encompasses around 811 square miles with a mix of rolling hills, forests, and lakes.</p>
 
-<p className="mb-4">
-<span className="font-semibold text-indigo-900">Economy:</span> Historically, the countys economy was based on <span className="font-semibold text-indigo-900">agriculture</span> and <span className="font-semibold text-indigo-900">mineral extraction</span>, including cotton farming, iron ore, and coal mining.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Government</b></p>
+<p className="mb-4 text-left">Operates under a commission form. Consists of five elected commissioners managing county administration.</p>
 
-<p className="mb-4">
-<span className="font-semibold text-indigo-900">Geography:</span> Shelby County is part of the <span className="font-semibold text-indigo-900">Appalachian mountain range</span>, featuring the Coosa and Cahaba rivers. It has diverse terrain suitable for agriculture and pastures.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Economy</b></p>
+<p className="mb-4 text-left">Diverse sectors like healthcare, manufacturing, retail, and construction. Home to major companies, showing steady economic growth.</p>
 
-<p className="mb-4">
-<span className="font-semibold text-indigo-900">Transportation:</span> Major transport routes include <span className="font-semibold text-indigo-900">Interstate 65</span> and <span className="font-semibold text-indigo-900">U.S. Highways 31 and 280</span>.
-</p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Education</b></p>
+<p className="mb-4 text-left">Strong system with numerous public/private schools. Primary district is Shelby County Schools. Hosts colleges like University of Montevallo and Jefferson State Community College.</p>
 
-<p className="mb-4">
-<span className="font-semibold text-indigo-900">Workforce:</span> The largest employment sectors are <span className="font-semibold text-indigo-900">educational services, healthcare, and retail trade</span>.
-</p>
-<p className="text-gray-600">County | <span className="text-indigo-900">Shelby</span></p>
-<p className="text-gray-600">Zip Codes | <span className="text-indigo-900">35051, 35143</span></p>
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Transportation</b></p>
+<p className="mb-4 text-left">Well-developed network with Interstate 65. State highways and local roads connect the county. Birmingham-Shuttlesworth International Airport is the nearest major airport.</p>
+
+<p className="mb-2 text-xl text-left"><b className="font-semibold text-indigo-900">Attractions and Events</b></p>
+<p className="mb-4 text-left">Features Oak Mountain State Park for outdoor activities. Alabama Wildlife Center rehabilitates animals. Hosts various events like festivals and concerts.</p>
+
+
+
         </div>
 
         <div className="mt-auto">
         <Link href="/PackagePage" legacyBehavior>
                     <a className="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300">
-                        Elevate Your Shelby Business Online
+                        Elevate Your Shelby County Business Online
                     </a>
                     </Link>
                 </div>
@@ -84,25 +82,33 @@ const WebDesignShelby = () => {
 
         <div className="flex-1 flex flex-col">
     <div className="rounded-lg shadow-md mb-6 flex-grow" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
-        <h2 className="text-4xl font-bold text-indigo-950 mt-5 mb-6 text-center">Explore Shelby</h2>
+        <h2 className="text-3xl sm:text-4xl font-bold text-indigo-950 mt-4 sm:mt-5 mb-5 sm:mb-6 text-center">Best Things to Do in Shelby County</h2>
 
-        <p className="text-indigo-950 text-lg mb-1">Cahaba River & Shelby Iron Works Parks</p>
-<p className="mb-2 text-white text-lg">Outdoor exploration and relaxation.</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Oak Mountain State Park</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">This expansive park offers numerous outdoor activities such as hiking, biking, fishing, swimming, camping, and horseback riding. It also has a golf course, a petting zoo, and picnic areas.</p>
 
-<p className="text-indigo-950 text-lg mb-1">Karl C. Harrison Museum</p>
-<p className="mb-2 text-white text-lg">Historical artifacts from the 18th and 19th centuries.</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>American Village</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">A historical attraction preserving American history and citizenship principles with historic building replicas, exhibits, reenactments, and educational programs.</p>
 
-<p className="text-indigo-950 text-lg mb-1">The American Village</p>
-<p className="mb-2 text-white text-lg">Immersive early American history experience.</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Alabama Wildlife Center</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Located in Oak Mountain State Park, this center rehabilitates wildlife and offers educational programs and nature trails to observe native wildlife.</p>
 
-<p className="text-indigo-950 text-lg mb-1">Birmingham Bulls Hockey</p>
-<p className="mb-2 text-white text-lg">Exciting local hockey games.</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Cahaba River National Wildlife Refuge</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Home to diverse species, this refuge offers birdwatching, hiking, and nature photography along the Cahaba River.</p>
 
-<p className="text-indigo-950 text-lg mb-1">Rusted Roof Barn</p>
-<p className="mb-2 text-white text-lg">Horseback riding in Oak Mountain State Park.</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Downtown Columbiana</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Explore Columbianas downtown with boutique shops, antique stores, and local restaurants. Visit the historic courthouse square for a unique experience.</p>
 
-<p className="text-indigo-950 text-lg mb-1">Morgan Creek Winery</p>
-<p className="mb-2 text-white text-lg">Wine tasting in a scenic vineyard.</p>
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Shelby County Arts Council</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Promotes arts through exhibitions, workshops, performances, and events. Check for art shows, concerts, theater productions, and more.</p>
+
+<p className="text-indigo-950 text-xl sm:text-2xl text-center"><b>Scenic Drive Along Highway 119</b></p>
+<p className="mb-4 text-white text-base sm:text-lg text-center">Enjoy a picturesque drive through Shelby County on Highway 119, offering beautiful views of the countryside, hills, and forests with stops at local attractions.</p>
+
+
+
+
+
         </div>
     </div>
 </div>
@@ -110,7 +116,7 @@ const WebDesignShelby = () => {
 
 {/* Elevate Your Business Title Section */}
 <div className="bg-gradient-to-br from-gray-100 to-blue-50 p-8 rounded-3xl shadow-2xl">
-<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Business with Web Design</h2>
+<h2 className="text-5xl font-bold text-gray-800 mb-5 text-center">Elevate Your Shelby County Business with Web Design</h2>
 
 {/* Elevate Your Business Section */}
 <p className="mt-6 mb-8 text-lg md:text-md text-gray-700">
