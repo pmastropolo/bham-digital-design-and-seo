154ca569d8889beb1fcfd8f057ea85eea4ecf6c0
Peyton Mastropolo <amyhytwn@aol.com>
2024-01-25 20:12:12 -0600

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -5,18 +5,18 @@ import { NextSeo } from 'next-seo';
 const privacy = () => (
   <>
     <NextSeo
-      title="Privacy Policy | Bham SEO & Design Co."
-      description="Learn about our privacy policy at Bham SEO & Design Co. We are committed to protecting your personal information and privacy."
+      title="Privacy Policy | Bham Web Design and SEO Co."
+      description="Learn about our privacy policy at Bham Web Design and SEO Co. We are committed to protecting your personal information and privacy."
       canonical="https://bhamseoanddesignco.com/privacy"
       openGraph={{
         url: 'https://bhamseoanddesignco.com/privacy',
-        title: 'Privacy Policy | Bham SEO & Design Co.',
-        description: 'Explore the privacy policy at Bham SEO & Design Co. Understand how we handle your personal information and protect your privacy.',
+        title: 'Privacy Policy | Bham Web Design and SEO Co.',
+        description: 'Explore the privacy policy at Bham Web Design and SEO Co. Understand how we handle your personal information and protect your privacy.',
       }}
       additionalMetaTags={[
         {
           property: 'keywords',
-          content: 'privacy, personal information, privacy policy, policy, data protection, Bham SEO & Design Co.'
+          content: 'privacy, personal information, privacy policy, policy, data protection, Bham Web Design and SEO Co.'
         },
         {
           name: 'viewport',
